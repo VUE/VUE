faa6e8fd1238b091158478f9dcd015c5109f00f7
Anoop Kumar <anoopkum@gmail.com>
2003-04-01 19:46:23 +0000

    no message
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@17 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/DRBrowser.java
+++ VUE2/src/tufts/vue/DRBrowser.java
@@ -1,14 +1,18 @@
 package tufts.vue;
 
 import javax.swing.border.TitledBorder;
+import javax.swing.*;
 
 /**
  * Digital Repositor Browser
  */
-class DRBrowser extends javax.swing.JPanel
+class DRBrowser extends javax.swing.JTabbedPane
 {
     public DRBrowser()
     {
         setBorder(new TitledBorder("DR Browser"));
+        add("File", new JInternalFrame("Local Files"));
+        add("FEDORA",new JInternalFrame("FEDORA"));
     }
+    
 }
