f7ee5dd4c1f41049294cce089c8072242c575c24
Scott Fraize <scott.fraize@gmail.com>
2003-04-06 17:43:26 +0000

    tweak
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@35 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/VueConstants.java
+++ VUE2/src/tufts/vue/VueConstants.java
@@ -11,6 +11,16 @@ public interface VueConstants
     static Font DefaultFont = new Font("SansSerif", Font.PLAIN, 18);
     static Font MediumFont = new Font("SansSerif", Font.PLAIN, 12);
     static Font SmallFont = new Font("SansSerif", Font.PLAIN, 10);
+    static Font LinkLabelFont = new Font("SansSerif", Font.PLAIN, 10);
+    
+    static java.awt.Color COLOR_LINK_LABEL = java.awt.Color.darkGray;
+    static java.awt.Color COLOR_SELECTION = java.awt.Color.blue;
+    static java.awt.Color COLOR_SELECTION_DRAG = java.awt.Color.gray;
+    static java.awt.Color COLOR_INDICATION = java.awt.Color.red;
+    static java.awt.Color COLOR_DEFAULT = java.awt.Color.black;
+    static java.awt.Color COLOR_FAINT = java.awt.Color.lightGray;
+
+    static java.awt.Color DEFAULT_NODE_COLOR = new Color(200, 200, 255);
     
     // todo: create our own cursors for most of these
     // named cursor types
@@ -33,13 +43,6 @@ public interface VueConstants
     static java.awt.Stroke STROKE_SELECTION = new java.awt.BasicStroke(1f);
     static java.awt.Stroke STROKE_SELECTION_DYNAMIC = new java.awt.BasicStroke(1f);
 
-    static java.awt.Color COLOR_SELECTION = java.awt.Color.blue;
-    static java.awt.Color COLOR_SELECTION_DRAG = java.awt.Color.gray;
-    static java.awt.Color COLOR_INDICATION = java.awt.Color.red;
-    static java.awt.Color COLOR_DEFAULT = java.awt.Color.black;
-    static java.awt.Color COLOR_FAINT = java.awt.Color.lightGray;
-
-    static java.awt.Color DEFAULT_NODE_COLOR = new Color(200, 200, 255);
 
 
     
