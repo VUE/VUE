2fae03147cc871e14bc4cc196f1a7c842b65e2eb
daisuke daisuke <daisuke@tufts.edu>
2003-05-30 18:54:11 +0000

    no message
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@81 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/ImageConversion.java
+++ VUE2/src/tufts/vue/ImageConversion.java
@@ -15,20 +15,26 @@ import javax.swing.*;
  *
  * @author  Daisuke Fujiwara
  */
+
+/**a class which constructs a JPEG image of the current concept map*/
 public class ImageConversion extends AbstractAction {
     
     /** Creates a new instance of ImageConversion */
     public ImageConversion() {
     }
     
+    /**A constructor */
     public ImageConversion(String label)
     {
         super(label);
         putValue(Action.SHORT_DESCRIPTION,label);        
     }
     
+    /**A method which takes in the image object and the location of the file along with the file format
+     and saves the current map to the given file*/
     public void convert(BufferedImage image, String location, String format)
     {   
+        //the conversion is done using the ImageIO class's static method
         try
         {
             //String[] files = ImageIO.getReaderFormatNames();
@@ -44,6 +50,7 @@ public class ImageConversion extends AbstractAction {
         }
     }
     
+    /**A default draw function for the testing purpose */
     private void drawImage(Graphics2D g2, Dimension size)
     {
         g2.setColor(Color.white);
@@ -53,6 +60,7 @@ public class ImageConversion extends AbstractAction {
         
     }
     
+    /** A main function for the testing purpose */
     public static void main (String[] args)
     {
         BufferedImage defaultImage = new BufferedImage(300, 400, BufferedImage.TYPE_INT_RGB);       
@@ -62,20 +70,27 @@ public class ImageConversion extends AbstractAction {
         test.convert(defaultImage, "c:\\test.jpeg", "jpeg");
     }
     
+    /**A method defined in the interface */
     public void actionPerformed(java.awt.event.ActionEvent actionEvent) {
         System.out.println("Performing Conversion:" + actionEvent.getActionCommand());
         
+        //retrives the current map and gets its size
         MapViewer currentMap = (MapViewer)VUE.tabbedPane.getSelectedComponent();
         Dimension size = currentMap.getSize();
         
+        //creates an image object and sets up the graphics object of the image
         BufferedImage mapImage = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_RGB);
         Graphics g = mapImage.getGraphics();
         g.setClip(0, 0, size.width, size.height);
+        
+        //let the map draws to the image object's graphic object
         currentMap.paintComponent(g);
         
+        //outlining the returned image
         g.setColor(Color.black);
         g.drawRect(0, 0, size.width - 1, size.height - 1);
         
+        //begins the conversion to the file
         convert(mapImage, "c:\\" + currentMap.getMap().getLabel() + ".jpeg", "jpeg");
     }
     
