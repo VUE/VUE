59b3d326c8565da0b3f159856cb2b86cc5c09d3c
jay jay <jay@tufts.edu>
2003-06-06 19:43:24 +0000

    no message
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@94 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/vue.bat
+++ VUE2/src/vue.bat
@@ -1,14 +1,14 @@
 
 @echo off
-REM Change the following line to set your JDK path
-set JAVA_HOME=%JAVA_HOME%
+REM Change the following two lines to set your JDK path and vue home path
+
+set JAVA_HOME=C:\j2sdk1.4.0_03
+set VUE_HOME=C:\vueproject\VUEDevelopment\src
+
 set JAVA=%JAVA_HOME%\bin\java
 set JAVAC=%JAVA_HOME%\bin\javac
 
 @echo Create the classpath
-set CP=c:\anoop\lib\castor-0.9.4.3.jar;c:\anoop\lib\castor-0.9.4.3-xml.jar;c:\anoop\lib\xercesImpl.jar;c:\anoop\castor\castor-0.9.4.3\examples\SourceGenerator\;c:\anoop\lib\jakarta-regexp-1.2.jar;c:\anoop\dr;c:\anoop\euclid\VUEDevelopment\src;c:\anoop\lib\src\
-
-set CP=%CP%;..\..\..\build\classes;%JDK_BIN%\lib\tools.jar
-
+set CP=%VUE_HOME%;%VUE_HOME%\..\lib\castor-0.9.4.3.jar;%VUE_HOME%\..\lib\castor-0.9.4.3-xml.jar;%VUE_HOME%\..\lib\batik-svggen.jar;%VUE_HOME%\..\lib\xercesImpl.jar;%VUE_HOME%\..\lib\jakarta-regexp-1.2.jar;%VUE_HOME%\..\lib\batik-awt-util.jar;%VUE_HOME%\..\lib\batik-ext.jar;%VUE_HOME%\..\lib\batik-util.jar;%VUE_HOME%\tufts\dr;
  
 %JAVA% -cp %CP% tufts.vue.VUE
