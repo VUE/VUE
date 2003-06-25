74124352bca83d95984b6ba70beb957177fc3e4e
jay jay <jay@tufts.edu>
2003-06-25 14:43:57 +0000

    no message
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@204 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/tufts/vue/InspectorWindow.java
@@ -0,0 +1,47 @@
+/*
+ * InspectorWindow.java
+ *
+ * Created on June 25, 2003, 10:27 AM
+ *
+ * This class can be used to create a stand alone window thats always
+ * on top of the main app. It can be used instead of the ToolWindow class
+ * to allow editing of fields and the ability for the user to close the
+ * window.
+ */
+
+package tufts.vue;
+
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.awt.Point;
+import java.awt.Dimension;
+
+/**
+ *
+ * @author  Jay Briedis
+ */
+public class InspectorWindow extends JDialog {
+    
+    private JFrame owner = null;
+    private Point location = new Point(300, 300);
+    private Dimension size = new Dimension(200, 200);
+    
+    public InspectorWindow() {
+        this((JFrame)null, "Default Window");
+    }
+    
+    public InspectorWindow(JFrame owner, String title, Point loc){
+        this(owner, title);
+        this.location = loc;
+    }
+    
+    public InspectorWindow(JFrame owner, String title) {
+        super(owner, title);
+        this.owner = owner;
+        this.setModal(false);
+        this.setLocation(location);
+        
+        this.setSize(size);
+    }
+}
