1aec3e760778d7ab6c5c42fcea7655ec4d38fc95
Anoop Kumar <anoopkum@gmail.com>
2003-06-27 14:07:42 +0000

    Fedora Updated
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@227 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/DRBrowser.java
+++ VUE2/src/tufts/vue/DRBrowser.java
@@ -4,6 +4,7 @@ import javax.swing.border.TitledBorder;
 import javax.swing.*;
 
 import osid.dr.*;
+import osid.OsidException;
 import tufts.dr.fedora.*;
 /**
  * Digital Repositor Browser
@@ -18,13 +19,21 @@ class DRBrowser extends javax.swing.JTabbedPane
         JInternalFrame fileBrowser = new JInternalFrame("File System");
         JScrollPane jSP = new JScrollPane(tree);
         SearchPanel searchPanel = new SearchPanel(400,600);
-      /**
+   
         DigitalRepository dr = new DR();
-        FedoraTree  fedoraTree= new FedoraTree(dr);
-        JScrollPane jspFedora  = new JScrollPane(fedoraTree);
-        **/
-        add("File", jSP);
-       // add("FEDORA",jspFedora);
+        
+        FedoraTree  fedoraTree = null;
+        try {
+            fedoraTree = new FedoraTree(dr);
+        } catch(OsidException e) {
+            JOptionPane.showMessageDialog(this,"Cannot connect to FEDORA Server.","FEDORA Alert", JOptionPane.ERROR_MESSAGE);
+          //  System.out.println(e);
+        }
+        if(fedoraTree != null) {
+            JScrollPane jspFedora  = new JScrollPane(fedoraTree);
+            add("FEDORA",jspFedora);
+        }
+        add("File", jSP);  
         add("Search", searchPanel);
         
     }
