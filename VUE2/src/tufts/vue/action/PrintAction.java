87e8c3c60b0fb9e3bceea6ec39b5f610098d0b74
daisuke daisuke <daisuke@tufts.edu>
2003-07-01 16:37:33 +0000

    no message
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@253 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/action/PrintAction.java
+++ VUE2/src/tufts/vue/action/PrintAction.java
@@ -16,51 +16,68 @@ import tufts.vue.*;
  *
  * @author  Daisuke Fujiwara
  */
+
+/**A class which handles the printing task of the currently active map*/
 public class PrintAction extends AbstractAction implements Printable {
     
     /** Creates a new instance of PrintAction */
     public PrintAction() {
+        super();
     }
     
     public PrintAction(String label){
         super(label);
         putValue(Action.SHORT_DESCRIPTION, label);
     }
+    
+    /**A method which actualy takes care of what to print*/
     public int print(java.awt.Graphics graphics, java.awt.print.PageFormat pageFormat, int param) throws java.awt.print.PrinterException {
         
+        //page number 1
         if (param == 0)
         {
+            //draws the map content using the given graphics object
             Graphics2D g2 = (Graphics2D)graphics;
+            
+            //border outline of the map
+            
             MapViewer currentMap = VUE.getActiveViewer();
             Dimension size = currentMap.getSize();
             
+            //translate it to the printable section of the paper
             g2.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
             g2.setClip(0, 0, size.width, size.height);
+             
             currentMap.paintComponent(g2);
-         
+          
             g2.setColor(Color.black);
-            g2.drawRect(0, 0, size.width - 1, size.height - 1);
+            //g2.drawRect(0, 0, size.width - 1, size.height - 1);
+            g2.drawRect(0, 0, (int)pageFormat.getImageableWidth() - 1, (int)pageFormat.getImageableHeight() - 1);
             
             return Printable.PAGE_EXISTS;
         }
         
+        //more than page 1 then don't print
+        //could change in the future
         else 
             return Printable.NO_SUCH_PAGE;
     }
     
+    /**Reacts to the action dispatched by the class*/
     public void actionPerformed(ActionEvent ae)
     {
         PrinterJob job = PrinterJob.getPrinterJob();
-     
-        job.setPrintable(this);
-        
+         
         try
         {
+            PageFormat format = job.pageDialog(job.defaultPage());
+            job.setPrintable(this, format);
+            //job.setPrintable(this);
+            
             if (job.printDialog()) {
                 try 
                 {
                     job.print();
-                    System.out.println("done printing");
                 } 
                 catch (Exception ex) 
                 {
