e092165989f20cd7fe913adc4967a3f2bcef8080
daisuke daisuke <daisuke@tufts.edu>
2003-07-30 16:11:41 +0000

    no message
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@345 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/action/PrintAction.java
+++ VUE2/src/tufts/vue/action/PrintAction.java
@@ -33,30 +33,62 @@ public class PrintAction extends AbstractAction implements Printable {
     /**A method which actualy takes care of what to print*/
     public int print(java.awt.Graphics graphics, java.awt.print.PageFormat pageFormat, int param) throws java.awt.print.PrinterException {
         
-        //page number 1
-        if (param == 0)
+        MapViewer currentMap = VUE.getActiveViewer();
+        Dimension size = currentMap.getSize();
+        int totalPages;
+        
+        //calcuates the total page number using the component size and the paper size
+        if (pageFormat.getOrientation() == PageFormat.PORTRAIT)
+          totalPages = (int)Math.ceil((size.getHeight() / pageFormat.getImageableHeight()));
+          
+        else
+          totalPages = (int)Math.ceil((size.getWidth() / pageFormat.getImageableWidth()));
+         
+        //if the page number is less than one, make it one
+        //maybe not necessary?
+        if (totalPages <= 1)
+            totalPages = 1;
+        
+        System.out.println("total page number is " + totalPages);
+        
+        //printing pages
+        if(param < totalPages)
         {
-            //draws the map content using the given graphics object
             Graphics2D g2 = (Graphics2D)graphics;
+            int xClip = 0, yClip = 0;
+            double xTranslation, yTranslation;
             
-            //border outline of the map
+            if (pageFormat.getOrientation() == PageFormat.LANDSCAPE)
+            {
+                xClip = (int)pageFormat.getImageableWidth() * param;
+                xTranslation = pageFormat.getImageableX() - xClip;
+                
+                System.out.println("xClip is " + xClip);
+                System.out.println("xTranslation is " + xTranslation);
+                
+                //translate it to the printable section of the paper
+                g2.translate(xTranslation, pageFormat.getImageableY());
+                g2.setClip(xClip, 0, (int)pageFormat.getImageableWidth() - 1, (int)pageFormat.getImageableHeight() - 1);
+            }
             
-            MapViewer currentMap = VUE.getActiveViewer();
-            Dimension size = currentMap.getSize();
+            else
+            {
+                //translate it to the printable section of the paper
+                g2.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
+                g2.setClip(0, 0, (int)pageFormat.getImageableWidth() - 1, (int)pageFormat.getImageableHeight() - 1);
+            }
             
-            //translate it to the printable section of the paper
-            g2.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
-            g2.setClip(0, 0, (int)pageFormat.getImageableWidth() - 1, (int)pageFormat.getImageableHeight() - 1);
+            //printing the currently selected map using the given graphics object
             currentMap.paintComponent(g2);
           
+            //border outline of the map
             g2.setColor(Color.black);
-            g2.drawRect(0, 0, (int)pageFormat.getImageableWidth() - 1, (int)pageFormat.getImageableHeight() - 1);
+            g2.drawRect(xClip, yClip, (int)pageFormat.getImageableWidth() - 1, (int)pageFormat.getImageableHeight() - 1);
             
             return Printable.PAGE_EXISTS;
         }
         
-        //more than page 1 then don't print
-        //could change in the future
+        //if it is an out of bound page number then the page doesn't exist
         else 
             return Printable.NO_SUCH_PAGE;
     }
