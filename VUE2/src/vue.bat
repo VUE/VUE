0823c0cd5a25ab2bf5373eb1ec9d0fcc45c2155d
Anoop Kumar <anoopkum@gmail.com>
2003-10-20 18:24:47 +0000

    no message
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@433 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/vue.bat
+++ VUE2/src/vue.bat
@@ -2,22 +2,18 @@
 @echo off
 REM Change the following two lines to set your JDK path and vue home path
 
-<<<<<<< vue.bat
-set JAVA_HOME=C:\forte_jdk\j2sdk1.4.0
-set VUE_HOME=C:\Daisuke\VUEDevelopment\src
-=======
+
 set JAVA_HOME=C:\j2sdk1.4.0_03
-<<<<<<< vue.bat
-set VUE_HOME=C:\vueproject\VUEDevelopment\src
-=======
-set VUE_HOME=C:\anoop\euclid\VUEDevelopment\src
->>>>>>> 1.4
->>>>>>> 1.5
+ 
+set VUE_HOME=C:\anoop\euclid\VUEDevelopment\src\
+set VUE_LIB=%VUE_HOME%\..\lib\
+  
 
 set JAVA=%JAVA_HOME%\bin\java
 set JAVAC=%JAVA_HOME%\bin\javac
 
 @echo Create the classpath
-set CP=%VUE_HOME%;%VUE_HOME%\..\lib\castor-0.9.4.3.jar;%VUE_HOME%\..\lib\castor-0.9.4.3-xml.jar;%VUE_HOME%\..\lib\batik-svggen.jar;%VUE_HOME%\..\lib\batik-awt-util.jar;%VUE_HOME%\tufts\dr;%VUE_HOME%\..\lib\batik-util.jar;%VUE_HOME%\..\lib\fop.jar;%VUE_HOME%\..\lib\osid.jar
+set CP=%VUE_HOME%;%VUE_LIB%castor-0.9.4.3.jar;%VUE_LIB%castor-0.9.4.3-xml.jar;%VUE_LIB%batik-svggen.jar;%VUE_LIB%batik-awt-util.jar;%VUE_HOME%tufts\dr;%VUE_LIB%batik-util.jar;%VUE_LIB%fop.jar;%VUE_LIB%osid.jar;%VUE_LIB%fedorautilities.jar;%VUE_LIB%fedoragentypes.jar;%VUE_LIB%axis.jar;%VUE_LIB%axis-ant.jar;%VUE_LIB%commons-discovery.jar;%VUE_LIB%commons-logging.jar;%VUE_LIB%jaxrpc.jar;%VUE_LIB%log4j-1.2.4.jar;%VUE_LIB%saaj.jar;%VUE_LIB%xerces-2_4_0/xercesImpl.jar;%VUE_LIB%xerces-2_4_0/xml-apis.jar;
+REM set CP=%VUE_HOME%;"VUE_LIB%vue-lib.zip;
  
-%JAVA% -cp %CP% tufts.vue.VUE
+%JAVA%  -cp %CP% tufts.vue.VUE
