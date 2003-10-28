70a58252f90425a42ea4a24614512446b0e3b8c7
Scott Fraize <scott.fraize@gmail.com>
2003-10-28 01:29:10 +0000

    in-progress work for generated shape icons
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@492 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/NodeTool.java
+++ VUE2/src/tufts/vue/NodeTool.java
@@ -42,10 +42,7 @@ public class NodeTool extends VueTool
         return null;
     }
 
-    public boolean supportsSelection()
-    {
-        return false;
-    }
+    public boolean supportsSelection() { return false; }
     
     /*
     public boolean supportsXORSelectorDrawing()
@@ -55,6 +52,9 @@ public class NodeTool extends VueTool
     */
     
     private java.awt.geom.RectangularShape currentShape = new tufts.vue.shape.RectangularPoly2D(4);
+    // todo: if we had a DrawContext here instead of just the graphics,
+    // we could query it for zoom factor (passed in from mapViewer) so the stroke width would
+    // look right.
     public void drawSelector(java.awt.Graphics2D g, java.awt.Rectangle r)
     {
         //g.setXORMode(java.awt.Color.blue);
@@ -66,7 +66,8 @@ public class NodeTool extends VueTool
         g.setRenderingHint(java.awt.RenderingHints.KEY_ANTIALIASING,  java.awt.RenderingHints.VALUE_ANTIALIAS_ON);
         g.fill(currentShape);
         g.setColor(COLOR_BORDER);
-        g.setStroke(STROKE_TWO);
+        g.setStroke(STROKE_TWO); // todo: scale based on the scale in the GC affine transform
+        //g.setStroke(new BasicStroke(2f * (float) g.getTransform().getScaleX())); // GC not scaled while drawing selector...
         g.draw(currentShape);
         /*
         if (VueUtil.isMacPlatform()) // Mac 1.4.1 handles XOR differently than PC
@@ -145,6 +146,94 @@ public class NodeTool extends VueTool
                     
         }
 
-        
+        //private static final Color bgColor = new Color(127,127,127);
+        //private static final Color bgColor = new Color(230,230,230);
+        //private static final Color fillColor = new Color(175,191,198);
+        private static final int ICON_DEFAULT = 0;
+        private static final int ICON_DOWN = 1;
+        private static final int ICON_SELECTED = 2;
+        private static final Color fillColor = new Color(165,178,208);
+        class ShapeIcon implements Icon
+        {
+            final int w = 38;
+            final int h = 27;
+            final int xInset = 7;
+            final int yInset = 6;
+            final int arc = 13;
+
+            int type = ICON_DEFAULT;
+
+            private Color bgColor = new Color(230,230,230);
+            //private Color bgColor = new Color(200,200,200);
+            //private Color bgColor = Color.gray;
+
+            protected ShapeIcon(int type)
+            {
+                this.type = type;
+            }
+            
+            protected ShapeIcon(Color c)
+            {
+                bgColor = c;
+            }
+            
+            public int getIconWidth() {return w; }
+            public int getIconHeight() { return h; }
+
+            public void paintIcon(Component c, Graphics g, int x, int y) {
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2.setColor(Color.red);
+                g2.fillRect(0,0, 99,99);
+                g2.translate(3,3);
+                float gw = w;
+                //GradientPaint gradient = new GradientPaint(gw/2,0,Color.white,gw/2,h/2,bgColor,true);
+                //GradientPaint gradient = new GradientPaint(gw/6,0,Color.white,gw/2,h/2,bgColor,true);
+                GradientPaint gradient = new GradientPaint(gw/6,0,Color.white,gw*.33f,h/2,bgColor,true);
+                g2.setPaint(gradient);                
+                //g2.setColor(bgColor);
+                g2.fillRoundRect(0,0, w-3,h-3, arc,arc);
+                g2.setColor(Color.black);
+                g2.drawRoundRect(0,0, w-3,h-3, arc,arc);
+                g2.setColor(fillColor);
+                RectangularShape shape = getShape();
+                if (shape instanceof RoundRectangle2D) {
+                    // hack to deal with arcs being too small on a tiny icon
+                    shape = getShapeInstance();
+                    ((RoundRectangle2D)shape).setRoundRect(xInset, yInset, 20,12, 7,7);
+                } else
+                    shape.setFrame(xInset,yInset, 20,12);
+                //shape.setFrame(xInset,yInset, w-xInset*2, h-yInset*2);                
+                g2.fill(shape);
+                g2.setColor(Color.black);
+                g2.draw(shape);
+            }
+        }
+
+        /*
+	public Icon getIcon() {
+            //return new ShapeIcon(Color.yellow);
+            return new ShapeIcon(ICON_DEFAULT);
+	}
+        // downIcon only drawn when the mouse is being held down over it
+	public Icon getDownIcon() {
+            //return new ShapeIcon(Color.red);
+            return getSelectedIcon();
+	}
+        // The selected "down" icon (depressed)
+	public Icon getSelectedIcon() {
+            //return new ShapeIcon(ICON_DEFAULT);
+            //return new ShapeIcon(new Color(200,200,200));
+            return new ShapeIcon(Color.lightGray);
+	}
+	public Icon getRolloverIcon() {
+            return new ShapeIcon(Color.white);
+	}
+	public Icon getDisabledIcon() {
+            return new ShapeIcon(Color.black);
+	}
+        */
+
     }
+    
 }
