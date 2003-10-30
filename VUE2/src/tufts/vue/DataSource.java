f611755001ac9eb7b1684c738594cf352965446c
Anoop Kumar <anoopkum@gmail.com>
2003-10-30 14:40:51 +0000

    datasource model
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@519 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/DataSource.java
+++ VUE2/src/tufts/vue/DataSource.java
@@ -13,12 +13,17 @@ package tufts.vue;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.*;
+import java.util.Vector;
+import java.io.*;
 
 
 public class DataSource {
-  
+    public static final int FAVORITES = 0;
+    public static final int FILING_LOCAL = 1;
+    public static final int FILING_REMOTE = 2;
+    public static final int DR_FEDORA = 3;
     private String id;
-    private String type;
+    private int type;
     private String displayName; 
     private String name;  
     private String address;   
@@ -38,6 +43,26 @@ public class DataSource {
         this.name = name;
         resourceViewer = new JLabel(displayName+" : No Viewer Available");
     }
+    
+    public DataSource(String id,String displayName,String name,int type){
+        this.id = id;
+        this.displayName = displayName;
+        this.name = name;
+        if(type == FAVORITES) {
+            setResourceViewer(new FavoritesWindow(displayName));
+        }else if(type == FILING_LOCAL) {
+            Vector fileVector  = new Vector();
+            fileVector.add(new File("C:\\"));
+            VueDragTree fileTree = new VueDragTree(fileVector.iterator(),displayName);
+            JScrollPane jSP = new JScrollPane(fileTree);   
+            setResourceViewer(jSP);
+        }else if(type== DR_FEDORA) {
+            setResourceViewer(new DRViewer("fedora.conf", id,displayName,displayName));
+        } else {
+            setResourceViewer(new JLabel(displayName+" : No Viewer Available"));
+        }
+    }
+    
     public void setDisplayName(String displayName) {
         this.displayName = displayName;
     }
@@ -53,6 +78,14 @@ public class DataSource {
     public void setResourceViewer(JComponent resourceViewer) {
       
         this.resourceViewer = resourceViewer;
+    }
+    
+    public int getType() {
+        return this.type;
+    }
+    
+    public void setType(int type){
+        this.type = type;
     }
     
     public JComponent getResourceViewer() {
