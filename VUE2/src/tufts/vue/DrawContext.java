5966722ca19af05264723996e170f0314721bd59
Scott Fraize <scott.fraize@gmail.com>
2003-11-17 18:40:59 +0000

    added helper functions
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@616 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/DrawContext.java
+++ VUE2/src/tufts/vue/DrawContext.java
@@ -2,10 +2,13 @@ package tufts.vue;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
 public class DrawContext
 {
     Graphics2D g;
     double zoom = 1.0;
+    private boolean disableAntiAlias = false;
     
     public DrawContext(Graphics2D g, double zoom)
     {
@@ -23,6 +26,46 @@ public class DrawContext
         this.zoom = dc.zoom;
         //this.scale = dc.scale;
     }
+
+    public void disableAntiAlias(boolean tv)
+    {
+        this.disableAntiAlias = tv;
+        if (tv)
+            setAntiAlias(false);
+    }
+        
+    public void setAntiAlias(boolean on)
+    {
+        if (disableAntiAlias)
+            on = false;
+        
+        Object shapeVal = on ? RenderingHints.VALUE_ANTIALIAS_ON : RenderingHints.VALUE_ANTIALIAS_OFF;
+        Object textVal = on ? RenderingHints.VALUE_TEXT_ANTIALIAS_ON : RenderingHints.VALUE_TEXT_ANTIALIAS_OFF;
+        this.g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, shapeVal);
+        this.g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, textVal);
+    }
+
+    public void setPrioritizeQuality(boolean on)
+    {
+        this.g.setRenderingHint(RenderingHints.KEY_RENDERING, on
+                                ? RenderingHints.VALUE_RENDER_QUALITY
+                                : RenderingHints.VALUE_RENDER_SPEED);
+    }
+
+    public void setPrioritizeSpeed(boolean on)
+    {
+        setPrioritizeQuality(!on);
+    }
+    
+    public void setFractionalFontMetrics(boolean on)
+    {
+        this.g.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, on
+                                ? RenderingHints.VALUE_FRACTIONALMETRICS_ON
+                                : RenderingHints.VALUE_FRACTIONALMETRICS_OFF);
+    }
+
+
+    
 
     /*
     public DrawContext createScaled(float scale)
