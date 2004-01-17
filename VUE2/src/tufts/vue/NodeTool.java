f7a03d25d7bb64e2633fec71ffa5e6279fce2f8e
Scott Fraize <scott.fraize@gmail.com>
2004-01-17 04:14:04 +0000

    removed static initializer, lots of work for generated icons tho not visible to build yet.
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@894 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/NodeTool.java
+++ VUE2/src/tufts/vue/NodeTool.java
@@ -12,7 +12,7 @@ import javax.swing.*;
 import tufts.vue.beans.VueBeanState;
 
 public class NodeTool extends VueTool
-                              implements VueConstants
+    implements VueConstants
 {
 	///////////
 	// Fields
@@ -21,7 +21,7 @@ public class NodeTool extends VueTool
     private static NodeTool singleton = null;
     
     /** the contextual tool panel **/
-    static private NodeToolPanel sNodeToolPanel = new NodeToolPanel();
+    private static NodeToolPanel sNodeToolPanel;
     
     
     public NodeTool()
@@ -41,31 +41,18 @@ public class NodeTool extends VueTool
         return singleton;
     }
     
-    public boolean handleKeyPressed(java.awt.event.KeyEvent e)  {
-        return false;
-    }
-    
-    public void handleSelection() {
-        
+    private static NodeToolPanel getNodeToolPanel()
+    {
+        if (sNodeToolPanel == null)
+            sNodeToolPanel = new NodeToolPanel();
+        return sNodeToolPanel;
     }
     
     public JPanel getContextualPanel() {
-        if( sNodeToolPanel == null) {
-        	
-        	sNodeToolPanel = new NodeToolPanel();
-        	}
-        	
-        return sNodeToolPanel;
+        return getNodeToolPanel();
     }
 
     public boolean supportsSelection() { return true; }
-    
-    /*
-    public boolean supportsXORSelectorDrawing()
-    {
-        return false;
-    }
-    */
     
     private java.awt.geom.RectangularShape currentShape = new tufts.vue.shape.RectangularPoly2D(4);
     // todo: if we had a DrawContext here instead of just the graphics,
@@ -119,7 +106,7 @@ public class NodeTool extends VueTool
     public static LWNode createNode(String name)
     {
         LWNode node = new LWNode(name, getActiveSubTool().getShapeInstance());
-        VueBeanState state = sNodeToolPanel.getValue();
+        VueBeanState state = getNodeToolPanel().getValue();
         if( state != null) {
         	state.applyState( node);
         	}
@@ -141,10 +128,10 @@ public class NodeTool extends VueTool
         node.setFillColor(COLOR_TRANSPARENT);
         node.setFont(FONT_TEXT_DEFAULT);
         
-        VueBeanState state = sNodeToolPanel.getValue();
-        if( state != null) {
-        	state.applyState( node);
-        	}
+        VueBeanState state = getNodeToolPanel().getValue();
+        if (state != null) {
+            state.applyState( node);
+        }
         
         return node;
     }
@@ -161,11 +148,16 @@ public class NodeTool extends VueTool
             
         public SubTool() {}
 
+	public void X_setID(String pID) {
+            super.setID(pID);
+            System.out.println(this + " ID set");
+        }
+
         public RectangularShape getShapeInstance()
         {
             if (shapeClass == null) {
                 String shapeClassName = getAttribute("shapeClass");
-                //System.out.println(this + " got shapeClass " + shapeClassName);
+                System.out.println(this + " got shapeClass " + shapeClassName);
                 try {
                     this.shapeClass = getClass().getClassLoader().loadClass(shapeClassName);
                 } catch (Exception e) {
@@ -196,29 +188,43 @@ public class NodeTool extends VueTool
         private static final int ICON_DEFAULT = 0;
         private static final int ICON_DOWN = 1;
         private static final int ICON_SELECTED = 2;
-        private static final Color fillColor = new Color(165,178,208);
+
+        //private static final Color sShapeColor = new Color(255,178,208); // diagnostic color
+        private static final Color sShapeColor = new Color(165,178,208); // melanie's steel blue
+        private static final Color sButtonColor = new Color(222,222,222);
+        private static final Color sOverColor = new Color(244,244,244);
+        private static final Color sDownColor = new Color(211,211,211);
+        
         class ShapeIcon implements Icon
         {
             final int w = 38;
             final int h = 27;
             final int xInset = 7;
             final int yInset = 6;
-            final int arc = 13;
+            final int arc = 15; // arc of rounded toolbar button border
 
             int type = ICON_DEFAULT;
 
-            private Color bgColor = new Color(230,230,230);
-            //private Color bgColor = new Color(200,200,200);
-            //private Color bgColor = Color.gray;
+            private Color mColor = new Color(230,230,230);
+            //private Color mColor = new Color(200,200,200);
+            //private Color mColor = Color.gray;
+
+            private boolean mIsDown = false;
+            private boolean mDrawButton = false;
 
             protected ShapeIcon(int type)
             {
                 this.type = type;
             }
             
-            protected ShapeIcon(Color c)
-            {
-                bgColor = c;
+            protected ShapeIcon(Color c, boolean drawButton, boolean down) {
+                mColor = c;
+                mDrawButton = drawButton; // else: draw rounded border
+                mIsDown = down;
+            }
+            
+            protected ShapeIcon(Color c) {
+                this(c, false, false);
             }
             
             public int getIconWidth() {return w; }
@@ -227,24 +233,36 @@ public class NodeTool extends VueTool
             public void paintIcon(Component c, Graphics g, int x, int y) {
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                g2.setColor(Color.red);
-                g2.fillRect(0,0, 99,99);
-                g2.translate(3,3);
+                //g2.setColor(Color.red);
+                //g2.fillRect(0,0, 99,99);
+                if (VueUtil.isMacPlatform())
+                    g2.translate(3,3);
+                else
+                    g2.translate(1,1);
                 float gw = w;
-                //GradientPaint gradient = new GradientPaint(gw/2,0,Color.white,gw/2,h/2,bgColor,true);
-                //GradientPaint gradient = new GradientPaint(gw/6,0,Color.white,gw/2,h/2,bgColor,true);
-                GradientPaint gradient = new GradientPaint(gw/6,0,Color.white,gw*.33f,h/2,bgColor,true);
-                g2.setPaint(gradient);                
-                //g2.setColor(bgColor);
-                g2.fillRoundRect(0,0, w-3,h-3, arc,arc);
-                g2.setColor(Color.black);
-                g2.drawRoundRect(0,0, w-3,h-3, arc,arc);
-                g2.setColor(fillColor);
+                //GradientPaint gradient = new GradientPaint(gw/2,0,Color.white,gw/2,h/2,mColor,true);
+                //GradientPaint gradient = new GradientPaint(gw/6,0,Color.white,gw/2,h/2,mColor,true);
+                GradientPaint gradient = new GradientPaint(gw/6,0,Color.white,gw*.33f,h/2,mColor,true);
+                // Set gradient for the whole button.
+                //g2.setPaint(gradient);
+                g2.setColor(mColor);
+                if (mDrawButton) {
+                    g2.fill3DRect(0,0, w-1,h-1, true);
+                    //g2.drawRect(0,0, w-3,h-3);
+                } else {
+                    
+                    g2.fillRoundRect(0,0, w-3,h-3, arc,arc);
+                    g2.setColor(Color.black);
+                    g2.drawRoundRect(0,0, w-3,h-3, arc,arc);
+                }
+                
+                g2.setColor(sShapeColor);
                 RectangularShape shape = getShape();
                 if (shape instanceof RoundRectangle2D) {
                     // hack to deal with arcs being too small on a tiny icon
                     shape = getShapeInstance();
-                    ((RoundRectangle2D)shape).setRoundRect(xInset, yInset, 20,12, 7,7);
+                    // plus 2 on x/y inset for mac?
+                    ((RoundRectangle2D)shape).setRoundRect(xInset, yInset, 20,12, 8,8);
                 } else
                     shape.setFrame(xInset,yInset, 20,12);
                 //shape.setFrame(xInset,yInset, w-xInset*2, h-yInset*2);                
@@ -254,30 +272,67 @@ public class NodeTool extends VueTool
             }
         }
 
+        private boolean debug = false;
         /*
-	public Icon getIcon() {
-            //return new ShapeIcon(Color.yellow);
-            return new ShapeIcon(ICON_DEFAULT);
-	}
-        // downIcon only drawn when the mouse is being held down over it
-	public Icon getDownIcon() {
-            //return new ShapeIcon(Color.red);
-            return getSelectedIcon();
-	}
-        // The selected "down" icon (depressed)
-	public Icon getSelectedIcon() {
+        
+        // TOOLBAR: Unselected/default
+        public Icon getIcon() {
             //return new ShapeIcon(ICON_DEFAULT);
-            //return new ShapeIcon(new Color(200,200,200));
-            return new ShapeIcon(Color.lightGray);
+            if (debug) return new ShapeIcon(Color.magenta);
+            return new ShapeIcon(sButtonColor);
 	}
-	public Icon getRolloverIcon() {
-            return new ShapeIcon(Color.white);
+        // TOOLBAR: Rollover
+        public Icon getRolloverIcon() {
+            if (debug) return new ShapeIcon(Color.yellow);
+            return new ShapeIcon(sOverColor);
 	}
+        // TOOLBAR: flashes briefly after sub-menu item selected
+        // or hen the mouse is being held down over it, but only
+        // AFTER the sub-menu was displayed, then you hold mouse
+        // down, and the sub-menu goes away, and you get this icon.
+        // Rarely seen, so maybe don't even bother with anything different.
+	public Icon getDownIcon() {
+            return new ShapeIcon(Color.red, false, true);
+            //return getSelectedIcon();
+
+        }
+        // TOOLBAR: Selected (down)
+	public Icon getSelectedIcon() {
+            if (debug) return new ShapeIcon(Color.orange);
+            return new ShapeIcon(sDownColor, false, true);
+	}
+
+        // SUB-MENU: default display
+	public Icon getMenuItemIcon() {
+            //return mMenuItemIcon;
+            return new ShapeIcon(sButtonColor, true, false);
+	}
+        
+        // SUB-MENU: Rollver (or down-simulation)
+	public Icon getMenuItemSelectedIcon() {
+            //return mMenuItemSelectedIcon;
+            if (debug) return new ShapeIcon(Color.green, true, true);
+            return new ShapeIcon(sOverColor, true, true);
+	}
+	
 	public Icon getDisabledIcon() {
             return new ShapeIcon(Color.black);
 	}
-        */
 
+
+        // The corner icon
+	public Icon getOverlayUpIcon() {
+            return mOverlayUpIcon;
+	}
+	public Icon getOverlayDownIcon() {
+            return getOverlayUpIcon();
+            // down-overlay out of position?
+            // Oh, for down, whole icon supposed to
+            // move down and to the right...
+            //return mOverlayDownIcon;
+	}
+
+        */
     }
     
 }
