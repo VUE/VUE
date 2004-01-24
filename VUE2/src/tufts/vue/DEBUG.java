46d8a2fcdb8d906b0dae09d3ee80fa01570f8b8a
Scott Fraize <scott.fraize@gmail.com>
2004-01-24 00:21:29 +0000

    added DEBUG.java
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@968 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/tufts/vue/DEBUG.java
@@ -0,0 +1,14 @@
+package tufts.vue;
+
+class DEBUG
+{
+    // Can leave these as static for runtime tweaking, or
+    // make final to have compiler strip them out entirely.
+    static boolean CONTAINMENT = false;
+    static boolean PARENTING = false;
+    static boolean LAYOUT = false;
+    static boolean BOXES = false;
+    static boolean ROLLOVER = false;
+    static boolean EVENTS = false;
+    static boolean SCROLL = false;
+}
