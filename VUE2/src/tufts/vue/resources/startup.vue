9400b8a5cfd9adbbbc18b489c654fe2f62d36f8c
Scott Fraize <scott.fraize@gmail.com>
2004-02-06 18:43:46 +0000

    pathched
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@1145 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/resources/startup.vue
+++ VUE2/src/tufts/vue/resources/startup.vue
@@ -1,2 +1,260 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<LW-MAP ID="0" label="New Map 1" x="0.0" y="0.0" width="0.0" height="0.0" strokeWidth="0.0"><fillColor>#ffffff</fillColor><font>SansSerif-plain-14</font><userZoom>1.0</userZoom><userOrigin x="0.0" y="0.0"/><PathwayList currentPathway="-1"/></LW-MAP>
+<LW-MAP ID="0" label="startup.vue" x="36.0" y="24.0" width="726.0"
+    height="474.5" strokeWidth="0.0">
+    <fillColor>#ffffff</fillColor>
+    <font>SansSerif-plain-18</font>
+    <child ID="3" x="129.89722" y="175.0" width="98.97212"
+        height="115.0" strokeWidth="1.0" controlCount="0" arrowState="0"
+        xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <point1 x="129.89722" y="290.0"/>
+        <point2 x="228.86934" y="175.0"/>
+        <ID1>1</ID1>
+        <ID2>2</ID2>
+    </child>
+    <child ID="5" x="147.42307" y="234.0" width="133.53848"
+        height="56.0" strokeWidth="1.0" controlCount="0" arrowState="0"
+        xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <point1 x="147.42307" y="290.0"/>
+        <point2 x="280.96155" y="234.0"/>
+        <ID1>1</ID1>
+        <ID2>4</ID2>
+    </child>
+    <child ID="7" x="188.30048" y="287.65225" width="122.199524"
+        height="8.882874" strokeWidth="1.0" controlCount="0"
+        arrowState="0" xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <point1 x="188.30048" y="296.53513"/>
+        <point2 x="310.5" y="287.65225"/>
+        <ID1>1</ID1>
+        <ID2>6</ID2>
+    </child>
+    <child ID="9" x="139.93988" y="313.0" width="152.58372"
+        height="88.0" strokeWidth="1.0" controlCount="0" arrowState="0"
+        xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <point1 x="139.9399" y="313.0"/>
+        <point2 x="292.52362" y="401.0"/>
+        <ID1>1</ID1>
+        <ID2>8</ID2>
+    </child>
+    <child ID="26" x="175.06061" y="313.0" width="136.9243"
+        height="28.598114" strokeWidth="1.0" controlCount="0"
+        arrowState="0" xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <point1 x="175.06062" y="313.0"/>
+        <point2 x="311.98492" y="341.5981"/>
+        <ID1>1</ID1>
+        <ID2>25</ID2>
+    </child>
+    <child ID="1" label="VUE Web Site" x="51.0" y="290.0" width="138.0"
+        height="23.0" strokeWidth="1.0" autoSized="true" xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <resource referenceCreated="1076092887726" accessAttempted="0"
+            accessSuccessful="0" size="-1" xsi:type="map-resource">
+            <spec>http://vue.tccs.tufts.edu</spec>
+            <type>0</type>
+        </resource>
+        <fillColor>#d5e3f8</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-bold-14</font>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="2" label="About VUE%nl;(Home)" x="192.5" y="141.0"
+        width="102.0" height="34.0" strokeWidth="1.0" autoSized="true"
+        xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <notes>Background information about VUE.</notes>
+        <resource referenceCreated="1076092887796" accessAttempted="0"
+            accessSuccessful="0" size="-1" xsi:type="map-resource">
+            <spec>http://vue.tccs.tufts.edu/about/</spec>
+            <type>0</type>
+        </resource>
+        <fillColor>#a9ce97</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="4" label="Download%nl;VUE software" x="262.5" y="200.0"
+        width="118.0" height="34.0" strokeWidth="1.0" autoSized="true"
+        xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <notes>Download the latest version of VUE.</notes>
+        <resource referenceCreated="1076092887855" accessAttempted="0"
+            accessSuccessful="0" size="-1" xsi:type="map-resource">
+            <spec>http://vue.tccs.tufts.edu/download/</spec>
+            <type>0</type>
+        </resource>
+        <fillColor>#a9ce97</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="6" label="User%nl;Documentation" x="310.5" y="266.0"
+        width="128.0" height="34.0" strokeWidth="1.0" autoSized="true"
+        xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <notes>Online user documentation for VUE.</notes>
+        <resource referenceCreated="1076092887904" accessAttempted="0"
+            accessSuccessful="0" size="-1" xsi:type="map-resource">
+            <spec>http:/vue.tccs.tufts.edu/userdoc/</spec>
+            <type>0</type>
+        </resource>
+        <fillColor>#d7f1ca</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="8" label="User Forums" x="263.5" y="401.0" width="117.0"
+        height="34.0" strokeWidth="1.0" autoSized="true" xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <notes>Post questions, bug reports and comments to the online forums.</notes>
+        <resource referenceCreated="1076092887938" accessAttempted="0"
+            accessSuccessful="0" size="-1" xsi:type="map-resource">
+            <spec>http://vue.tccs.tufts.edu/forums/</spec>
+            <type>0</type>
+        </resource>
+        <fillColor>#a9ce97</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="14" label="VUE%nl;Resources" x="191.5" y="464.0"
+        width="105.0" height="34.0" strokeWidth="1.0" autoSized="true"
+        xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <notes>Download sample maps and link to related Web resource.</notes>
+        <resource referenceCreated="1076092887998" accessAttempted="0"
+            accessSuccessful="0" size="-1" xsi:type="map-resource">
+            <spec>http://vue.tccs.tufts.edu/resources/</spec>
+            <type>0</type>
+        </resource>
+        <fillColor>#a9ce97</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="25" label="My Maps" x="311.5" y="334.0" width="91.0"
+        height="34.0" strokeWidth="1.0" autoSized="true" xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <notes>Upload and manage your VUE maps.</notes>
+        <resource referenceCreated="1076092888430" accessAttempted="0"
+            accessSuccessful="0" size="-1" xsi:type="map-resource">
+            <spec>http://vue.tccs.tufts.edu/mymaps</spec>
+            <type>0</type>
+        </resource>
+        <fillColor>#a9ce97</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="27" label="Creating maps" x="525.5" y="218.0"
+        width="126.0" height="22.0" strokeWidth="1.0" autoSized="true"
+        xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <resource referenceCreated="1076092888488" accessAttempted="0"
+            accessSuccessful="0" size="-1" xsi:type="map-resource">
+            <spec>http:/vue.tccs.tufts.edu/userdoc/maps.cfm</spec>
+            <type>0</type>
+        </resource>
+        <fillColor>#fcfabb</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="29" x="434.1255" y="240.0" width="110.78192"
+        height="27.954315" strokeWidth="1.0" controlCount="0"
+        arrowState="0" xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <point1 x="434.1255" y="267.9543"/>
+        <point2 x="544.9074" y="240.0"/>
+        <ID1>6</ID1>
+        <ID2>27</ID2>
+    </child>
+    <child ID="30" label="Finding and %nl;linking resources." x="526.5"
+        y="266.0" width="142.0" height="34.0" strokeWidth="1.0"
+        autoSized="true" xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <resource referenceCreated="1076092888548" accessAttempted="0"
+            accessSuccessful="0" size="-1" xsi:type="map-resource">
+            <spec>http:/vue.tccs.tufts.edu/userdoc/resources.cfm</spec>
+            <type>0</type>
+        </resource>
+        <fillColor>#fcfabb</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="31" x="438.5" y="283.0" width="88.0" height="0.0"
+        strokeWidth="1.0" controlCount="0" arrowState="0"
+        xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <point1 x="438.5" y="283.0"/>
+        <point2 x="526.5" y="283.0"/>
+        <ID1>6</ID1>
+        <ID2>30</ID2>
+    </child>
+    <child ID="32" label="Creating and %nl;Viewing Pathways" x="525.5"
+        y="313.0" width="143.0" height="34.0" strokeWidth="1.0"
+        autoSized="true" xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <resource referenceCreated="1076092888593" accessAttempted="0"
+            accessSuccessful="0" size="-1" xsi:type="map-resource">
+            <spec>http:/vue.tccs.tufts.edu/userdoc/pathways.cfm</spec>
+            <type>0</type>
+        </resource>
+        <fillColor>#fcfabb</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="33" x="436.26196" y="296.0464" width="92.634094"
+        height="19.567657" strokeWidth="1.0" controlCount="0"
+        arrowState="0" xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <point1 x="436.26196" y="296.04636"/>
+        <point2 x="528.89606" y="315.614"/>
+        <ID1>6</ID1>
+        <ID2>32</ID2>
+    </child>
+    <child ID="37" label="Welcome to VUE" x="36.0" y="24.0"
+        width="166.0" height="27.0" strokeWidth="0.0" autoSized="true"
+        xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <fillColor>#ffffff</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Verdana-bold-16</font>
+        <shape x="36" y="24" width="166" height="27" xsi:type="rectangle"/>
+    </child>
+    <child ID="38"
+        label="This is an example of one of the many kinds of maps that you can create using the VUE software.  "
+        x="36.0" y="52.0" width="640.0" height="23.0" strokeWidth="0.0"
+        autoSized="true" xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <fillColor>#ffffff</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <textColor>#2d4322</textColor>
+        <font>Arial-plain-14</font>
+        <shape x="36" y="52" width="640" height="23" xsi:type="rectangle"/>
+    </child>
+    <child ID="39"
+        label="Each node on this map links directly to a page on the VUE Web site where you can find related VUE information."
+        x="36.0" y="72.0" width="726.0" height="23.0" strokeWidth="0.0"
+        autoSized="true" xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <fillColor>#ffffff</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <textColor>#2d4322</textColor>
+        <font>Arial-plain-14</font>
+        <shape x="36" y="72" width="726" height="23" xsi:type="rectangle"/>
+    </child>
+    <child ID="40" x="127.94429" y="313.0" width="104.31199"
+        height="151.0" strokeWidth="1.0" controlCount="0" arrowState="0"
+        xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <point1 x="232.25627" y="464.0"/>
+        <point2 x="127.94428" y="313.0"/>
+        <ID1>14</ID1>
+        <ID2>1</ID2>
+    </child>
+    <userZoom>1.0</userZoom>
+    <userOrigin x="34.283356" y="25.2583"/>
+    <PathwayList currentPathway="-1"/>
+</LW-MAP>
