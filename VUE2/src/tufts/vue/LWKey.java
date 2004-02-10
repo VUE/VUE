f15f5f53b98f82fbc675cabc6d7d04b7143ce22a
Scott Fraize <scott.fraize@gmail.com>
2004-02-10 23:31:17 +0000

    added LWKey
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@1199 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/tufts/vue/LWKey.java
@@ -0,0 +1,36 @@
+package tufts.vue;
+
+/** Some pre-defined property types.  Any string may be used as an
+ * property or event identifier, but you must be sure to use the constant
+ * object here for any of these events or they may not be
+ * recognized */
+public interface LWKey {
+
+    public String Location = "location"; 
+    public String Size = "size";
+    
+    public String Label = "label"; 
+    public String Notes = "notes"; 
+    public String Scale = "scale"; 
+    public String Resource = "resource"; 
+    public String FillColor = "fillColor"; 
+    public String TextColor = "textColor"; 
+    public String StrokeColor = "strokeColor"; 
+    public String StrokeWidth = "strokeWidth"; 
+    public String Font = "font";
+
+    public String Added = "added"; // a child components add-notify
+    //public String ChildAdded = "childAdded";// the parent component's add-notify
+    public String ChildrenAdded = "childrenAdded";// the parent component's group add-notify
+    public String ChildRemoved = "childRemoved";// the parent component's remove-notify
+    public String ChildrenRemoved = "childrenRemoved";// the parent component's group remove-notify
+
+    public String Deleting = ":deleting"; // the component's just-before-delete notify
+    public String Deleted = "deleted"; // the component's after-delete notify
+
+    public String Repaint = "repaint"; // general: visual change but no permanent data change
+
+    public String LinkArrows = "linkArrows"; // link arrow state (0,1,2)
+    
+    
+}
