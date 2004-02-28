4fa167452a53fe2f9a18b4999d127f3b2e62e8d9
Anoop Kumar <anoopkum@gmail.com>
2004-02-28 23:16:42 +0000

    no message
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@1331 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/tufts/vue/filter/BooleanType.java
@@ -0,0 +1,42 @@
+/*
+ * BooleanType.java
+ *
+ * Created on February 28, 2004, 5:59 PM
+ */
+
+package tufts.vue.filter;
+
+/**
+ *
+ * @author  akumar03
+ */
+public class BooleanType extends DefaultType {
+    
+    public static String OP_EQUAL = "equals";
+    public static String OP_NOT_EQUAL = "not equals";
+    /** Creates a new instance of BooleanType */
+    public BooleanType() {
+        super(Type.BOOLEAN_TYPE);
+        Operator eqOperator = new DefaultOperator(OP_EQUAL ,"is",true) ;
+        Operator neOperator = new DefaultOperator(OP_NOT_EQUAL,"is not",false);
+        operatorList.add(eqOperator);
+        operatorList.add(neOperator);
+    }
+    public boolean compare(Statement s1,Statement s2) {
+        Object value1  = s1.getValue();
+        Object value2 = s2.getValue();
+        if(!(value1 instanceof String) || !(value2 instanceof String))
+            return false;
+        boolean v1 = ((Boolean)value1).booleanValue();
+        boolean v2 = ((Boolean)value2).booleanValue();
+        if(s1.getOperator().getDisplayName().equals(OP_EQUAL)){
+            if(s2.getOperator().getDisplayName().equals(OP_EQUAL)) {
+                return v1 && v2;
+            } else if (s2.getOperator().getDisplayName().equals(OP_NOT_EQUAL)) {
+                return v1 && !v2;
+            }
+        }
+        throw new RuntimeException("Not Implemented");
+    }
+    
+}
