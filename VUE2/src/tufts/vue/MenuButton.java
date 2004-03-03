b2a77148703db7065e98ee95a782ecf606ee6798
Scott Fraize <scott.fraize@gmail.com>
2004-03-03 04:42:16 +0000

    new
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@1383 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/tufts/vue/MenuButton.java
@@ -0,0 +1,167 @@
+package tufts.vue;
+
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.beans.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+/**
+ * MenuButton
+ *
+ * @author Scott Fraize
+ * @version March 2004
+ *
+ */
+
+public abstract class MenuButton extends JButton //implements ActionListener
+// todo: cleaner to get this to subclass from JMenu, and then cross-menu drag-rollover
+// menu-popups would automatically work also.
+{
+    protected String mPropertyName;
+    protected JPopupMenu mPopup;
+
+    public MenuButton()
+    {
+        //setIcon(sIcon);
+        //mPopup = getPopupMenu();
+        //buildMenu(pColors, pMenuNames, pHasCustom);
+        //setBorder(new CompoundBorder(BorderFactory.createRaisedBevelBorder(), new EmptyBorder(3,3,3,3)));
+        //setBorder(new CompoundBorder(BorderFactory.createEtchedBorder(), new LineBorder(Color.blue, 6)));
+        //setBorder(BorderFactory.createRaisedBevelBorder());
+        setBorder(BorderFactory.createEtchedBorder());
+        setFont(VueConstants.FONT_ICONIC);
+        setFocusable(false);
+        setContentAreaFilled(false);
+        setText("v ");
+        addMouseListener(new MouseAdapter() {
+                public void mousePressed(MouseEvent e) {
+                    Component c = e.getComponent(); 	
+                    getPopupMenu().show(c, 0, (int) c.getBounds().getHeight());
+                }
+            });
+    }
+	
+    protected JPopupMenu getPopupMenu() {
+        return mPopup;
+    }
+    
+    public void setPropertyName(String pName) {
+        mPropertyName = pName;
+    }
+
+    public String getPropertyName() {
+        return mPropertyName;
+    }
+
+    public abstract void setPropertyValue(Object propertyValue);
+    public abstract Object getPropertyValue();
+	
+    protected void handleMenuSelection(Object propertyValue) {
+        Object oldValue = getPropertyValue();
+        setPropertyValue(propertyValue);
+        firePropertyChanged(oldValue, propertyValue);
+        repaint();
+    }
+	
+    protected void firePropertyChanged(Object oldValue, Object newValue)
+    {
+        PropertyChangeListener [] listeners = getPropertyChangeListeners();
+        if (listeners != null) {
+            PropertyChangeEvent event = new PropertyChangeEvent( this, mPropertyName, oldValue, newValue);
+            for (int i=0; i< listeners.length; i++) {
+                listeners[i].propertyChange(event);
+            }
+        }
+    }
+	
+    public void paint(java.awt.Graphics g) {
+        ((java.awt.Graphics2D)g).setRenderingHint
+            (java.awt.RenderingHints.KEY_TEXT_ANTIALIASING,
+             java.awt.RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        super.paint(g);
+    }
+	
+    /* -- why was this here?
+     * actionPerformed( ActionEvent pEvent)
+     * This method handles remote or direct  selection of a MenuButtonItem
+     *
+     * It will update its own icons based on the selected item
+     *
+     * @param pEvent the action event.
+     **/
+    public void X_actionPerformed( ActionEvent pEvent) {
+        // fake a click to handle radio selection after menu selection
+        doClick();		
+    }
+}
+
+
+
+	
+    /*
+     * paint( Graphics g)
+     * Overrides paint method and renders an additional icon ontop of
+     * of the normal rendering to indicate if this button contains
+     * a popup handler.
+     *
+     * param Graphics g the Graphics.
+     **/
+    /*
+
+    // default offsets for drawing popup arrow via code
+    public int mArrowSize = 3;
+    public int mArrowHOffset  = -9;
+    public int mArrowVOffset = -7;
+	
+    // the popup overlay icons for up and down states
+    public Icon mPopupIndicatorIconUp = null;
+    public Icon mPopupIndicatorDownIcon = null;
+
+    
+    public void paint( java.awt.Graphics pGraphics) {
+        super.paint( pGraphics);
+        if (true) return;
+        Dimension dim = getPreferredSize();
+        Insets insets = getInsets();
+		
+        // now overlay the popup menu icon indicator
+        // either from an icon or by brute painting
+        if(  (! false ) 
+             &&  (mPopup != null) 
+             && ( !mPopup.isVisible() ) ) {
+            // draw popup arrow
+            Color saveColor = pGraphics.getColor();
+            pGraphics.setColor( Color.black);
+			
+            int w = getWidth();
+            int h = getHeight();
+			
+            int x1 = w + mArrowHOffset;
+            int y = h + mArrowVOffset;
+            int x2 = x1 + (mArrowSize * 2) -1;
+			
+            for(int i=0; i< mArrowSize; i++) { 
+                pGraphics.drawLine(x1,y,x2,y);
+                x1++;
+                x2--;
+                y++;
+            }
+            pGraphics.setColor( saveColor);
+        }
+        else  // Use provided popup overlay  icons
+            if(   (mPopup != null) && ( !mPopup.isVisible() ) ) {
+			
+                Icon overlay = null;
+                if( isSelected() ) {
+				
+                    overlay = mPopupIndicatorDownIcon;
+                }
+                if( overlay != null) {
+                    overlay.paintIcon( this, pGraphics, insets.top, insets.left);
+                }
+            }
+    }
+    */
