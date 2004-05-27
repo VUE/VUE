7281526502882cef86b2d3dea5fd8856a9356662
Scott Fraize <scott.fraize@gmail.com>
2004-05-27 12:49:10 +0000

    added
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@1701 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/build.sh
@@ -0,0 +1,10 @@
+set -x
+javac -d /VUE/classes tufts/google/*.java
+javac -d /VUE/classes tufts/oki/shared/*.java
+javac -d /VUE/classes tufts/oki/authentication/*.java
+javac -d /VUE/classes tufts/oki/authorization/*.java
+javac -d /VUE/classes tufts/oki/hierarchy/*.java
+javac -d /VUE/classes tufts/oki/localFiling/*.java
+javac -d /VUE/classes tufts/oki/removeFiling/*.java
+javac -d /VUE/classes tufts/oki/dr/fedora/*.java
+javac -d /VUE/classes tufts/vue/action/*.java tufts/vue/gui/*.java tufts/vue/filter/*.java tufts/vue/beans/*.java tufts/vue/shape/*.java tufts/vue/*.java
