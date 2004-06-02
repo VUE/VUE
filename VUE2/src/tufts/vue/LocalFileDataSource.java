6104c56a6ee2176ee7031dd6778ba7bb97c2b50c
rsaigal rsaigal <rsaigal@tufts.edu>
2004-06-02 21:17:01 +0000

    no message
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@1720 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/tufts/vue/LocalFileDataSource.java
@@ -0,0 +1,128 @@
+package tufts.vue;
+/*
+ * -----------------------------------------------------------------------------
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2003, 2004 
+ * Tufts University. All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+
+/*
+ * LocalFileDataSource.java
+ *
+ * Created on October 15, 2003, 5:28 PM
+ */
+
+
+
+import javax.swing.*;
+import java.util.Vector;
+import java.util.*;
+import java.awt.*;
+
+import javax.swing.border.*;
+
+import java.io.*;
+
+import osid.filing.*;
+import tufts.oki.remoteFiling.*;
+import tufts.oki.localFiling.*;
+import tufts.oki.shared.*;
+
+
+
+/**
+ *
+ * @author  rsaigal
+ */
+
+
+public class LocalFileDataSource extends VueDataSource{
+ 
+    private JComponent resourceViewer;
+    private String address;
+    public LocalFileDataSource(String DisplayName, String address){
+          this.setDisplayName(DisplayName); 
+          this.setAddress(address);
+          this.setResourceViewer();
+        
+     }
+
+      public void setAddress(String address){
+        
+        this.address = address;
+        
+    }
+     public String getAddress(){
+        
+        return this.address;
+        
+    }
+   public void  setResourceViewer(){
+          
+             Vector cabVector = new Vector();
+            try{
+            LocalFilingManager manager = new LocalFilingManager();   // get a filing manager
+         
+            if (this.getAddress().compareTo("") == 0){     
+                LocalCabinetEntryIterator rootCabs = (LocalCabinetEntryIterator) manager.listRoots();
+                osid.shared.Agent agent = null; //  This may cause problems later.
+              while(rootCabs.hasNext()){  
+                    LocalCabinetEntry rootNode = (LocalCabinetEntry)rootCabs.next();
+                    CabinetResource res = new CabinetResource(rootNode);
+                    cabVector.add(res);
+                }
+                
+            }
+          
+            // setPublishMode(Publisher.PUBLISH_CMAP);
+           
+            
+            else {
+                osid.shared.Agent agent = null;
+                LocalCabinet rootNode = new LocalCabinet(this.getAddress(),agent,null);
+                CabinetResource res = new CabinetResource(rootNode);
+                cabVector.add(res); 
+            }
+               }catch (Exception ex) {}
+            VueDragTree fileTree = new VueDragTree(cabVector.iterator(), this.getDisplayName());
+            fileTree.setRootVisible(true);
+            fileTree.setShowsRootHandles(true);
+            fileTree.expandRow(0);
+            fileTree.setRootVisible(false);
+            JPanel localPanel = new JPanel();
+            JScrollPane rSP = new JScrollPane(fileTree);
+            localPanel.setMinimumSize(new Dimension(290,100));
+            localPanel.setLayout(new BorderLayout());
+            localPanel.add(rSP,BorderLayout.CENTER);
+            this.resourceViewer = localPanel;
+   
+   }
+
+   public JComponent getResourceViewer(){
+       
+          return this.resourceViewer;   
+       
+   }
+ 
+   
+    
+}
+
+
+    
+
+
+
+
+
+
---
+++ VUE2/src/tufts/vue/VueDataSource.java
@@ -0,0 +1,158 @@
+/*
+ * -----------------------------------------------------------------------------
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2003, 2004 
+ * Tufts University. All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+/*
+ * VueDataSource.java
+ *
+ * Created on October 15, 2003, 5:28 PM
+ */
+
+package tufts.vue;
+
+/**
+ *
+ * @author  rsaigal
+ */
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+
+
+public class VueDataSource implements DataSource{
+  
+    private String displayName;
+    private String address;
+    private String Id;
+    private boolean isAutoConnect;
+    private int publishMode;
+    private JPanel resourceViewer; 
+    private JPanel addDataSourcePanel;
+    private JPanel editDataSourcePanel;
+    
+    
+    public VueDataSource(){
+        
+        
+    }
+    
+    public VueDataSource(String DisplayName){
+        
+     this.displayName = DisplayName;   
+     this.setResourceViewer();
+        
+    }
+    
+    
+     public String getDisplayName(){
+         
+      return this.displayName;   
+     }
+   
+    public void setDisplayName(String DisplayName){
+        
+        this.displayName = DisplayName;  
+        
+      }
+    
+     
+     public String getAddress(){
+         
+         return this.address;
+         
+         
+     }
+    
+    
+    /*
+       *Returns an id for the DataSource. 
+       *
+       */
+  
+    
+    
+    public String getId(){
+        
+        return this.Id; 
+    }
+   
+    public int getPublishMode(){
+        
+        return this.publishMode;   
+        
+    }
+   
+    public boolean isAutoConnect(){
+         return this.isAutoConnect;   
+        
+    }
+    
+    public void  setResourceViewer(){
+        
+     this.resourceViewer = new JPanel();   
+        
+    }
+    /**
+     *Returns a JPanel that is the Viewer for the DataSource
+     *
+     */
+    public JComponent getResourceViewer(){
+           return this.resourceViewer;   
+        
+    }
+    
+    
+   public void setAddDataSourcePanel(){
+       this.addDataSourcePanel = new JPanel();
+       
+       
+   }
+   
+ 
+       
+   
+   public void setEditDataSourcePanel(){
+       
+         this.editDataSourcePanel = new JPanel();
+       
+       
+   }
+   
+     /**
+     *Returns a JComponent that is the panel to add the datasource
+     *
+     */
+   public  JComponent getAddDataSourcePanel(){
+       return this.addDataSourcePanel;   
+       
+   }
+   
+   
+    
+   public JComponent getEditDataSourcePanel(){
+       
+         return this.editDataSourcePanel;  
+       
+       
+       
+   }
+    
+    
+}
+
+
+
