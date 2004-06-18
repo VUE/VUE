6ce1e4458be333ffd0dd41107348954a69673fc2
Anoop Kumar <anoopkum@gmail.com>
2004-06-18 21:12:50 +0000

    set default values automatically
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@1770 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/filter/Statement.java
+++ VUE2/src/tufts/vue/filter/Statement.java
@@ -64,6 +64,8 @@ public class Statement {
     
     public void setKey(Key key) {
         this.key = key;
+        setValue(key.getDefaultValue());
+        setOperator(key.getType().getDefaultOperator());
     }
     
     public Key getKey() {
