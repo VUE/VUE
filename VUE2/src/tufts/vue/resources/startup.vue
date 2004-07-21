92f535716a12113ca93a66c8f9fe54058b516940
Scott Fraize <scott.fraize@gmail.com>
2004-07-21 20:44:31 +0000

    new save file format & added an Untitled Pathway
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@1957 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/resources/startup.vue
+++ VUE2/src/tufts/vue/resources/startup.vue
@@ -1,67 +1,70 @@
+<!-- Do Not Remove: VUE mapping @version(1.0) jar:file:/VUE/src/VUE.jar!/tufts/vue/resources/lw_mapping.xml -->
+<!-- Do Not Remove: Saved Wed Jul 21 16:40:39 EDT 2004 by sfraize on platform Mac OS X 10.3.4 -->
 <?xml version="1.0" encoding="UTF-8"?>
-<LW-MAP ID="0" label="startup.vue" x="35.0" y="24.0" width="679.7"
-    height="474.5" strokeWidth="0.0" autoSized="true">
+<LW-MAP xsi:noNamespaceSchemaLocation="none" ID="0" label="Welcome"
+    x="35.0" y="24.0" width="646.35" height="474.5" strokeWidth="0.0"
+    autoSized="true" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
     <fillColor>#ffffff</fillColor>
     <font>SansSerif-plain-18</font>
     <nodeFilter/>
-    <child ID="3" x="126.417404" y="175.0" width="100.174225"
+    <child ID="3" x="125.77751" y="175.0" width="100.77526"
         height="115.0" strokeWidth="1.0" autoSized="true"
-        controlCount="0" arrowState="0" xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        controlCount="0" arrowState="0" xsi:type="link">
         <textColor>#404040</textColor>
         <font>Verdana-plain-11</font>
         <nodeFilter/>
-        <point1 x="126.417404" y="290.0"/>
-        <point2 x="226.59163" y="175.0"/>
+        <point1 x="125.77752" y="290.0"/>
+        <point2 x="226.55278" y="175.0"/>
         <ID1>1</ID1>
         <ID2>2</ID2>
     </child>
-    <child ID="5" x="144.13611" y="234.0" width="135.06267"
-        height="56.0" strokeWidth="1.0" autoSized="true"
-        controlCount="0" arrowState="0" xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <child ID="5" x="143.4837" y="234.0" width="135.29471" height="56.0"
+        strokeWidth="1.0" autoSized="true" controlCount="0"
+        arrowState="0" xsi:type="link">
         <textColor>#404040</textColor>
         <font>Verdana-plain-11</font>
         <nodeFilter/>
-        <point1 x="144.13612" y="290.0"/>
-        <point2 x="279.1988" y="234.0"/>
+        <point1 x="143.4837" y="290.0"/>
+        <point2 x="278.7784" y="234.0"/>
         <ID1>1</ID1>
         <ID2>4</ID2>
     </child>
-    <child ID="7" x="181.16275" y="287.5441" width="129.33725"
-        height="9.299438" strokeWidth="1.0" autoSized="true"
-        controlCount="0" arrowState="0" xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <child ID="7" x="179.77307" y="287.4992" width="130.72691"
+        height="9.395691" strokeWidth="1.0" autoSized="true"
+        controlCount="0" arrowState="0" xsi:type="link">
         <textColor>#404040</textColor>
         <font>Verdana-plain-11</font>
         <nodeFilter/>
-        <point1 x="181.16275" y="296.84354"/>
-        <point2 x="310.5" y="287.5441"/>
+        <point1 x="179.77309" y="296.8949"/>
+        <point2 x="310.5" y="287.4992"/>
         <ID1>1</ID1>
         <ID2>6</ID2>
     </child>
-    <child ID="9" x="136.56201" y="313.0" width="154.28326"
+    <child ID="9" x="135.89905" y="313.0" width="154.56654"
         height="88.0" strokeWidth="1.0" autoSized="true"
-        controlCount="0" arrowState="0" xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        controlCount="0" arrowState="0" xsi:type="link">
         <textColor>#404040</textColor>
         <font>Verdana-plain-11</font>
         <nodeFilter/>
-        <point1 x="136.56201" y="313.0"/>
-        <point2 x="290.84528" y="401.0"/>
+        <point1 x="135.89903" y="313.0"/>
+        <point2 x="290.46558" y="401.0"/>
         <ID1>1</ID1>
         <ID2>8</ID2>
     </child>
-    <child ID="26" x="171.68127" y="313.0" width="140.18956"
-        height="29.163177" strokeWidth="1.0" autoSized="true"
-        controlCount="0" arrowState="0" xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <child ID="26" x="170.81497" y="312.9162" width="141.06155"
+        height="29.21875" strokeWidth="1.0" autoSized="true"
+        controlCount="0" arrowState="0" xsi:type="link">
         <textColor>#404040</textColor>
         <font>Verdana-plain-11</font>
         <nodeFilter/>
-        <point1 x="171.68129" y="313.0"/>
-        <point2 x="311.87085" y="342.16318"/>
+        <point1 x="170.81497" y="312.9162"/>
+        <point2 x="311.87653" y="342.13495"/>
         <ID1>1</ID1>
         <ID2>25</ID2>
     </child>
-    <child ID="1" label="VUE Web Site" x="51.0" y="290.0" width="130.8"
-        height="23.0" strokeWidth="1.0" autoSized="true" xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-        <resource referenceCreated="1085871879992" accessAttempted="0"
+    <child ID="1" label="VUE Web Site" x="51.0" y="290.0" width="129.4"
+        height="23.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <resource referenceCreated="1090442390537" accessAttempted="0"
             accessSuccessful="0" size="-1" xsi:type="map-resource">
             <spec>http://vue.tccs.tufts.edu</spec>
             <type>0</type>
@@ -73,10 +76,10 @@
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
     <child ID="2" label="About VUE%nl;(Home)" x="192.5" y="141.0"
-        width="97.8" height="34.0" strokeWidth="1.0" autoSized="true"
-        xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        width="97.899994" height="34.0" strokeWidth="1.0"
+        autoSized="true" xsi:type="node">
         <notes>Background information about VUE.</notes>
-        <resource referenceCreated="1085871880035" accessAttempted="0"
+        <resource referenceCreated="1090442390571" accessAttempted="0"
             accessSuccessful="0" size="-1" xsi:type="map-resource">
             <spec>http://vue.tccs.tufts.edu/about/</spec>
             <type>0</type>
@@ -88,10 +91,9 @@
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
     <child ID="4" label="Download%nl;VUE software" x="262.5" y="200.0"
-        width="115.4" height="34.0" strokeWidth="1.0" autoSized="true"
-        xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        width="114.7" height="34.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
         <notes>Download the latest version of VUE.</notes>
-        <resource referenceCreated="1085871880085" accessAttempted="0"
+        <resource referenceCreated="1090442390644" accessAttempted="0"
             accessSuccessful="0" size="-1" xsi:type="map-resource">
             <spec>http://vue.tccs.tufts.edu/download/</spec>
             <type>0</type>
@@ -103,10 +105,9 @@
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
     <child ID="6" label="User%nl;Documentation" x="310.5" y="266.0"
-        width="126.4" height="34.0" strokeWidth="1.0" autoSized="true"
-        xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        width="125.2" height="34.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
         <notes>Online user documentation for VUE.</notes>
-        <resource referenceCreated="1085871880135" accessAttempted="0"
+        <resource referenceCreated="1090442390717" accessAttempted="0"
             accessSuccessful="0" size="-1" xsi:type="map-resource">
             <spec>http://vue.tccs.tufts.edu/userdoc/</spec>
             <type>0</type>
@@ -117,10 +118,11 @@
         <nodeFilter/>
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
-    <child ID="8" label="User Forums" x="263.5" y="401.0" width="114.3"
-        height="34.0" strokeWidth="1.0" autoSized="true" xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <child ID="8" label="User Forums" x="263.5" y="401.0"
+        width="113.649994" height="34.0" strokeWidth="1.0"
+        autoSized="true" xsi:type="node">
         <notes>Post questions, bug reports and comments to the online forums.</notes>
-        <resource referenceCreated="1085871880296" accessAttempted="0"
+        <resource referenceCreated="1090442390761" accessAttempted="0"
             accessSuccessful="0" size="-1" xsi:type="map-resource">
             <spec>http://vue.tccs.tufts.edu/forums/</spec>
             <type>0</type>
@@ -132,10 +134,10 @@
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
     <child ID="14" label="VUE%nl;Resources" x="191.5" y="464.0"
-        width="101.1" height="34.0" strokeWidth="1.0" autoSized="true"
-        xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        width="101.049995" height="34.0" strokeWidth="1.0"
+        autoSized="true" xsi:type="node">
         <notes>Download sample maps and link to related Web resource.</notes>
-        <resource referenceCreated="1085871880335" accessAttempted="0"
+        <resource referenceCreated="1090442390835" accessAttempted="0"
             accessSuccessful="0" size="-1" xsi:type="map-resource">
             <spec>http://vue.tccs.tufts.edu/resources/</spec>
             <type>0</type>
@@ -146,10 +148,10 @@
         <nodeFilter/>
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
-    <child ID="25" label="My Maps" x="311.5" y="334.0" width="85.7"
-        height="34.0" strokeWidth="1.0" autoSized="true" xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <child ID="25" label="My Maps" x="311.5" y="334.0" width="86.35"
+        height="34.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
         <notes>Upload and manage your VUE maps.</notes>
-        <resource referenceCreated="1085871880385" accessAttempted="0"
+        <resource referenceCreated="1090442390863" accessAttempted="0"
             accessSuccessful="0" size="-1" xsi:type="map-resource">
             <spec>http://vue.tccs.tufts.edu/mymaps</spec>
             <type>0</type>
@@ -161,9 +163,8 @@
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
     <child ID="27" label="Creating maps" x="525.5" y="218.0"
-        width="124.200005" height="22.0" strokeWidth="1.0"
-        autoSized="true" xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-        <resource referenceCreated="1085871880415" accessAttempted="0"
+        width="123.1" height="22.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <resource referenceCreated="1090442390919" accessAttempted="0"
             accessSuccessful="0" size="-1" xsi:type="map-resource">
             <spec>http://vue.tccs.tufts.edu/userdoc/maps.cfm</spec>
             <type>0</type>
@@ -174,21 +175,21 @@
         <nodeFilter/>
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
-    <child ID="29" x="432.73584" y="240.0" width="111.29187"
-        height="28.09613" strokeWidth="1.0" autoSized="true"
-        controlCount="0" arrowState="0" xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <child ID="29" x="431.70306" y="240.0" width="111.76453"
+        height="28.208862" strokeWidth="1.0" autoSized="true"
+        controlCount="0" arrowState="0" xsi:type="link">
         <textColor>#404040</textColor>
         <font>Verdana-plain-11</font>
         <nodeFilter/>
-        <point1 x="432.73584" y="268.09613"/>
-        <point2 x="544.0277" y="240.0"/>
+        <point1 x="431.70306" y="268.20886"/>
+        <point2 x="543.4676" y="240.0"/>
         <ID1>6</ID1>
         <ID2>27</ID2>
     </child>
     <child ID="30" label="Finding and %nl;linking resources." x="526.5"
-        y="266.0" width="141.8" height="34.0" strokeWidth="1.0"
-        autoSized="true" xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-        <resource referenceCreated="1085871880495" accessAttempted="0"
+        y="266.0" width="139.9" height="34.0" strokeWidth="1.0"
+        autoSized="true" xsi:type="node">
+        <resource referenceCreated="1090442391016" accessAttempted="0"
             accessSuccessful="0" size="-1" xsi:type="map-resource">
             <spec>http://vue.tccs.tufts.edu/userdoc/resources.cfm</spec>
             <type>0</type>
@@ -199,21 +200,21 @@
         <nodeFilter/>
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
-    <child ID="31" x="436.90002" y="283.0" width="89.600006"
-        height="0.0" strokeWidth="1.0" autoSized="true" controlCount="0"
-        arrowState="0" xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <child ID="31" x="435.7" y="283.0" width="90.79999" height="0.0"
+        strokeWidth="1.0" autoSized="true" controlCount="0"
+        arrowState="0" xsi:type="link">
         <textColor>#404040</textColor>
         <font>Verdana-plain-11</font>
         <nodeFilter/>
-        <point1 x="436.9" y="283.0"/>
+        <point1 x="435.7" y="283.0"/>
         <point2 x="526.5" y="283.0"/>
         <ID1>6</ID1>
         <ID2>30</ID2>
     </child>
     <child ID="32" label="Creating and %nl;Viewing Pathways" x="525.5"
-        y="313.0" width="142.9" height="34.0" strokeWidth="1.0"
-        autoSized="true" xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-        <resource referenceCreated="1085871880647" accessAttempted="0"
+        y="313.0" width="140.95" height="34.0" strokeWidth="1.0"
+        autoSized="true" xsi:type="node">
+        <resource referenceCreated="1090442391057" accessAttempted="0"
             accessSuccessful="0" size="-1" xsi:type="map-resource">
             <spec>http://vue.tccs.tufts.edu/userdoc/pathways.cfm</spec>
             <type>0</type>
@@ -224,40 +225,39 @@
         <nodeFilter/>
         <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
     </child>
-    <child ID="33" x="434.7873" y="295.86047" width="94.042206"
-        height="19.79837" strokeWidth="1.0" autoSized="true"
-        controlCount="0" arrowState="0" xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <child ID="33" x="433.64917" y="295.76865" width="94.97531"
+        height="20.028442" strokeWidth="1.0" autoSized="true"
+        controlCount="0" arrowState="0" xsi:type="link">
         <textColor>#404040</textColor>
         <font>Verdana-plain-11</font>
         <nodeFilter/>
-        <point1 x="434.78732" y="295.86047"/>
-        <point2 x="528.8295" y="315.65884"/>
+        <point1 x="433.6492" y="295.76865"/>
+        <point2 x="528.6245" y="315.7971"/>
         <ID1>6</ID1>
         <ID2>32</ID2>
     </child>
     <child ID="37" label="Welcome to VUE" x="35.0" y="24.0"
-        width="152.8" height="24.0" strokeWidth="0.0" autoSized="true"
-        xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        width="143.4" height="24.0" strokeWidth="0.0" autoSized="true" xsi:type="node">
         <strokeColor>#404040</strokeColor>
         <font>Verdana-bold-14</font>
         <nodeFilter/>
         <shape xsi:type="rectangle"/>
     </child>
-    <child ID="40" x="124.45" y="313.0" width="105.700005"
+    <child ID="40" x="123.79323" y="313.0" width="106.267845"
         height="151.0" strokeWidth="1.0" autoSized="true"
-        controlCount="0" arrowState="0" xsi:type="link" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        controlCount="0" arrowState="0" xsi:type="link">
         <textColor>#404040</textColor>
         <font>Verdana-plain-11</font>
         <nodeFilter/>
-        <point1 x="230.15001" y="464.0"/>
-        <point2 x="124.450005" y="313.0"/>
+        <point1 x="230.06108" y="464.0"/>
+        <point2 x="123.793236" y="313.0"/>
         <ID1>14</ID1>
         <ID2>1</ID2>
     </child>
     <child ID="41"
         label="This is an example of one of the many kinds of maps that you can create using the VUE software.  %nl;Each node on this map links directly to a page on the VUE Web site where you can find related VUE information."
-        x="35.0" y="52.0" width="679.7" height="34.0" strokeWidth="0.0"
-        autoSized="true" xsi:type="node" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        x="35.0" y="52.0" width="646.35" height="34.0" strokeWidth="0.0"
+        autoSized="true" xsi:type="node">
         <strokeColor>#404040</strokeColor>
         <font>Arial-plain-12</font>
         <nodeFilter/>
@@ -265,6 +265,14 @@
     </child>
     <userZoom>1.0</userZoom>
     <userOrigin x="23.0" y="12.0"/>
-    <PathwayList currentPathway="-1"/>
+    <PathwayList currentPathway="0">
+        <pathway label="Untitled Pathway" x="0.0" y="0.0" width="10.0"
+            height="10.0" strokeWidth="0.0" autoSized="true"
+            currentIndex="-1" locked="false" open="true">
+            <strokeColor>#993333</strokeColor>
+            <font>SansSerif-plain-14</font>
+            <nodeFilter/>
+        </pathway>
+    </PathwayList>
     <mapFilterModel/>
 </LW-MAP>
