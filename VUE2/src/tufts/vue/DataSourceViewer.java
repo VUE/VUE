1d51b436ce15f80ed86fb286c872b13040b29883
Anoop Kumar <anoopkum@gmail.com>
2005-03-25 21:49:06 +0000

    new repository
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@2116 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/DataSourceViewer.java
+++ VUE2/src/tufts/vue/DataSourceViewer.java
@@ -458,7 +458,7 @@ public class DataSourceViewer  extends JPanel implements KeyListener{
             addDataSource(ds1);
             DataSource ds2 = new FavoritesDataSource("My Favorites");
             addDataSource(ds2);
-            DataSource ds3 = new FedoraDataSource("Tufts Digital Library","snowflake.lib.tufts.edu", "test","test");
+            DataSource ds3 = new FedoraDataSource("Tufts Digital Library","dl.lib.tufts.edu", "test","test");
             addDataSource(ds3);
             DataSource ds4 = new GoogleDataSource("Tufts Web","http://googlesearch.tufts.edu","tufts01","tufts01");
             addDataSource(ds4);
