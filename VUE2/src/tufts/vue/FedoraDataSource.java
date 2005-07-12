4a44f545231ea7610c23587e0dc157c3abdea5e3
Anoop Kumar <anoopkum@gmail.com>
2005-07-12 20:29:59 +0000

    merged googleapi and osid2
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@2146 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/FedoraDataSource.java
+++ VUE2/src/tufts/vue/FedoraDataSource.java
@@ -2,7 +2,7 @@
  * -----------------------------------------------------------------------------
  *
  * <p><b>License and Copyright: </b>The contents of this file are subject to the
- * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * Mozilla Public License Version  (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License
  * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
  *
