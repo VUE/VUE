e2d69c085db0513941a6c12e0dc4f1f8eca26c50
Scott Fraize <scott.fraize@gmail.com>
2005-08-19 01:55:02 +0000

    diagnostic change
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@2211 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/VueResources.java
+++ VUE2/src/tufts/vue/VueResources.java
@@ -52,7 +52,7 @@ public class VueResources
         try {
             featureSet = System.getProperty("tufts.vue.features");
             classPath = System.getProperty("java.class.path");
-            System.out.println("CLASSPATH: " + classPath);
+            if (DEBUG.INIT) System.out.println("CLASSPATH: " + classPath);
         } catch (java.security.AccessControlException e) {
             System.err.println("Can't access system properties: applet assumed");
         }
@@ -237,7 +237,7 @@ public class VueResources
         try {
             //url =new File(sResourceBundle.getClass().getResource(getString(pLookupKey)).getFile().replaceAll("%20"," ")).toURL();
             url = sResourceBundle.getClass().getResource(getString(pLookupKey));
-            System.out.println("URL found for lookup key <" + pLookupKey + "> : " + url);
+            if (DEBUG.INIT) System.out.println("URL found for lookup key <" + pLookupKey + "> : " + url);
         } catch (Exception e) {
             alert("  !!! failed to lead due to "+ e.toString() );    
         }    
