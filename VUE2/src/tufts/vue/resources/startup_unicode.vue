144e15b2d9d4c271d6de95827d9c7bc65bd10431
Scott Fraize <scott.fraize@gmail.com>
2005-08-19 02:07:21 +0000

    new test startup file with "welcome" messages in various languges and unicode charsets
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@2222 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/tufts/vue/resources/startup_unicode.vue
@@ -0,0 +1,351 @@
+<!-- Do Not Remove: VUE mapping @version(1.1) file:/VUE/src/tufts/vue/resources/lw_mapping_1_1.xml -->
+<!-- Do Not Remove: Saved Thu Aug 18 16:01:39 EDT 2005 by sfraize on platform Mac OS X 10.4.2 -->
+<!-- Do Not Remove: Saved by @(#)VUE: built August 18 2005 at 1313 by sfraize on Mac OS X 10.4.2 ppc JVM 1.4.2_07-215 -->
+<?xml version="1.0" encoding="UTF-8"?>
+<LW-MAP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="none" ID="0"
+    label="startup_unicode.vue" x="35.0" y="24.0" width="770.0496"
+    height="487.49582" strokeWidth="0.0" autoSized="true">
+    <notes>This is the VUE start-up map.</notes>
+    <fillColor>#ffffff</fillColor>
+    <font>SansSerif-plain-18</font>
+    <nodeFilter/>
+    <child ID="3" x="127.79338" y="175.0" width="99.93377"
+        height="115.0" strokeWidth="1.0" autoSized="true"
+        controlCount="0" arrowState="0" xsi:type="link">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <nodeFilter/>
+        <point1 x="127.79339" y="290.0"/>
+        <point2 x="227.72716" y="175.0"/>
+        <ID1>1</ID1>
+        <ID2>2</ID2>
+    </child>
+    <child ID="5" x="145.29794" y="234.0" width="133.90295"
+        height="56.0" strokeWidth="1.0" autoSized="true"
+        controlCount="0" arrowState="0" xsi:type="link">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <nodeFilter/>
+        <point1 x="145.29794" y="290.0"/>
+        <point2 x="279.2009" y="234.0"/>
+        <ID1>1</ID1>
+        <ID2>4</ID2>
+    </child>
+    <child ID="7" x="183.93335" y="287.10266" width="126.566666"
+        height="10.0" strokeWidth="1.0" autoSized="true"
+        controlCount="0" arrowState="0" xsi:type="link">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <nodeFilter/>
+        <point1 x="183.93333" y="296.69788"/>
+        <point2 x="310.5" y="287.50748"/>
+        <ID1>1</ID1>
+        <ID2>6</ID2>
+    </child>
+    <child ID="9" x="137.63626" y="313.0" width="151.79056"
+        height="88.0" strokeWidth="1.0" autoSized="true"
+        controlCount="0" arrowState="0" xsi:type="link">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <nodeFilter/>
+        <point1 x="137.63626" y="313.0"/>
+        <point2 x="289.42682" y="401.0"/>
+        <ID1>1</ID1>
+        <ID2>8</ID2>
+    </child>
+    <child ID="26" x="173.07553" y="313.0" width="138.85092"
+        height="28.887756" strokeWidth="1.0" autoSized="true"
+        controlCount="0" arrowState="0" xsi:type="link">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <nodeFilter/>
+        <point1 x="173.07553" y="313.0"/>
+        <point2 x="311.92645" y="341.88776"/>
+        <ID1>1</ID1>
+        <ID2>25</ID2>
+    </child>
+    <child ID="1" label="VUE Web Site" x="51.0" y="290.0" width="133.6"
+        height="23.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <resource referenceCreated="1124395257544" accessAttempted="0"
+            accessSuccessful="0" size="-1"
+            spec="http://vue.tccs.tufts.edu" type="0" xsi:type="map-resource"/>
+        <fillColor>#d5e3f8</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-bold-14</font>
+        <nodeFilter/>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="2" label="About VUE&#xa;(Home)" x="192.5" y="141.0"
+        width="100.0" height="34.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <notes>Background information about VUE.</notes>
+        <resource referenceCreated="1124395257564" accessAttempted="0"
+            accessSuccessful="0" size="-1"
+            spec="http://vue.tccs.tufts.edu/about/" type="0" xsi:type="map-resource"/>
+        <fillColor>#a9ce97</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <nodeFilter/>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="4" label="Download&#xa;VUE software" x="262.5" y="200.0"
+        width="114.7" height="34.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <notes>Download the latest version of VUE.</notes>
+        <resource referenceCreated="1124395257597" accessAttempted="0"
+            accessSuccessful="0" size="-1"
+            spec="http://vue.tccs.tufts.edu/download/" type="0" xsi:type="map-resource"/>
+        <fillColor>#a9ce97</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <nodeFilter/>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="6" label="User&#xa;Documentation" x="310.5" y="266.0"
+        width="124.149994" height="34.0" strokeWidth="1.0"
+        autoSized="true" xsi:type="node">
+        <notes>Online user documentation for VUE.</notes>
+        <resource referenceCreated="1124395257621" accessAttempted="0"
+            accessSuccessful="0" size="-1"
+            spec="http://vue.tccs.tufts.edu/userdoc/" type="0" xsi:type="map-resource"/>
+        <fillColor>#d7f1ca</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <nodeFilter/>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="8" label="User Forums" x="263.5" y="401.0" width="110.5"
+        height="34.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <notes>Post questions, bug reports and comments to the online forums.</notes>
+        <resource referenceCreated="1124395257990" accessAttempted="0"
+            accessSuccessful="0" size="-1"
+            spec="http://vue.tccs.tufts.edu/forums/" type="0" xsi:type="map-resource"/>
+        <fillColor>#a9ce97</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <nodeFilter/>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="14" label="VUE&#xa;Resources" x="191.5" y="464.0"
+        width="98.95" height="34.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <notes>Download sample maps and link to related Web resource.</notes>
+        <resource referenceCreated="1124395258019" accessAttempted="0"
+            accessSuccessful="0" size="-1"
+            spec="http://vue.tccs.tufts.edu/resources/" type="0" xsi:type="map-resource"/>
+        <fillColor>#a9ce97</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <nodeFilter/>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="25" label="My Maps" x="311.5" y="334.0" width="88.45"
+        height="34.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <notes>Upload and manage your VUE maps.</notes>
+        <resource referenceCreated="1124395258063" accessAttempted="0"
+            accessSuccessful="0" size="-1"
+            spec="http://vue.tccs.tufts.edu/mymaps" type="0" xsi:type="map-resource"/>
+        <fillColor>#a9ce97</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <nodeFilter/>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="27" label="Creating maps" x="525.5" y="218.0"
+        width="121.0" height="22.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <resource referenceCreated="1124395258132" accessAttempted="0"
+            accessSuccessful="0" size="-1"
+            spec="http://vue.tccs.tufts.edu/userdoc/maps.cfm" type="0" xsi:type="map-resource"/>
+        <fillColor>#fcfabb</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <nodeFilter/>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="29" x="430.75708" y="240.0" width="111.76752"
+        height="28.278992" strokeWidth="1.0" autoSized="true"
+        controlCount="0" arrowState="0" xsi:type="link">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <nodeFilter/>
+        <point1 x="430.75708" y="268.279"/>
+        <point2 x="542.5246" y="240.0"/>
+        <ID1>6</ID1>
+        <ID2>27</ID2>
+    </child>
+    <child ID="30" label="Finding and &#xa;linking resources." x="526.5"
+        y="266.0" width="137.79999" height="34.0" strokeWidth="1.0"
+        autoSized="true" xsi:type="node">
+        <resource referenceCreated="1124395258195" accessAttempted="0"
+            accessSuccessful="0" size="-1"
+            spec="http://vue.tccs.tufts.edu/userdoc/resources.cfm"
+            type="0" xsi:type="map-resource"/>
+        <fillColor>#fcfabb</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <nodeFilter/>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="31" x="434.65002" y="278.0" width="91.850006"
+        height="10.0" strokeWidth="1.0" autoSized="true"
+        controlCount="0" arrowState="0" xsi:type="link">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <nodeFilter/>
+        <point1 x="434.65" y="283.0"/>
+        <point2 x="526.5" y="283.0"/>
+        <ID1>6</ID1>
+        <ID2>30</ID2>
+    </child>
+    <child ID="32" label="Creating and &#xa;Viewing Pathways" x="525.5"
+        y="313.0" width="142.0" height="34.0" strokeWidth="1.0"
+        autoSized="true" xsi:type="node">
+        <resource referenceCreated="1124395258318" accessAttempted="0"
+            accessSuccessful="0" size="-1"
+            spec="http://vue.tccs.tufts.edu/userdoc/pathways.cfm"
+            type="0" xsi:type="map-resource">
+            <property xsi:type="propertyEntry">
+                <entryKey xsi:type="java:java.lang.String">title</entryKey>
+                <entryValue xsi:type="java:java.lang.String"></entryValue>
+            </property>
+        </resource>
+        <fillColor>#fcfabb</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <nodeFilter/>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="33" x="432.6997" y="295.6197" width="95.97406"
+        height="20.144165" strokeWidth="1.0" autoSized="true"
+        controlCount="0" arrowState="0" xsi:type="link">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <nodeFilter/>
+        <point1 x="432.6997" y="295.6197"/>
+        <point2 x="528.67377" y="315.76385"/>
+        <ID1>6</ID1>
+        <ID2>32</ID2>
+    </child>
+    <child ID="37" label="Welcome to VUE" x="35.80995" y="24.0"
+        width="145.5" height="24.0" strokeWidth="0.0" autoSized="true" xsi:type="node">
+        <strokeColor>#404040</strokeColor>
+        <font>Verdana-bold-14</font>
+        <nodeFilter/>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="40" x="125.691444" y="313.0" width="103.617966"
+        height="151.0" strokeWidth="1.0" autoSized="true"
+        controlCount="0" arrowState="0" xsi:type="link">
+        <textColor>#404040</textColor>
+        <font>Verdana-plain-11</font>
+        <nodeFilter/>
+        <point1 x="229.3094" y="464.0"/>
+        <point2 x="125.69144" y="313.0"/>
+        <ID1>14</ID1>
+        <ID2>1</ID2>
+    </child>
+    <child ID="41"
+        label="This is an example of one of the many kinds of maps that you can create using the VUE software.  &#xa;Each node on this map links directly to a page on the VUE Web site where you can find related VUE information."
+        x="35.0" y="52.0" width="645.3" height="34.0" strokeWidth="0.0"
+        autoSized="true" xsi:type="node">
+        <strokeColor>#404040</strokeColor>
+        <font>Arial-plain-12</font>
+        <nodeFilter/>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="42" label="환영" x="699.44965" y="413.88016"
+        width="55.199997" height="32.0" strokeWidth="0.0"
+        autoSized="true" xsi:type="node">
+        <textColor>#666666</textColor>
+        <font>Arial-plain-22</font>
+        <nodeFilter/>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="43" label="歓迎" x="699.44965" y="362.2645"
+        width="55.199997" height="32.0" strokeWidth="0.0"
+        autoSized="true" xsi:type="node">
+        <textColor>#666666</textColor>
+        <font>Arial-plain-22</font>
+        <nodeFilter/>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="44" label="欢迎" x="699.44965" y="310.64883"
+        width="55.199997" height="32.0" strokeWidth="0.0"
+        autoSized="true" xsi:type="node">
+        <textColor>#666666</textColor>
+        <font>Arial-plain-22</font>
+        <nodeFilter/>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="45" label="Boa vinda" x="699.44965" y="189.80185"
+        width="68.85" height="20.0" strokeWidth="0.0" autoSized="true" xsi:type="node">
+        <textColor>#666666</textColor>
+        <font>Arial-bold-12</font>
+        <nodeFilter/>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="46" label="Recepción" x="699.44965" y="150.1862"
+        width="73.049995" height="20.0" strokeWidth="0.0"
+        autoSized="true" xsi:type="node">
+        <textColor>#666666</textColor>
+        <font>Arial-bold-12</font>
+        <nodeFilter/>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="47" label="Benvenuto" x="699.44965" y="70.954895"
+        width="74.1" height="20.0" strokeWidth="0.0" autoSized="true" xsi:type="node">
+        <textColor>#666666</textColor>
+        <font>Arial-bold-12</font>
+        <nodeFilter/>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="48" label="Willkommen" x="699.44965" y="110.57055"
+        width="82.5" height="20.0" strokeWidth="0.0" autoSized="true" xsi:type="node">
+        <textColor>#666666</textColor>
+        <font>Arial-bold-12</font>
+        <nodeFilter/>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="49" label="Bienvenue" x="699.44965" y="31.339241"
+        width="73.049995" height="20.0" strokeWidth="0.0"
+        autoSized="true" xsi:type="node">
+        <textColor>#666666</textColor>
+        <font>Arial-bold-12</font>
+        <nodeFilter/>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="50" label="ευπρόσδεκτος" x="699.44965"
+        y="229.41751" width="104.549995" height="22.0" strokeWidth="0.0"
+        autoSized="true" xsi:type="node">
+        <textColor>#666666</textColor>
+        <font>Arial-bold-13</font>
+        <nodeFilter/>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="52" label="ПРИВЕТСТВИЕ" x="699.44965"
+        y="271.03317" width="105.6" height="20.0" strokeWidth="0.0"
+        autoSized="true" xsi:type="node">
+        <textColor>#666666</textColor>
+        <font>Arial-bold-12</font>
+        <nodeFilter/>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="53" label="ترحيب" x="699.44965" y="465.49582"
+        width="67.799995" height="46.0" strokeWidth="0.0"
+        autoSized="true" xsi:type="node">
+        <textColor>#666666</textColor>
+        <font>Arial-plain-18</font>
+        <nodeFilter/>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <userZoom>1.1103180574823766</userZoom>
+    <userOrigin x="19.361132" y="-30.151617"/>
+    <PathwayList currentPathway="0" revealerIndex="-1">
+        <pathway label="Untitled Pathway" x="0.0" y="0.0" width="10.0"
+            height="10.0" strokeWidth="0.0" autoSized="true"
+            currentIndex="-1" locked="false" open="true">
+            <strokeColor>#993333</strokeColor>
+            <font>SansSerif-plain-14</font>
+            <nodeFilter/>
+        </pathway>
+    </PathwayList>
+    <mapFilterModel/>
+</LW-MAP>
