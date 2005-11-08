e4824d84bf6e896a11e5d2b2815d484ce46c4263
Anoop Kumar <anoopkum@gmail.com>
2005-11-08 22:28:55 +0000

    testing commit
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@2363 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/vue.bat
+++ VUE2/src/vue.bat
@@ -8,6 +8,7 @@ set VUE_LIB=%VUE_HOME%\..\lib\
 set JAVA=%JAVA_HOME%\bin\java
 set JAVAC=%JAVA_HOME%\bin\javac
 
+REM setting classpath
 @echo Create the classpath
 set CP=%VUE_HOME%;%VUE_LIB%castor-0.9.4.3.jar;%VUE_LIB%castor-0.9.4.3-xml.jar;%VUE_LIB%batik-svggen.jar;%VUE_LIB%batik-awt-util.jar;%VUE_LIB%batik-util.jar;%VUE_LIB%fop.jar;%VUE_LIB%okiSID_rc6_1.jar;%VUE_LIB%fedorautilities.jar;%VUE_LIB%fedoragentypes.jar;%VUE_LIB%fedora-client.jar;%VUE_LIB%fedora-server.jar;%VUE_LIB%axis.jar;%VUE_LIB%axis-ant.jar;%VUE_LIB%commons-discovery.jar;%VUE_LIB%commons-logging.jar;%VUE_LIB%jaxrpc.jar;%VUE_LIB%log4j-1.2.4.jar;%VUE_LIB%saaj.jar;%VUE_LIB%xerces-2_4_0/xercesImpl.jar;%VUE_LIB%xerces-2_4_0/xml-apis.jar;
 
