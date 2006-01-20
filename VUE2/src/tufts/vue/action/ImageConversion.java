d7ce76bdda81e54cfdd635f43588b2bcc381b84d
Scott Fraize <scott.fraize@gmail.com>
2006-01-20 17:37:50 +0000

    uses VueAction & new LWComponent image generation code
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@2444 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/action/ImageConversion.java
+++ VUE2/src/tufts/vue/action/ImageConversion.java
@@ -18,12 +18,6 @@
 
 package tufts.vue.action;
 
-/*
- * ImageConversion.java
- *
- * Created on May 28, 2003, 11:08 AM
- */
-
 import javax.imageio.*;
 import java.awt.image.*;
 import java.io.*;
@@ -33,12 +27,12 @@ import javax.swing.*;
 import tufts.vue.*;
 
 /**
- *
+ * @version $Revision: 1.17 $ / $Date: 2006-01-20 17:37:50 $ / $Author: sfraize $ *
  * @author  Daisuke Fujiwara
  */
 
 /**a class which constructs a JPEG image of the current concept map*/
-public class ImageConversion extends AbstractAction {
+public class ImageConversion extends VueAction {
     
     /** Creates a new instance of ImageConversion */
     public ImageConversion() {
@@ -53,7 +47,7 @@ public class ImageConversion extends AbstractAction {
     
     /**A method which takes in the image object and the location of the file along with the file format
      and saves the current map to the given file*/
-    public void convert(BufferedImage image, File location, String format)
+    public static void convert(BufferedImage image, File location, String format)
     {   
         //the conversion is done using the ImageIO class's static method
         try
@@ -67,51 +61,21 @@ public class ImageConversion extends AbstractAction {
     }
     
     /**A method which sets up for converting the active viewer to a Jpeg file*/
-    public void createJpeg(File location)
+    public static void createActiveMapJpeg(File location)
     {
-         //retrives the current map and gets its size
-         LWMap currentMap = VUE.getActiveMap();
-         
-         Rectangle2D bounds = currentMap.getBounds();
-         Dimension size = new Dimension((int)bounds.getWidth(), (int)bounds.getHeight());
-         
-         //creates an image object and sets up the graphics object of the image
-         BufferedImage mapImage = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_RGB);
-         Graphics2D g = (Graphics2D) mapImage.getGraphics();
-        
-        
-         g.setColor(Color.white);
-         g.fillRect(0, 0, size.width, size.height);
-         //g.setColor(Color.black);
-        // g.drawRect(0, 0, size.width-1, size.height-1);
-         
-         //translate and set the clip for the map content
-         g.translate(-(int)bounds.getX(), -(int)bounds.getY());
-         //g.setClip(0, 0, size.width, size.height);
-         g.setClip(bounds);   
-         DrawContext dc = new DrawContext(g);
-         dc.disableAntiAlias(true);
-         dc.setPrinting(true);
-         // render the map
-         currentMap.draw(dc);
-         //begins the conversion to the file
-         convert(mapImage, location, "jpeg");
+        convert(VUE.getActiveMap().getAsImage(), location, "jpeg");
     }
     
-    /**A method defined in the interface */
-    public void actionPerformed(java.awt.event.ActionEvent actionEvent) {
-        
-       try 
-       {
+    public void act() {
+       try {
+
            File selectedFile = ActionUtil.selectFile("Saving JPEG", "jpeg");
            
            if (selectedFile != null)
-             createJpeg(selectedFile);
-       }
-        
-       catch(Exception ex) 
-       {
-            System.out.println("Couldn't convert to jpeg:" + ex);
+               createActiveMapJpeg(selectedFile);
+
+       } catch (Throwable t) {
+            System.out.println("Couldn't convert to jpeg:" + t);
        }   
     }
     
