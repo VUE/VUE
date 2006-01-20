66481414c2512cafb466cb91310981cf5068a74b
Scott Fraize <scott.fraize@gmail.com>
2006-01-20 19:55:57 +0000

    debug change
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@2503 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/MouseAdapter.java
+++ VUE2/src/tufts/vue/MouseAdapter.java
@@ -25,29 +25,30 @@ public class MouseAdapter implements javax.swing.event.MouseInputListener
     private String name = "";
     public MouseAdapter() {}
     public MouseAdapter(String name) { this.name = name + ": "; }
+    public MouseAdapter(Class clazz) { this(clazz.getName()); }
     
     public static final MouseAdapter debug = new MouseAdapter("MouseAdapter");
 
 // The MouseListener methods
 // implements java.awt.event.MouseListener
     
-    public void mouseClicked(MouseEvent e) {if (DEBUG.MOUSE) System.out.println(name + e);}
-    public void mousePressed(MouseEvent e) {if (DEBUG.MOUSE) System.out.println(name + e);}
-    public void mouseReleased(MouseEvent e) {if (DEBUG.MOUSE) System.out.println(name + e);}
-    public void mouseEntered(MouseEvent e) {if (DEBUG.MOUSE) System.out.println(name + e);}
-    public void mouseExited(MouseEvent e) {if (DEBUG.MOUSE) System.out.println(name + e);}
+    public void mouseClicked(MouseEvent e) {if (DEBUG.MOUSE&&DEBUG.META) System.out.println(name + e);}
+    public void mousePressed(MouseEvent e) {if (DEBUG.MOUSE&&DEBUG.META) System.out.println(name + e);}
+    public void mouseReleased(MouseEvent e) {if (DEBUG.MOUSE&&DEBUG.META) System.out.println(name + e);}
+    public void mouseEntered(MouseEvent e) {if (DEBUG.MOUSE&&DEBUG.META) System.out.println(name + e);}
+    public void mouseExited(MouseEvent e) {if (DEBUG.MOUSE&&DEBUG.META) System.out.println(name + e);}
 
 // The MouseMotionListener methods
 // implements java.awt.event.MouseMotionListener
     
     public void mouseDragged(MouseEvent e)
     {
-        if (DEBUG.MOUSE)
+        if (DEBUG.MOUSE && DEBUG.META)
             System.out.println(name + "[" + e.paramString() + "] on " + e.getSource().getClass().getName());
     }
     public void mouseMoved(MouseEvent e)
     {
-        if (DEBUG.MOUSE&&DEBUG.META)
+        if (DEBUG.MOUSE && DEBUG.META)
             System.err.println(name + "[" + e.paramString() + "] on " + e.getSource().getClass().getName());
     }
 }
