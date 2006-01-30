1e13e14805445e9b5a777d29d72a77e17c4f1153
Scott Fraize <scott.fraize@gmail.com>
2006-01-30 07:11:08 +0000

    use of in-progress new VueTextField, test harness
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@2633 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/LWCInfoPanel.java
+++ VUE2/src/tufts/vue/LWCInfoPanel.java
@@ -24,9 +24,13 @@ import javax.swing.*;
 import javax.swing.text.JTextComponent;
 import javax.swing.border.*;
 
-import tufts.vue.gui.VueTextField;
+import tufts.vue.gui.*;
 
 // TODO FIX: the text input fields should save value on focus loss
+
+/**
+ * @version $Revision: 1.24 $ / $Date: 2006-01-30 07:11:08 $ / $Author: sfraize $
+ */
 
 class LWCInfoPanel extends javax.swing.JPanel
     implements VueConstants,
@@ -35,8 +39,16 @@ class LWCInfoPanel extends javax.swing.JPanel
                ActionListener,
                FocusListener
 {
-    private JTextField labelField = new JTextField(15);
-    private VueTextField resourceField = new tufts.vue.gui.VueTextField();
+    //private JTextField labelField = new JTextField(15);
+    //private JTextComponent labelField = new JTextPane();
+    private JTextComponent labelField = new VueTextPane();
+    
+    //private JLabel resourceField = new JLabel();
+    //private JTextComponent resourceField = new tufts.vue.gui.VueTextField();
+    //private JTextArea resourceField = new JTextArea();
+    //private JTextComponent resourceField = new JTextPane();
+    private JTextComponent resourceField = new VueTextPane();
+    
     private JLabel sizeField = new JLabel();
     
     private JPanel fieldPane = new JPanel();
@@ -52,7 +64,20 @@ class LWCInfoPanel extends javax.swing.JPanel
     
     private LWComponent lwc;
     
-    public LWCInfoPanel() {
+
+    public LWCInfoPanel()
+    {
+        if (!GUI.isMacAqua()) {
+            Border textPaneBorder = BorderFactory.createEtchedBorder();
+            //Border textPaneBorder = BorderFactory.createLineBorder(Color.lightGray);
+            labelField.setBorder(textPaneBorder);
+            resourceField.setBorder(textPaneBorder);
+        }
+
+        resourceField.setEditable(false);
+        resourceField.setOpaque(false);
+        resourceField.setBorder(null);
+        
         //setBorder(BorderFactory.createEmptyBorder(6,6,6,6));
         GridBagLayout gridBag = new GridBagLayout();
         GridBagConstraints c = new GridBagConstraints();
@@ -65,7 +90,7 @@ class LWCInfoPanel extends javax.swing.JPanel
         add(metadataPane,BorderLayout.CENTER);
         VUE.ModelSelection.addListener(this);
     }
-    
+
     private void setUpMetadataPane() {
         metadataPane.setLayout(new BorderLayout());
         metadataPane.setBorder(BorderFactory.createEmptyBorder(10,0,0,0));
@@ -73,39 +98,68 @@ class LWCInfoPanel extends javax.swing.JPanel
         propertiesEditor = new PropertiesEditor(false);
         metadataPane.add(propertiesEditor,BorderLayout.CENTER);
         validate();
-        
     }
     
-    private void addLabelTextRows(Object[] labelTextPairs,
-    GridBagLayout gridbag,
-    Container container) {
+    private void addLabelTextRows(Object[] labelTextPairs, GridBagLayout gridbag, Container container)
+    {
         GridBagConstraints c = new GridBagConstraints();
         c.anchor = GridBagConstraints.EAST;
         int num = labelTextPairs.length;
+        boolean lastWasLabelAbove = false;
+
+        Border lastBorder = null;
         
         for (int i = 0; i < num; i += 2) {
-            c.insets = new Insets(0, 0, 1, 0);
-            c.gridwidth = GridBagConstraints.RELATIVE; //next-to-last
-            c.fill = GridBagConstraints.NONE;      //reset to default
-            c.weightx = 0.0;                       //reset to default
-            c.anchor = GridBagConstraints.WEST;
+            
             String txt = (String) labelTextPairs[i];
             boolean readOnly = false;
-            if (txt.startsWith("-")) {
+            boolean labelAbove = false;
+            if (txt.charAt(0) == '-') {
                 txt = txt.substring(1);
                 readOnly = true;
+            } else if (txt.charAt(0) == '+') {
+                labelAbove = true;
+                txt = txt.substring(1);
             }
             txt += ": ";
             
+            //-------------------------------------------------------
+            // Add the label field
+            //-------------------------------------------------------
+
+            int topPad = lastWasLabelAbove ? 3 : 1;
+            
+            if (labelAbove) {
+                c.insets = new Insets(topPad, 0, 0, 0);
+                c.gridwidth = GridBagConstraints.REMAINDER; // last in row
+                c.anchor = GridBagConstraints.WEST;
+            } else {
+                c.insets = new Insets(topPad, 0, 1, 0);
+                c.gridwidth = GridBagConstraints.RELATIVE; // next-to-last in row
+                c.anchor = GridBagConstraints.EAST;
+                // this makes labels stay at top left of multi-line fields, tho it throws off
+                // baseline alignment for normal cases.  What we REALLY want is a baseline
+                // alignment against the first line of text in the field.
+                // c.anchor = GridBagConstraints.NORTHEAST;
+            }
+            c.fill = GridBagConstraints.NONE; // the label never grows
+            c.weightx = 0.0;                  // reset
+            
             JLabel label = new JLabel(txt);
-            //JLabel label = new JLabel(labels[i]);
-            //label.setFont(VueConstants.SmallFont);
             gridbag.setConstraints(label, c);
             container.add(label);
+            label.setFont(FONT_NARROW);
+
+            //-------------------------------------------------------
+            // Add the text value field
+            //-------------------------------------------------------
             
-            c.gridwidth = GridBagConstraints.REMAINDER;     //end row
+            c.gridwidth = GridBagConstraints.REMAINDER;     // last in row
             c.fill = GridBagConstraints.HORIZONTAL;
-            c.insets = new Insets(0, 5, 1, 0);
+            if (labelAbove)
+                c.insets = new Insets(0, 0, 0, 0);
+            else
+                c.insets = new Insets(0, 0, 1, 0);
             c.weightx = 1.0;
             
             JComponent field = (JComponent) labelTextPairs[i+1];
@@ -115,21 +169,40 @@ class LWCInfoPanel extends javax.swing.JPanel
                 ((JTextField)field).addActionListener(this);
                    ((JTextField)field).addFocusListener(this);
             }
+            //field.setFont(FONT_NARROW);
             gridbag.setConstraints(field, c);
             container.add(field);
-            
+
+            if (lastBorder != null && field instanceof JTextPane) {
+                field.setBorder(lastBorder);
+            }
             
             
             if (readOnly) {
-                field.setBorder(new EmptyBorder(1,1,1,1));
-                if (field instanceof JTextField) {
-                    JTextField tf = (JTextField) field;
-                    tf.setEditable(false);
-                    tf.setFocusable(false);
+                Border b = field.getBorder();
+                //System.out.println("LWCInfoPanel: got border " + b);
+                //lastBorder = b;
+                if (b != null) {
+                    final Insets borderInsets = b.getBorderInsets(field);
+                    System.out.println("LWCInfoPanel: got border insets " + borderInsets + " for " + field);
+                    field.putClientProperty(VueTextField.ActiveBorderKey, b);
+                    Border emptyBorder = new EmptyBorder(borderInsets);
+                    field.putClientProperty(VueTextField.InactiveBorderKey, emptyBorder);
+                    field.setBorder(emptyBorder);
                 }
-                if (VueUtil.isMacPlatform())
-                    field.setBackground(SystemColor.control);
+                //field.setBorder(new EmptyBorder(1,1,1,1));
+                if (field instanceof JTextComponent) {
+                    JTextComponent tc = (JTextComponent) field;
+                    tc.setEditable(false);
+                    tc.setFocusable(false);
+                }
+                if (VueUtil.isMacPlatform()) {
+                    //field.setBackground(SystemColor.control);
+                    field.setOpaque(false);
+                }
             }
+
+            lastWasLabelAbove = labelAbove;
         }
         /**
          * JLabel field  = new JLabel("Metadata");
@@ -161,6 +234,15 @@ class LWCInfoPanel extends javax.swing.JPanel
     }
     
     private void loadText(JTextComponent c, String text) {
+        String hasText = c.getText();
+        // This prevents flashing where fields of
+        // length greater the the visible area do
+        // a flash-scroll when setting the text, even
+        // if it's the same as what's there.
+        if (hasText != text && !hasText.equals(text))
+            c.setText(text);
+    }
+    private void loadText(JLabel c, String text) {
         String hasText = c.getText();
         // This prevents flashing where fields of
         // length greater the the visible area do
@@ -204,7 +286,7 @@ class LWCInfoPanel extends javax.swing.JPanel
         final Resource r = c.getResource();
         
         if (r != null)
-            loadText(resourceField, r.toString());
+            loadText(resourceField, r.getSpec());
         else
             loadText(resourceField, "");
         
@@ -274,6 +356,38 @@ class LWCInfoPanel extends javax.swing.JPanel
     public String toString() {
         return "LWCInfoPanel@" + Integer.toHexString(hashCode());
     }
+
+    public static void main(String args[]) {
+
+        VUE.init(args);
+
+        LWCInfoPanel p = new LWCInfoPanel();
+        LWComponent node = new LWNode("Test Node");
+        node.setNotes("I am a note.");
+        node.setResource("file:///System/Library/Frameworks/JavaVM.framework/Versions/1.4.2/Home");
+        Resource r = node.getResource();
+        for (int i = 1; i < 6; i++)
+            r.setProperty("field_" + i, "value_" + i);
+        if (args.length > 1) {
+            //ToolWindow w = VUE.createToolWindow("LWCInfoPanel", p);
+            DockWindow w = new DockWindow("LWCInfoPanel", p);
+            w.setVisible(true);
+        } else
+            tufts.Util.displayComponent(p);
+        VUE.getSelection().setTo(node); // setLWComponent does diddly -- need this
+
+        // Must have at least ONE active frame for our focus manager to work
+        new Frame("An Active Frame").setVisible(true);
+        
+        /*
+        p.setAllEnabled(true);
+        p.labelField.setEditable(true);
+        p.labelField.setEnabled(true);
+        p.resourceField.setEditable(true);
+        p.resourceField.setEnabled(true);
+        */
+    }
+    
     
     
 }
