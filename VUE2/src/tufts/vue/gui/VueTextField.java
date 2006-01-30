510cefcbf7e33034a7a78334a3ae0218fc9a04f0
Scott Fraize <scott.fraize@gmail.com>
2006-01-30 07:10:07 +0000

    started to add multi-line support
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@2632 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/gui/VueTextField.java
+++ VUE2/src/tufts/vue/gui/VueTextField.java
@@ -30,30 +30,118 @@ package tufts.vue.gui;
  *
  * @author  akumar03
  */
-import javax.swing.*;
+import tufts.vue.DEBUG;
+
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.border.Border;
 
-public class VueTextField extends JTextField implements MouseListener,FocusListener{
+public class VueTextField extends JTextField
+    implements MouseListener, FocusListener
+               //, java.beans.PropertyChangeListener, CaretListener
+{
+    public static final String ActiveBorderKey = "VueActiveBorder";
+    public static final String InactiveBorderKey = "VueInactiveBorder";
     
     public static final int LENGTH = 42;
     private String longText ="";
-    /** Creates a new instance of VueTextField */
     
     public VueTextField() {
-        super();
+        this("");
     }
     public VueTextField(String text) {
-        super();
-        setText(text);
+        this(text, text.length() + 2);
+    }
+    public VueTextField(String text, int length) {
+        super(text, length);
+        setToolTipText(null);
+        //addMouseListener(this);
+        addFocusListener(this);
+
+        setDragEnabled(false); // keep things simple for now
+
+        // background -- appears to work only when manually calling
+        // getCaret().setSelectionVisible as we do in our hack.
+        //setSelectionColor(Color.red);
         
+        //setSelectedTextColor(Color.red); // text foreground
+        
+        //addPropertyChangeListener(this);
+        //addCaretListener(this);
+    }
+
+    public Container X_getFocusCycleRootAncestor() {
+        tufts.Util.printStackTrace(this + " getFocusCycleRootAncestor");
+        Container r = super.getFocusCycleRootAncestor();
+        System.err.println("FOCUS-ANCESTOR " + r
+                           + "\n\t  isShowing: " + r.isShowing()
+                           + "\n\tisFocusable: " + r.isFocusable()
+                           + "\n\t  isEnabled: " + r.isEnabled());
+	return r;
+    }
+
+    public boolean X_requestFocus(boolean temporary) {
+        tufts.Util.printStackTrace(this + " requestFocus, temporary=" + temporary + " isFocusable=" + isFocusable());
+        return super.requestFocus(temporary);
+    }
+
+
+    public void X_setCaretPosition(int p) {
+        tufts.Util.printStackTrace(this + " setCaretPosition " + p);
+        super.setCaretPosition(p);
     }
     
+    public void X_moveCaretPosition(int p) {
+        tufts.Util.printStackTrace(this + " moveCaretPosition " + p);
+        super.moveCaretPosition(p);
+    }
+
+    public void X_select(int start, int end) {
+        tufts.Util.printStackTrace(this + " select, start=" + start + " end=" + end);
+        super.select(start, end);
+    }
+
+    public void X_paint(Graphics g) {
+        tufts.Util.printStackTrace("paint");
+        //System.out.println(this + " paint");
+        super.paint(g);
+    }
+
+    public void X_caretUpdate(CaretEvent e) {
+        System.out.println(this + " " + e);
+        getCaret().setVisible(true);
+        
+    }
+
+    public void X_propertyChange(java.beans.PropertyChangeEvent e) {
+        System.out.println(this + " " + e);
+    }
+
+    public void X_setBorder(Border border) {
+        //tufts.Util.printStackTrace("setBorder " + border);
+        super.setBorder(border);
+    }
+                                          
+    
+    public void mousePressed(MouseEvent e) {
+        System.out.println(this + " " + e);
+        //requestFocus();
+    }
+    public boolean isFocusable() {
+	return true;
+    }
+
     public void setText(String text) {
         this.longText = text;
         if(text.length() > LENGTH) {
             super.setText(text.substring(0,LENGTH)+"...");
             super.setEditable(false);
+            super.setOpaque(false);
+            Border b = (Border) getClientProperty(InactiveBorderKey);
+            if (b != null)
+                setBorder(b);
             this.addMouseListener(this);
             this.addFocusListener(this);
         }else {
@@ -67,11 +155,27 @@ public class VueTextField extends JTextField implements MouseListener,FocusListe
     public void mouseClicked(MouseEvent e) {
         if(longText.length() > LENGTH) {
             if((e.getButton() == MouseEvent.BUTTON1) && (e.getClickCount() == 2)) {
-                super.setEditable(true);
-                super.setText(longText);               
+                unlock();
+                super.setText(longText);
             }
         }
     }
+
+    
+    private void unlock() {
+        System.out.println(this + " is unlocking");
+        Border b = (Border) getClientProperty(ActiveBorderKey);
+        if (b != null) {
+            //System.out.println("found native border " + b);
+            setBorder(b);
+        }
+        setFocusable(true);
+        setEditable(true);
+        setOpaque(true);
+        //repaint();
+        requestFocus();
+    }
+
     
     public void mouseEntered(MouseEvent e) {
     }
@@ -79,18 +183,27 @@ public class VueTextField extends JTextField implements MouseListener,FocusListe
     public void mouseExited(MouseEvent e) {
     }
     
-    public void mousePressed(MouseEvent e) {
-    }
-    
     public void mouseReleased(MouseEvent e) {
     }
     
     public void focusGained(FocusEvent e) {
+        //tufts.Util.printStackTrace(this + " focusGained: hasFocus=" + hasFocus() + " " + e);
+        if (DEBUG.FOCUS) System.out.println("\t" + this + " focusGained: hasFocus=" + hasFocus());
     }
     
     public void focusLost(FocusEvent e) {
-        if(super.isEditable())
-            setText(this.getText());
+        //tufts.Util.printStackTrace(this + " focusLost: hasFocus=" + hasFocus() + " " + e);
+        if (DEBUG.FOCUS) System.out.println("\t" + this + " focusLost: hasFocus=" + hasFocus());
+        //if(super.isEditable())
+        //    setText(this.getText());
+    }
+
+    public String toString() {
+        String text = "<uninitialized>";
+        try {
+            text = getText();
+        } catch (Exception e) {}
+        return "VueTextField@" + Integer.toHexString(hashCode()) + "[" + text + "]";
     }
     
 }
