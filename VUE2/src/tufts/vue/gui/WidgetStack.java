5a759af0825f7af6b8d41ae1482bbfd472c0493f
Scott Fraize <scott.fraize@gmail.com>
2006-03-17 15:38:10 +0000

    fixed final layout bugs for expanding widget stacks, added error checks, made API smarter / safer, code cleanup
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@2787 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/gui/WidgetStack.java
+++ VUE2/src/tufts/vue/gui/WidgetStack.java
@@ -30,86 +30,89 @@ import javax.swing.*;
  * Note that the ultimate behaviour of the stack will be very dependent on the
  * the preferredSize/maximumSize/minimumSize settings on the contained JComponent's.
  *
- * @version $Revision: 1.2 $ / $Date: 2006-03-17 07:47:57 $ / $Author: sfraize $
+ * @version $Revision: 1.3 $ / $Date: 2006-03-17 15:38:10 $ / $Author: sfraize $
  * @author Scott Fraize
  */
 public class WidgetStack extends JPanel
 {
     private final JPanel mGridBag;
     private final GridBagConstraints _gbc = new GridBagConstraints();
+    private final Insets ExpandedTitleBarInsets = GUI.EmptyInsets;
+    private final Insets CollapsedTitleBarInsets = new Insets(0,0,1,0);
+    private final GridBagLayout mLayout;
+    private final JComponent mDefaultExpander;
 
-    private boolean mExpanderWasAdded = false;
+    private int mExpandersOpen = 0;
 
     public WidgetStack() {
-        //super(new BorderLayout());
-        //setBorder(new LineBorder(Color.red));
-        if (true) {
-            mGridBag = this;
-            setLayout(new GridBagLayout());
-        } else {
-            mGridBag = new JPanel(new GridBagLayout());
-            add(mGridBag, BorderLayout.NORTH);
-            // add something to eat extra vertical space
-            // (easier than doing via the grid bag itself)
-            JLabel empty = new JLabel("empty space");
-            //empty.setPreferredSize(new Dimension(0,0));
-            add(empty, BorderLayout.CENTER);
-        }
+        mLayout = new GridBagLayout();
+        mGridBag = this;
+        setLayout(mLayout);
 
-        _gbc.gridwidth = GridBagConstraints.REMAINDER;
+        _gbc.gridwidth = GridBagConstraints.REMAINDER; // last in line as only one column
         _gbc.anchor = GridBagConstraints.NORTH;
-        _gbc.weightx = 1;
+        _gbc.weightx = 1;                              // always expand horizontally
+        _gbc.gridx = 0;
+        _gbc.gridy = 0;
+
+        // We now add a component "guaranteed" to be at the bottom of the stack
+        // (gridy=64), that starts invisible, but when all other vertical expanders
+        // become invisible (are closed), this component will be set to visible (tho it
+        // will display nothing), and will eat up any leftover vertical space at the
+        // bottom of gridbag (it has a non-zero weighty), so if everything is collapsed,
+        // the titleBar's all go to the top, instead of the middle.  (In a grid bag,
+        // there must always be at least one visible component with a non-zero gridy
+        // value, or everything is clumped in the center of the display area).
         
+        GridBagConstraints c = (GridBagConstraints) _gbc.clone();
+        c.fill = GridBagConstraints.BOTH;
+        c.anchor = GridBagConstraints.SOUTH;
+        c.weighty = 1;
+        c.gridy = 64; 
+        mDefaultExpander = new JPanel();
+        mDefaultExpander.setVisible(false);
+        add(mDefaultExpander, c);
     }
 
-    public void addPane(String title, JComponent widget, int verticalExpansionWeight) {
-        //WidgetBox box = new WidgetBox(title, widget);
-        WidgetTitle titleBar = new WidgetTitle(title, widget);
 
-        if (verticalExpansionWeight != 0)
-            mExpanderWasAdded = true;
+    /**
+     * At least one pane MUST have a non-zero vertical expansion
+     * weight (usually values between 0.0 and 1.0: meaninful only
+     * relative to each other), otherwise the panes will all clump
+     * together in the middle.
+     */
+    
+    public void addPane(String title, JComponent widget, float verticalExpansionWeight) {
+        //verticalExpansionWeight=0.0f;
+        boolean isExpander = (verticalExpansionWeight != 0.0f);
+        WidgetTitle titleBar = new WidgetTitle(title, widget, isExpander);
+
+        if (isExpander)
+            mExpandersOpen++;
 
         _gbc.weighty = 0;
         _gbc.fill = GridBagConstraints.HORIZONTAL;
         mGridBag.add(titleBar, _gbc);
-        
+        _gbc.gridy++;
         _gbc.fill = GridBagConstraints.BOTH;
         _gbc.weighty = verticalExpansionWeight;
         mGridBag.add(widget, _gbc);
+
+        _gbc.gridy++;
+        
     }
 
     public void addPane(String title, JComponent widget) {
-        addPane(title, widget, 0);
+        addPane(title, widget, 1f);
     }
-    
 
     public void addNotify() {
         super.addNotify();
-
-        // we need this all the time as it's now possible for
-        // everything to be collpased, even if there was an
-        // expander, now that we've done away with WidgetBox
-        //if (!mExpanderWasAdded)
-        
-        // Add a component that is invisible (min size = 0), but will
-        // eat up any leftover vertical space in the gridbag, so if
-        // everything is collapsed, it will all go to the top, instead
-        // of the middle.  Note: if we end up adding panes
-        // dynamically, we'll need to manage this, or try going back
-        // to handling with a borderlayout or something.
-        
-        GridBagConstraints c = (GridBagConstraints) _gbc.clone();
-        c.fill = GridBagConstraints.CENTER;
-        c.weighty = 1;
-        JLabel empty = new JLabel("empty space");
-        empty.setPreferredSize(new Dimension(0,0));
-        empty.setMinimumSize(new Dimension(0,0));
-        add(empty, c);
+        if (mExpandersOpen == 0)
+            tufts.Util.printStackTrace("warning: no vertical expanding panes; WidgetStack will not layout properly");
     }
+    
 
-}
-
-class WidgetTitle extends Box {
     private static final int TitleHeight = 20;
     private static final Color TopGradient = new Color(79,154,240);
     private static final Color BottomGradient = new Color(0,133,246);
@@ -120,127 +123,99 @@ class WidgetTitle extends Box {
     private static final char RightArrowChar = 0x25B6; // unicode "black right pointing triangle"
     private static final char DownArrowChar = 0x25BC; // unicode "black down pointing triangle"
 
-    private final JLabel mTitle;
-    private final JComponent mWidget;
-    //private final WidgetBox mWidgetBox;
-    private final GUI.IconicLabel mIcon;
-
-    private boolean mExpanded = true;
-
     private static final boolean isMac = tufts.Util.isMacPlatform();
 
-    
-    //public WidgetTitle(String label, WidgetBox box) {
-    public WidgetTitle(String label, JComponent widget) {
-        super(BoxLayout.X_AXIS);
-        setName(label);
-        setOpaque(true);
-        //mWidgetBox = box;
-        mWidget = widget;
-        mTitle = new JLabel(label);
-        mTitle.setForeground(Color.white);
-        mTitle.setFont(new Font("Lucida Grande", Font.BOLD, 12));
-        add(Box.createHorizontalStrut(9));
-        int iconHeight = 10;
-        int iconWidth = 9;
-        int fontSize = 9;
-        mIcon = new GUI.IconicLabel(DownArrowChar, fontSize, Color.white, iconWidth, iconHeight);
-        add(mIcon);
-        add(Box.createHorizontalStrut(4));
-        add(mTitle);
-        setPreferredSize(new Dimension(-1, TitleHeight));
-        setMaximumSize(new Dimension(Short.MAX_VALUE, TitleHeight));
-        setMinimumSize(new Dimension(50, TitleHeight));
+    class WidgetTitle extends Box {
 
+        private final JLabel mTitle;
+        private final JComponent mWidget;
+        private final GUI.IconicLabel mIcon;
 
-        addMouseListener(new tufts.vue.MouseAdapter(label) {
-                public void mouseClicked(MouseEvent e) {
-                    setExpanded(!mExpanded);
-                }
-                public void mousePressed(MouseEvent e) {
-                    mIcon.setForeground(TopGradient.brighter());
-                }
-                public void mouseReleased(MouseEvent e) {
-                    mIcon.setForeground(Color.white);
-                }
-            });
+        private final boolean isExpander;
         
-    }
-    
-    private void setExpanded(boolean expanded) {
-        mExpanded = expanded;
-        if (mExpanded)
-            mIcon.setText("" + DownArrowChar);
-        else
-            mIcon.setText("" + RightArrowChar);
-        mWidget.setVisible(expanded);
-        //mWidgetBox.setExpanded(mExpanded);
-    }
-    
+        private boolean mExpanded = true;
 
-    public void paint(Graphics g) {
-        if (!isMac) {
-            // this is on by default on the mac
-            Graphics2D g2 = (Graphics2D) g;
-            g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        }
-        super.paint(g);
-    }
-
-    public void paintComponent(Graphics g) {
-        paintGradient((Graphics2D)g);
-        super.paintComponent(g);
-    }
-
-    private void paintGradient(Graphics2D g)
-    {
-        g.setPaint(Gradient);
-        g.fillRect(0, 0, getWidth(), TitleHeight);
-    }
-
-    
-}
-
-/*
-class WidgetBox extends JPanel {
-
-    private final JComponent mWidget;
-    private GridBagConstraints c = new GridBagConstraints();
-
-    WidgetBox(String title, JComponent widget) {
-        super(new BorderLayout());
-        //super(new GridLayout(2, 1));
-        //super(new GridBagLayout());
-        //setBorder(new LineBorder(Color.green));
-        
-        setName(title);
-        JComponent titleBar = new WidgetTitle(title, this);
-        if (widget == null)
-            mWidget = new JTextArea(title + ":\nfoo\nbar\nbaz\boobie\nbazzie");
-        else
+        public WidgetTitle(String label, JComponent widget, boolean isExpander) {
+            super(BoxLayout.X_AXIS);
+            this.isExpander = isExpander;
+            setName(label);
+            setOpaque(true);
             mWidget = widget;
+            mTitle = new JLabel(label);
+            mTitle.setForeground(Color.white);
+            mTitle.setFont(new Font("Lucida Grande", Font.BOLD, 12));
+            add(Box.createHorizontalStrut(9));
+            int iconHeight = 10;
+            int iconWidth = 9;
+            int fontSize = 9;
+            mIcon = new GUI.IconicLabel(DownArrowChar, fontSize, Color.white, iconWidth, iconHeight);
+            add(mIcon);
+            add(Box.createHorizontalStrut(4));
+            add(mTitle);
+            setPreferredSize(new Dimension(-1, TitleHeight));
+            setMaximumSize(new Dimension(Short.MAX_VALUE, TitleHeight));
+            setMinimumSize(new Dimension(50, TitleHeight));
 
-        //mWidget.setBorder(new LineBorder(Color.blue));
+
+            addMouseListener(new tufts.vue.MouseAdapter(label) {
+                    public void mouseClicked(MouseEvent e) {
+                        setExpanded(!mExpanded);
+                    }
+                    public void mousePressed(MouseEvent e) {
+                        mIcon.setForeground(TopGradient.brighter());
+                    }
+                    public void mouseReleased(MouseEvent e) {
+                        mIcon.setForeground(Color.white);
+                    }
+                });
         
-        add(titleBar, BorderLayout.NORTH);
-        add(mWidget, BorderLayout.CENTER);
-        
-        /*
-        c.gridwidth = GridBagConstraints.REMAINDER;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 1;
-        add(titleBar, c);
-        //c.fill = GridBagConstraints.CENTER;
-        c.weighty = 1;
-        add(mWidget, c);
-        *
+        }
+    
+        private void setExpanded(boolean expanded) {
+            mExpanded = expanded;
+            if (mExpanded) {
+                mIcon.setText("" + DownArrowChar);
+                if (isExpander)
+                    mExpandersOpen++;
+            } else {
+                mIcon.setText("" + RightArrowChar);
+                if (isExpander)
+                    mExpandersOpen--;
+            }
+            if (mExpandersOpen == 0)
+                mDefaultExpander.setVisible(true);
+            else
+                mDefaultExpander.setVisible(false);
+                
+            mWidget.setVisible(expanded);
+            GridBagConstraints c = mLayout.getConstraints(this);
+            c.insets = expanded ? ExpandedTitleBarInsets : CollapsedTitleBarInsets;
+            mLayout.setConstraints(this, c);
+        }
+    
+
+        public void paint(Graphics g) {
+            if (!isMac) {
+                // this is on by default on the mac
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+            }
+            super.paint(g);
+        }
+
+        public void paintComponent(Graphics g) {
+            paintGradient((Graphics2D)g);
+            super.paintComponent(g);
+        }
+
+        private void paintGradient(Graphics2D g)
+        {
+            g.setPaint(Gradient);
+            g.fillRect(0, 0, getWidth(), TitleHeight);
+        }
+
+    
     }
 
-    void setExpanded(boolean expanded) {
-        mWidget.setVisible(expanded);
-        //validate();
-    }
+}    
 
-}
-
-*/
