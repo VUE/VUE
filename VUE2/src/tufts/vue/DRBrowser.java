6f2c8c7984021c157d219e37667f81d875e88d1f
Scott Fraize <scott.fraize@gmail.com>
2006-03-20 18:10:31 +0000

    major cleanup: now optionally supports using a WidgetStack
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@2794 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/DRBrowser.java
+++ VUE2/src/tufts/vue/DRBrowser.java
@@ -26,93 +26,234 @@ import tufts.vue.gui.*;
 /**
  * Digital Repository Browser
  *
- * @version $Revision: 1.37 $ / $Date: 2006-03-03 20:58:40 $ / $Author: jeff $ 
+ * @version $Revision: 1.38 $ / $Date: 2006-03-20 18:10:31 $ / $Author: sfraize $ 
  */
-public class DRBrowser extends JPanel {
-    public static DataSourceViewer dsViewer = null;
-	public static tufts.vue.gui.DockWindow dockWindow = null;
+public class DRBrowser extends JPanel
+{
+    private static final boolean SingleDockWindowImpl = false;
+    
+    final JPanel librariesPanel;
+    final Widget searchPane = new Widget("Search");
+    final Widget browsePane = new Widget("Browse");
+    final Widget savedResourcesPane = new Widget("Saved Resources");
+    final Widget previewPane = new Widget("Preview");
 
-    private static final int DRBrowserWidth = 319;
-
+    final DockWindow dockWindow;
+    
     private JLabel loadingLabel;
-   
-    public DRBrowser(boolean delayedLoading,
-					 DockWindow librariesDWindow,
-					 DockWindow searchDWindow,
-					 DockWindow browseDWindow,
-					 DockWindow savedResourcesDWindow,
-					 DockWindow previewDWindow)
+    
+    public DRBrowser(boolean delayedLoading, DockWindow dockWindow)
     {
-        Dimension startSize = new Dimension(400,240);
-        // todo: move this size setting to VUE app init
-        //Dimension startSize = new Dimension(tufts.vue.gui.GUI.isSmallScreen() ? 250 : 400,
-        //                                    tufts.vue.gui.DockWindow.getMaxContentHeight());
-        
-        setLayout(new BorderLayout());
+        super(new BorderLayout());
+        setName("Libraries");
+        Dimension startSize = new Dimension(300,160);
         setPreferredSize(startSize);
+
+        this.dockWindow = dockWindow;
+        this.librariesPanel = this;
+
+        buildWidgets();
         
         if (delayedLoading) {
-            //setMinimumSize(new Dimension(DRBrowserWidth,100));
-            loadingLabel = new JLabel("Loading data sources...");
-            loadingLabel.setBorder(new EmptyBorder(22,22,22,22));
-            add(loadingLabel, BorderLayout.NORTH);
+            loadingLabel = new JLabel("Loading data sources...", SwingConstants.CENTER);
+            loadingLabel.setMinimumSize(new Dimension(startSize.width/2, startSize.height/2));
+            add(loadingLabel);
         } else {
-            loadDataSourceViewer(librariesDWindow,
-								 searchDWindow,
-								 browseDWindow,
-								 savedResourcesDWindow,
-								 previewDWindow);
+            loadDataSourceViewer();
         }
+
+        if (SingleDockWindowImpl)
+            buildSingleDockWindow();
+        else
+            buildMultipleDockWindows();
     }
-    
-    public DRBrowser() {
-        this(false,
-			 null,
-			 null,
-			 null,
-			 null,
-			 null);
+
+    private void buildSingleDockWindow() {
+        WidgetStack stack = new WidgetStack();
+
+        //searchPane.setExpanded(false);
+        //browsePane.setExpanded(false);
+        savedResourcesPane.setExpanded(false);
+
+        stack.addPane(searchPane, 0f);
+        stack.addPane(librariesPanel, 0f);
+        stack.addPane(browsePane, 1f);
+        stack.addPane(savedResourcesPane, 0f);
+
+        this.dockWindow.setContent(stack);
     }
-    
-	public void setDockWindow(tufts.vue.gui.DockWindow dWindow)
-	{
-		dockWindow = dWindow;
-	}
-	
-    public void loadDataSourceViewer(DockWindow librariesDWindow,
-									 DockWindow searchDWindow,
-									 DockWindow browseDWindow,
-									 DockWindow savedResourcesDWindow,
-									 DockWindow previewDWindow)
+
+    private void buildMultipleDockWindows()
     {
+        // make sure the loading label will be visible
+        this.dockWindow.setContent(librariesPanel);
+                              
+        // now create the stack of DockWindows
+        DockWindow drBrowserDock = this.dockWindow;
+        
+        DockWindow searchDock = GUI.createDockWindow(searchPane);
+        DockWindow browseDock = GUI.createDockWindow(browsePane);
+        DockWindow savedResourcesDock = GUI.createDockWindow(savedResourcesPane);
+        DockWindow previewDock = GUI.createDockWindow(previewPane);
+
+        
+        drBrowserDock.setStackOwner(true);
+        drBrowserDock.addChild(searchDock);
+        drBrowserDock.addChild(browseDock);
+        //drBrowserDock.addChild(previewDock);
+        previewDock.setLocation(300,300);
+        drBrowserDock.addChild(savedResourcesDock);
+        
+        searchDock.add(searchPane);
+        searchDock.setRolledUp(true);
+        
+        browseDock.add(browsePane);
+        browseDock.setRolledUp(true);
+        
+        savedResourcesDock.add(savedResourcesPane);
+        savedResourcesDock.setRolledUp(true);
+        
+        previewDock.add(previewPane);
+        previewDock.setRolledUp(true);
+
+    }
+    
+    private void buildWidgets()
+    {
+        Dimension startSize = new Dimension(tufts.vue.gui.GUI.isSmallScreen() ? 250 : 400,
+                                            100);        
+
+        //-----------------------------------------------------------------------------
+        // Search
+        //-----------------------------------------------------------------------------
+        
+        searchPane.setBackground(Color.white);
+        searchPane.setLayout(new BorderLayout());
+        searchPane.setPreferredSize(startSize);
+        searchPane.add(new JLabel("Please Select A Library"), BorderLayout.CENTER);
+		
+        //-----------------------------------------------------------------------------
+        // Local File Data Source
+        //-----------------------------------------------------------------------------
+        
         try {
-            DataSourceViewer dsv = new DataSourceViewer(this,
-														librariesDWindow,
-														searchDWindow,
-														browseDWindow,
-														savedResourcesDWindow,
-														previewDWindow);
-            dsv.setName("Data Source Viewer"); 
+            LocalFileDataSource localFileDataSource = new LocalFileDataSource("My Computer","");
+            browsePane.setBackground(Color.white);
+            browsePane.setLayout(new BorderLayout());
+            startSize = new Dimension(tufts.vue.gui.GUI.isSmallScreen() ? 250 : 400,
+                                      300);
+            if (true) startSize.height = GUI.GScreenHeight / 5;
+            //browsePanel.setPreferredSize(startSize);
+            JComponent comp = localFileDataSource.getResourceViewer();
+            comp.setVisible(true);
+            browsePane.add(comp);
+        } catch (Exception ex) {
+            if (DEBUG.DR) System.out.println("Problem loading local file library");
+        }
+		
+        //-----------------------------------------------------------------------------
+        // Saved Resources
+        //-----------------------------------------------------------------------------
+		
+        savedResourcesPane.setBackground(Color.white);
+        savedResourcesPane.setPreferredSize(startSize);
+        savedResourcesPane.add(new JLabel("saved resources"));
+		
+        //-----------------------------------------------------------------------------
+        // Preview
+        //-----------------------------------------------------------------------------
+		
+        previewPane.setBackground(Color.white);
+        previewPane.setPreferredSize(startSize);
+        previewPane.add(new JLabel("no preview"));
+        
+    }
+    
+    /*
+    public DRBrowser() {
+        this(false, null);
+    }
+    */
+    
+    /*
+    public void setDockWindow(tufts.vue.gui.DockWindow dWindow) {
+        dockWindow = dWindow;
+    }
+    */
+	
+    public void loadDataSourceViewer()
+    {
+        if (DEBUG.DR || DEBUG.Enabled) System.out.println("DRBrowser: loading the DataSourceViewer...");
+            
+        try {
+            DataSourceViewer dsv = new DataSourceViewer(this);
+            dsv.setName("Data Source Viewer");
+            /*
             if (dsViewer == null) {
                 // set the statics to the first initialized DRBrowser only
                 dsViewer = dsv;
                 //tufts.vue.VUE.dataSourceViewer = dsv;
             }
+            */
             if (loadingLabel != null)
-                remove(loadingLabel);
+                librariesPanel.remove(loadingLabel);
             //setMinimumSize(null); some data-sources smaller: don't allow shrinkage
-            add(dsv, BorderLayout.NORTH);
-            validate();
-        } catch (Exception e) {
+            //librariesPanel.setPreferredSize(null);
+            librariesPanel.add(dsv);
+
+            if (false) {
+                System.out.println("dsv == " + dsv);
+                System.out.println("dsv sz " + dsv.getSize());
+                System.out.println("dsv ps " + dsv.getPreferredSize());
+                System.out.println("DRB.SZ " + getSize());
+                System.out.println("DRB.PS " + getPreferredSize());
+                validate();
+                System.out.println("validate");
+                System.out.println("dsv sz " + dsv.getSize());
+                System.out.println("dsv ps " + dsv.getPreferredSize());
+                System.out.println("DRB.SZ " + getSize());
+                System.out.println("DRB.PS " + getPreferredSize());
+                
+                //setMinimumSize(dsv.getPreferredSize());
+            }
+
+            revalidate();
+            // must do this to get re-laid out: apparently, the hierarchy
+            // events from the add don't automatically do this!
+            
+            // TODO; As the DSV top-level is normally a scroll-pane, it's
+            // preferred/min size is useless (always will go down
+            // to one line), so either we'll need a manual size
+            // set anytime there's a scroll pane, or maybe the WidgetStack
+            // can detect the scroll pane, and look at the pref size of it's
+            // contents.
+            //setPreferredSize(dsv.getPreferredSize());
+            
+        } catch (Throwable e) {
+            VUE.Log.error(e);
             e.printStackTrace();
             loadingLabel.setText(e.toString());
         }
+        
+        if (DEBUG.DR || DEBUG.Enabled) System.out.println("DRBrowser: done loading DataSourceViewer.");
     }
 
-/*
-	public static void main(String args[]) {
+    public static void main(String args[])
+    {
+        VUE.init(args);
+        
         DEBUG.DR = true;
-        DRBrowser drb = new DRBrowser(false);
+        DockWindow dw = GUI.createDockWindow("Resources");
+        DRBrowser drb = new DRBrowser(true, dw);
+
+        dw.setVisible(true);
+
+        drb.loadDataSourceViewer();
+
+        if (args.length > 1)
+            ResourcePanel.displayTestResourcePanel(null);
+        
+        /*
         tufts.Util.displayComponent(drb);
         try {
             java.util.prefs.Preferences p = tufts.oki.dr.fedora.FedoraUtils.getDefaultPreferences(null);
@@ -120,11 +261,10 @@ public class DRBrowser extends JPanel {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //drb.loadDataSourceViewer();
+        */
         
     }
 
-*/
        
     
 }
