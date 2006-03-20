c0223150c8cefd8d05dffdcdf69892943f3c631e
Scott Fraize <scott.fraize@gmail.com>
2006-03-20 18:14:17 +0000

    massive comments on where to go next
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@2798 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/Resource.java
+++ VUE2/src/tufts/vue/Resource.java
@@ -30,7 +30,7 @@ package tufts.vue;
  *  implement.  Together, they create a uniform way to handle dragging and dropping of
  *  resource objects.
  *
- * @version $Revision: 1.37 $ / $Date: 2006-02-17 20:24:58 $ / $Author: jeff $
+ * @version $Revision: 1.38 $ / $Date: 2006-03-20 18:14:17 $ / $Author: sfraize $
  * @author  akumar03
  */
 import java.util.Properties;
@@ -39,6 +39,87 @@ import javax.swing.JComponent;
 // todo: consider adding an optional icon that can be set for the resource
 // todo fix: type isn't always being set in VUE code (e.g., VueDragTree),
 //      and type may not really belong here (e.g., ASSET & FAVORITE types too specific)
+
+// TODO:
+// add getSource (or: get Location/Repository/Collection/DataSource/Where) (e.g. "ArtStor", "Internet/Web", "Local File")
+//      may be a good enough replacement for type?  type currently mixes location with content
+//      a bit by adding "directory", which along with "file" are really just both "Local File" locations,
+//      and "Favorites" is a total odd man-out: any given resource that happens to be in the
+//      favorites list may actually be of any of the other given types).
+// get rid of spec, but maybe include a getPath, and include a hasURL/getURL as is too damn handy.
+// add getReader, getContent (need something to get an image object, or do we handle that
+//      via a cross-cutting concern?)
+// get rid of getExtension, tho we may be trying to get a mime-type with that one.
+//      it *would* be really nice if we could easily know if this is, say, an image or HTML content tho
+//      maybe we do add mime-type and punt with "type/unknown" for most everything else
+//      until we might someday be able to extract this info from the filesystem.  Still
+//      might be useful enough to add a special case "isImage" tho (or maybe hasImage?)
+//
+// get rid of getToolTipInformation: getTitle will be our only special case
+// mapping of information from either meta-data or the URL/file-name to something
+// exposed in the API.
+//
+// Get rid if selection methods.
+//
+// Maybe even get rid of displayContent: make a cross-cutting concern handled
+// by a ResourceHandler?  could theoretically put stuff like isImage/isHTML
+// there also, altho we could leave both of this in the API and address
+// the weight of it by having a good AbstractResource that handles all
+// this stuff for you.
+//
+// Oh, we'll still need something for persistance tho, which spec was handling.
+// Maybe getAddress?  getUniqueID (a GLOBAL id, which implies us coming up
+// with protocol names for every OSID resource, but we had that problem
+// anyway.  Hmm: we could actually use the class name of the OSID DR impl
+// and let that handle it?  (or really the DataSource, unless we get
+// rid of that).
+//
+// Maybe get rid of getPreview for now; may add in a set of getPreview / getViewer / getEditor later.
+//
+// getIcon: make getThumbnail?  this needs to be defined: is too generic: maybe get rid of.
+//      the Asset/DataSource logo should come from the DataSource.  So maybe we need
+//      a getDataSource, which is really what the getWhere ends up being, and returns
+//      an object instead of a name.  Tho is of course handy for not just thumbnails,
+//      but say type-based application icons (from OS meta-data), or say the favicon.ico
+//      from a website.
+//
+// AND: if we ever get int getting "parts", maybe we should throw this whole thing out
+//      use the DR API itself?  a bit heavy weight tho.
+//
+// And don't forget: altho we want to handle "any kind of data source", the majority
+// of them are probably all going to be URL based, at least for the forseeabe future,
+// which is a practical consideration worth keeping in mind.
+
+// So the basic services being provided are:
+//      1 - get data for VUE (provide a handle for it)
+//      2 - get meta-data about the data (including the all important where this thing came from)
+//      3 - provide persistance of the reference to the data for VUE
+//      4 - provide some convenince wrapping of meta-data to tell us some very
+//          useful things such as:
+//              - a title
+//              - a URL if available
+//              - what we can do with this data (it's type), such is isImage, isHTML, and/or getMimeType
+//
+// Items 1-3 are an absolute requiment.  Items in 4 are for things useful
+// enough to include in the API, but only priority items worth expanding
+// the API for (these items are all computed from the meta-data, both properties based and DataSource based)
+//
+// Could also consider calling this an "Asset", tho that may be just too confusing given OKI & Fedora.
+//
+// Possibly add a "getName", to be the raw stub of a URL v.s. a massaged one for getTitle,
+// or former v.s. the meta-data title of a properly title document/image from a repository.
+// This is pure convience for a GUI / possibly "important" data for a user.  Maybe only
+// really for local files tho.  Could opt out of it but putting it in title and just
+// not having a massaged version that leaves (that does stuff like create spaces, removes
+// the extension, etc).
+
+
+
+// Note: the inclusion of "setProperty" has huge implications, which means
+// writing back to the underlying repository, unless we're just going to
+// provide local masking/extension of that data, which I recommend against.
+// If we eventually want write-back, this could be provided via the DataSource.
+// Let's get rid of this?
 
 public interface Resource 
 {
