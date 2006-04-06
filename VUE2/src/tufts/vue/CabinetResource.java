112789d1487d9b6ee83f58ec1d5bcf2470964fb1
Scott Fraize <scott.fraize@gmail.com>
2006-04-06 01:36:00 +0000

    resource api change
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@2969 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/CabinetResource.java
+++ VUE2/src/tufts/vue/CabinetResource.java
@@ -38,7 +38,7 @@ import java.awt.*;
  *  A wrapper for CabinetEntry objects which can be used as the user object in a 
  *  DefaultMutableTreeNode.  It implements the Resource interface specification.
  *
- * @version $Revision: 1.19 $ / $Date: 2006-03-29 04:56:51 $ / $Author: sfraize $
+ * @version $Revision: 1.20 $ / $Date: 2006-04-06 01:36:00 $ / $Author: sfraize $
  * @author  Mark Norton
  */
 public class CabinetResource extends MapResource {
@@ -166,7 +166,7 @@ public class CabinetResource extends MapResource {
         
         //  Check for a restored resource.
         if (this.entry == null)
-            return (Properties)this.mProperties;
+            return super.getProperties();//(Properties)this.mProperties;
         else {
             try {if (this.entry instanceof tufts.oki.remoteFiling.RemoteByteStore)
                 if (this.entry instanceof RemoteByteStore) {
@@ -226,7 +226,8 @@ public class CabinetResource extends MapResource {
             catch (osid.shared.SharedException ex2) {
                 //  If we get an exception, just return what we got.
             }
-            this.mProperties = props;            //  Cache the metadata.
+            //this.mProperties = props;            //  Cache the metadata.
+            System.err.println("Unimplemented: filing properties on resource");
             return props;
         }
     }
