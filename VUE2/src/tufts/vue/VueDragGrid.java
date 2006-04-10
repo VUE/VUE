7beabbefaab379e7e081e831666d727122e51d69
Jeffrey Kahn <verbenaconsulting@mac.com>
2006-04-10 19:36:28 +0000

    new search results list
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@3022 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/VueDragGrid.java
+++ VUE2/src/tufts/vue/VueDragGrid.java
@@ -39,212 +39,62 @@ import osid.filing.*;
 import tufts.oki.remoteFiling.*;
 import tufts.oki.localFiling.*;
 
-
-import javax.swing.tree.*;
 import java.util.Iterator;
 
 /**
  *
- * @version $Revision: 1.5 $ / $Date: 2006-04-10 18:49:45 $ / $Author: sfraize $
+ * @version $Revision: 1.6 $ / $Date: 2006-04-10 19:36:28 $ / $Author: jeff $
  * @author  rsaigal
  */
-public class VueDragGrid extends JTree
-    implements DragGestureListener,
-               DragSourceListener,
-               TreeSelectionListener,
-               ActionListener
+public class VueDragGrid extends JList
+implements DragGestureListener, DragSourceListener
 {
-    
-    public static ResourceNode oldnode;
-    private ResourceSelection resourceSelection = null;
-    private static  ImageIcon nleafIcon = VueResources.getImageIcon("favorites.leafIcon") ;
-    private static ImageIcon inactiveIcon = VueResources.getImageIcon("favorites.inactiveIcon") ;
-    private static  ImageIcon activeIcon = VueResources.getImageIcon("favorites.activeIcon") ;
-    ///private javax.swing.JPanel previewPanel = null;
-    //	private tufts.vue.gui.DockWindow previewDockWindow = null;
+	private javax.swing.DefaultListModel model = new javax.swing.DefaultListModel();
+    private static ImageIcon nleafIcon = VueResources.getImageIcon("favorites.leafIcon") ;
+	
+	public VueDragGrid(Iterator iterator)
+	{
+		while (iterator.hasNext()) {
+			Resource resource = (Osid2AssetResource)iterator.next();
+			Icon icon = resource.getIcon(76,76);
+//			icon.setToolTipText(resource.getTitle());
+			model.addElement(icon);
+			setFixedCellHeight(80);
+		}
+		setModel(model);
+        //implementDrag(this);
+		DefaultListSelectionModel selectionModel = new DefaultListSelectionModel();
+		selectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		setSelectionModel(selectionModel);
+	}
 
-    public VueDragGrid(Object  obj, String treeName) {
-        setModel(createTreeModel(obj, treeName));
-        setName(treeName);
-        setRowHeight(36);
-        this.setRootVisible(true);
-        this.expandRow(0);
-        this.expandRow(1);
-        this.setRootVisible(false);
-        implementDrag(this);
-        createPopupMenu();
-        
-        getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-        //getSelectionModel().setSelectionMode(TreeSelectionModel.CONTIGUOUS_TREE_SELECTION);
-
-       resourceSelection = VUE.getResourceSelection();
-       addTreeSelectionListener(this);
-
-       addMouseListener(new MouseAdapter() {    
-               public void mouseClicked(MouseEvent me){
-                   if  (me.getClickCount() != 2)
-                       return;
-                   
-                   TreePath path = getPathForLocation(me.getX(), me.getY());
-                   if (path == null)
-                       return;
-                   
-                   Object c = path.getLastPathComponent();
-                   if (c instanceof CabinetNode) {
-                       CabinetNode cabNode = (CabinetNode) path.getLastPathComponent();
-                       Object uo = cabNode.getUserObject();
-                       if (uo instanceof Resource)
-                           ((Resource)uo).displayContent();
-                   }
-                   
-               }
-           });
-        
-    }
-    
-    public VueDragGrid(FavoritesNode favoritesNode)
-    {
-        setModel(new DefaultTreeModel(favoritesNode));
-        
-        this.expandRow(0);
-        createPopupMenu();
-        
-        implementDrag(this);
-        resourceSelection = VUE.getResourceSelection();
-        addTreeSelectionListener(this);
-    }
-    
-    
-    private void  implementDrag(VueDragGrid tree){
+	private void  implementDrag(VueDragGrid list){
         DragSource dragSource = DragSource.getDefaultDragSource();
-        dragSource.createDefaultDragGestureRecognizer(tree,
+        dragSource.createDefaultDragGestureRecognizer(list,
                                                       DnDConstants.ACTION_COPY |
                                                       DnDConstants.ACTION_MOVE |
                                                       DnDConstants.ACTION_LINK,
-                                                      tree);
-        addTreeExpansionListener(new TreeExpansionListener(){
-            public void treeCollapsed(TreeExpansionEvent e) {}
-            public void treeExpanded(TreeExpansionEvent e) {
-                TreePath path = e.getPath();
-                if(path != null) {
-                    if (path.getLastPathComponent() instanceof FileNode){
-                        FileNode node = (FileNode)path.getLastPathComponent();
-                        if( !node.isExplored()) {
-                            DefaultTreeModel model =
-                            (DefaultTreeModel)getModel();
-                            node.explore();
-                            model.nodeStructureChanged(node);
-                        }
-                    }
-                }
-            }
-        });
-        //  Add a tree will expand listener.
-        addTreeWillExpandListener(new TreeWillExpandListener() {
-            public void treeWillExpand(TreeExpansionEvent e) {
-                TreePath path = e.getPath();
-                if (path.getLastPathComponent() instanceof CabinetNode) {
-                    CabinetNode cabNode = (CabinetNode) path.getLastPathComponent();
-                  
-                    if (cabNode == null) return;
-                    
-                    setSelectionPath(path);
-                      
-                   if (cabNode.getCabinet() != null)cabNode.getDataModel().reload();
-                 
-                    
-                   
-                }
-            }
-            public void treeWillCollapse(TreeExpansionEvent e) {}
-        });
-        
-        
-        VueDragGridCellRenderer renderer = new VueDragGridCellRenderer(tree);
-        tree.setCellRenderer(renderer);
-        
-        
-        ToolTipManager.sharedInstance().registerComponent(tree);
+                                                      list);
     }
-    
-    
-    
-    
-    private DefaultTreeModel createTreeModel(Object obj, String treeName ){
-        
-        ResourceNode root = new ResourceNode(new MapResource(treeName));
-        
-        if (obj instanceof Iterator){
-            
-            Iterator i = (Iterator)obj;
-            
-            while (i.hasNext()){
-                Object resource = i.next();
-         
-                if (resource instanceof CabinetResource) {
-                    CabinetResource cabRes = (CabinetResource) resource;
-                    CabinetEntry entry = cabRes.getEntry();
-                    CabinetNode cabNode = null;
-                    if (entry instanceof RemoteCabinetEntry)
-                        cabNode = new CabinetNode(cabRes, CabinetNode.REMOTE);
-                    else
-                        
-                        cabNode = new CabinetNode(cabRes, CabinetNode.LOCAL);
-                    //System.out.println("ADDING CABNODE " + cabNode + " resource title " + cabRes.getTitle());
-                    root.add(cabNode);
-                    //System.out.println(" I am here in Vue drag" + cabNode.getCabinet());
-                   if (cabNode.getCabinet() != null)cabNode.explore();
-                    //root.add(new ResourceNode((Resource)resource));
-                } else {
-                    ResourceNode node = new ResourceNode((Resource)resource);
-                    root.add(node);
-                }
-            }
-        }
-        return new DefaultTreeModel(root);
-    }
-    
-    //****************************************
     
     public void dragGestureRecognized(DragGestureEvent e) {
       
-        if (getSelectionPath() != null) {
-            TreePath path = getLeadSelectionPath();
-            oldnode = (ResourceNode)path.getLastPathComponent();
-            ResourceNode parentnode = (ResourceNode)oldnode.getParent();
-
-            //Object resource = getObject();
-            Resource resource = oldnode.getResource();
-            
-            if (DEBUG.DND) System.out.println(this + " dragGestureRecognized " + e);
-            if (DEBUG.DND) System.out.println("selected node is " + oldnode.getClass() + "[" + oldnode + "] resource=" + resource);
-
-            if (resource != null) {
-
-                Image imageIcon = nleafIcon.getImage();
-                if (resource.getType() == Resource.DIRECTORY) {
-                    imageIcon = activeIcon.getImage();
-                } else if (oldnode instanceof CabinetNode) {
-                    CabinetNode cn = (CabinetNode) oldnode;
-                    if (!cn.isLeaf())
-                        imageIcon = activeIcon.getImage();
-                }
-                
-                e.startDrag(DragSource.DefaultCopyDrop, // cursor
-                            imageIcon, // drag image
-                            new Point(-10,-10), // drag image offset
-                            new tufts.vue.gui.GUI.ResourceTransfer(resource),
-                            //new VueDragGridNodeSelection(resource), // transferable
-                            this);  // drag source listener
-            }
-        }
+		System.out.println(getSelectedIndex());
+        if (getSelectedIndex() != -1) {
+			Resource resource = (Osid2AssetResource)getSelectedValue();
+			e.startDrag(DragSource.DefaultCopyDrop, // cursor
+						nleafIcon.getImage(), // drag image
+						new Point(-10,-10), // drag image offset
+						new tufts.vue.gui.GUI.ResourceTransfer(resource),
+						this);  // drag source listener
+		}
     }
     
     
     public void dragDropEnd(DragSourceDropEvent e) {
         if (tufts.vue.VUE.dropIsLocal == true){
-            DefaultTreeModel model = (DefaultTreeModel)this.getModel();
-            model.removeNodeFromParent(oldnode);
+            //DefaultListModel model = (DefaultListModel)this.getModel();
+            //model.removeElementAt(oldItem);
             tufts.vue.VUE.dropIsLocal = false;
         }
     }
@@ -255,142 +105,5 @@ public class VueDragGrid extends JTree
     public void dropActionChanged(DragSourceDragEvent e) {
         if (DEBUG.DND) System.out.println("VueDragGrid: dropActionChanged  to  " + tufts.vue.gui.GUI.dropName(e.getDropAction()));
     }
-    
-    
-    public Object getObject() {
-        TreePath path = getLeadSelectionPath();
-        if (path == null)
-            return null;
-        DefaultMutableTreeNode node = (DefaultMutableTreeNode)path.getLastPathComponent();
-        return (node.getUserObject());
-    }
-    
-    public void valueChanged(TreeSelectionEvent e) {
-        try {
-            if(e.getPath().getLastPathComponent() != null ) {
-                Resource resource = (Resource)((ResourceNode)e.getPath().getLastPathComponent()).getResource();
-                resourceSelection.setTo(resource);
-				
-                /*
-                this.previewDockWindow.setVisible(true);
-                this.previewPanel.removeAll();
-                this.previewPanel.add(resource.getPreview());
-                this.previewPanel.repaint();
-                this.previewPanel.validate();
-                */
-                //resourceSelection.remove(createResource(e.getPath().getLastPathComponent()));
-            }
-            /**
-         if(e.getPath().getPathComponent(0) != null) {
-             //resourceSelection.add(createResource(e.getPath().getLastPathComponent()));
-             System.out.println("Added Resource = "+createResource(e.getPath().getLastPathComponent())+" : size = "+resourceSelection.size());
-         }
-             **/
-        } catch(Exception ex) {
-            // VueUtil.alert(null,ex.toString(),"Error in VueDragGrid Selection");
-            System.out.println("VueDragGrid.valueChanged "+ex.getMessage());
-            ex.printStackTrace();
-        }
-        // System.out.println("elements in path = "+e.getPath().getPathCount());
-    }
-    
-    static class VueDragGridCellRenderer extends DefaultTreeCellRenderer{
-        String meta = "";
-        protected VueDragGrid tree;   
-		
-        public VueDragGridCellRenderer(VueDragGrid vdTree) {
-            this.tree = vdTree;
-        }
-		
-        public Component getTreeCellRendererComponent(
-                                                      JTree tree,
-                                                      Object value,
-                                                      boolean sel,
-                                                      boolean expanded,
-                                                      boolean leaf,
-                                                      int row,
-                                                      boolean hasFocus)
-        {
-            super.getTreeCellRendererComponent(tree, value, sel,
-                                               expanded, leaf, row,
-                                               hasFocus);
-            if (leaf) {
-                //Osid2AssetResource resource = (Osid2AssetResource)((DefaultMutableTreeNode)value).getUserObject();
-                Resource resource = (Resource)((DefaultMutableTreeNode)value).getUserObject();
-                setIcon(resource.getIcon(32,32));
-            }
-            return this;				 
-        }
-        
-        public String getToolTipText(){
-            return meta;
-        }
-    }
-    
-    
-    public void createPopupMenu() {
-        JMenuItem menuItem;
-        
-        //Create the popup menu.
-        JPopupMenu popup = new JPopupMenu();
-        menuItem = new JMenuItem("Open Resource");
-        menuItem.addActionListener(this);
-        popup.add(menuItem);
-       
-        
-        
-        //Add listener to the text area so the popup menu can come up.
-        MouseListener popupListener = new PopupListener(popup);
-        this.addMouseListener(popupListener);
-    }
-    public void actionPerformed(ActionEvent e) {
-        
-        if (e.getSource() instanceof JMenuItem){
-            
-            
-            JMenuItem source = (JMenuItem)(e.getSource());
-            TreePath tp = this.getSelectionPath();
-            
-           
-            
-            if (tp != null){
-                ResourceNode resNode = (ResourceNode)tp.getLastPathComponent();
-                resNode.getResource().displayContent();
-            }
-        }
-    }
-    
- class PopupListener extends MouseAdapter {
-    JPopupMenu popup;
-    
-    
-    PopupListener(JPopupMenu popupMenu) {
-        popup = popupMenu;
-    }
-    
-    public void mousePressed(MouseEvent e) {
-        maybeShowPopup(e);
-    }
-    
-    public void mouseClicked(MouseEvent e) {
-        maybeShowPopup(e);
-    }
-    
-    public void mouseReleased(MouseEvent e) {
-        maybeShowPopup(e);
-    }
-    
-    private void maybeShowPopup(MouseEvent e) {
-        if (VueDragGrid.this.getSelectionPath() != null){
-        if (e.isPopupTrigger()) {
-            
-            
-            popup.show(e.getComponent(),
-            e.getX(), e.getY());
-            
-        }
-    }
-    }
-}
 }
 
