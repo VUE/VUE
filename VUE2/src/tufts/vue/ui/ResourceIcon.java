9729609356329f4412e7fb49f25b578ba50d1a4a
Scott Fraize <scott.fraize@gmail.com>
2006-04-11 05:28:36 +0000

    fixed rounding error
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@3035 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/ui/ResourceIcon.java
+++ VUE2/src/tufts/vue/ui/ResourceIcon.java
@@ -34,7 +34,7 @@ import javax.swing.*;
  * TODO: merge common code with PreviewPane, and perhaps put in a 3rd class
  * so can have multiple icons referencing the same underlying image.
  *
- * @version $Revision: 1.4 $ / $Date: 2006-04-08 01:53:07 $ / $Author: sfraize $
+ * @version $Revision: 1.5 $ / $Date: 2006-04-11 05:28:36 $ / $Author: sfraize $
  * @author Scott Fraize
  */
 
@@ -49,7 +49,7 @@ public class ResourceIcon
     private int mImageWidth;
     private int mImageHeight;
     private boolean isLoading = false;
-    private Component mParent;
+    private Component mPainter = null;
 
     private int mWidth = -1;
     private int mHeight = -1;
@@ -57,16 +57,35 @@ public class ResourceIcon
     //private final JLabel StatusLabel = new JLabel("(status)", JLabel.CENTER);
 
 
-    /** Act like a regular Icon */
-    public ResourceIcon(Resource r, int width, int height) {
+    /**
+     *
+     * This constructor will be needed if the icon is to be placed into a component that
+     * uses cell renderers, such as JList, JTable, or JTree.  In the normal course,
+     * ResourceIcon's repaint when their data is loaded, and to do so they remember the
+     * component they're being painted on, and ask it to repaint when the data comes in.
+     *
+     * But this repaint doesn't work if what they're being asked to paint on is a renderer.
+     *
+     * The problem with renderer's is that "repaint" is overriden and defined as a
+     * no-op, as they're only used to temporarily take on and draw their values when
+     * they're asked to paint.
+     *
+     **/
+    public ResourceIcon(Resource r, int width, int height, Component painter) {
         setSize(width, height);
         //StatusLabel.setVisible(false);
         //add(StatusLabel);
         //loadResource(r);
         mResource = r;
+        mPainter = painter;
         //out("Constructed " + width + "x" + height + " " + r);
     }
         
+    /** Act like a regular Icon */
+    public ResourceIcon(Resource r, int width, int height) {
+        this(r, width, height, null);
+    }
+
     /** Expand's to fit the size of the component it's painted into */
     public ResourceIcon() {}
 
@@ -79,9 +98,9 @@ public class ResourceIcon
     }
 
     private void repaint() {
-        if (mParent != null) {
-            //if (DEBUG.IMAGE) out("repaint in " + GUI.name(mParent));
-            mParent.repaint();
+        if (mPainter != null) {
+            //if (DEBUG.IMAGE) out("repaint in " + GUI.name(mPainter));
+            mPainter.repaint();
         }
         // FYI, this will not repaint if the parent is a DefaultTreeCellRenderer,
         // and the parent of the parent (a JLabel), is null, so we can't get that.
@@ -220,8 +239,12 @@ public class ResourceIcon
     public void paintIcon(Component c, Graphics g, int x, int y)
     {
         final boolean expandToFit = (mWidth < 1);
-        
-        mParent = c;
+
+        if (mPainter == null) {
+            // note this means repaint updates would stop in a new parent,
+            // tho assuming it's loaded by then, regular paints would work fine.
+            mPainter = c;
+        }
         
         if (DEBUG.IMAGE) out("paint, parent=" + GUI.name(c));
 
@@ -287,8 +310,8 @@ public class ResourceIcon
         }
             
 
-        final int drawW = (int) (mImageWidth * zoomFit);
-        final int drawH = (int) (mImageHeight * zoomFit);
+        final int drawW = (int) (mImageWidth * zoomFit + 0.5);
+        final int drawH = (int) (mImageHeight * zoomFit + 0.5);
                                                      
         int xoff = x;
         int yoff = y;
@@ -305,9 +328,16 @@ public class ResourceIcon
             g.clipRect(x, y, mWidth, mHeight);
         }
             
-        if (DEBUG.IMAGE) out("painting " + Util.tag(mImage));
+        if (DEBUG.IMAGE) out("painting " + Util.tag(mImage) + " as " + drawW + "x" + drawH);
         g.drawImage(mImage, xoff, yoff, drawW, drawH, null);
 
+        if (DEBUG.BOXES) {
+            g.setColor(Color.green);
+            ((Graphics2D)g).setComposite(java.awt.AlphaComposite.getInstance(java.awt.AlphaComposite.SRC_OVER, 0.2f));
+            g.drawRect(x, y, mWidth-1, mHeight-1);
+            ((Graphics2D)g).setComposite(java.awt.AlphaComposite.SrcOver);
+        }
+        
         if (CropToSquare && !expandToFit)
             g.setClip(oldClip);
     }
