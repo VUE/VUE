6e6cc50fe88a759987cc15f363dc31e15e92ef62
Jeffrey Kahn <verbenaconsulting@mac.com>
2006-05-01 19:32:54 +0000

    test for Google Local
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@3170 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/test/src/repository/google/local/Type.java
@@ -0,0 +1,39 @@
+/*
+ * -----------------------------------------------------------------------------
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2006 
+ * Tufts University. All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+package test.repository.google.local;
+
+public class Type
+extends org.osid.shared.Type
+{
+
+    public Type(String authority
+               , String domain
+               , String keyword
+               , String description)
+    {
+        super(authority,domain,keyword,description);
+    }
+
+    public Type(String authority
+               , String domain
+               , String keyword)
+    {
+        super(authority,domain,keyword);
+    }
+}
