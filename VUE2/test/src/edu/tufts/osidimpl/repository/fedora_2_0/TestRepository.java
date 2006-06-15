35ba11a9da199406ffcfbef3a8afa999b7ee8c58
Anoop Kumar <anoopkum@gmail.com>
2006-06-15 13:45:29 +0000

    added test that results
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@3408 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/test/src/edu/tufts/osidimpl/repository/fedora_2_0/TestRepository.java
+++ VUE2/test/src/edu/tufts/osidimpl/repository/fedora_2_0/TestRepository.java
@@ -26,9 +26,61 @@ public class TestRepository extends TestCase {
                     "edu.tufts.osidimpl.repository.fedora_2_0",
                     context,
                     new java.util.Properties());
+            
+            
         } catch(Throwable t) {
             t.printStackTrace();
         }
+        try {
+            org.osid.OsidContext context = new org.osid.OsidContext();
+			java.util.Properties properties = new java.util.Properties();
+			properties.setProperty("fedora20ConfigurationFilename","fedora.xml");
+			properties.setProperty("fedora20DisplayName","Tufts Digital Library");
+			properties.setProperty("fedora20Address","dl.tufts.edu");
+			properties.setProperty("fedora20Port","8080");
+			properties.setProperty("fedora20UserName","test");
+			properties.setProperty("fedora20Password","test");
+			org.osid.repository.RepositoryManager repositoryManager = (org.osid.repository.RepositoryManager)org.osid.OsidLoader.getManager("org.osid.repository.RepositoryManager",
+																																			"edu.tufts.osidimpl.repository.fedora_2_0",
+																																			context,
+																																			properties);     
+			
+			org.osid.repository.RepositoryIterator ri = repositoryManager.getRepositories();
+			while (ri.hasNextRepository()) {
+				org.osid.repository.Repository repository = ri.nextRepository();
+				System.out.println("Next repository " + repository.getDisplayName() + " " + repository.getId().getIdString());
+				org.osid.shared.TypeIterator ti = repository.getSearchTypes();
+                while (ti.hasNextType()) System.out.println(ti.nextType().getKeyword());
+				System.out.println("starting search");
+                org.osid.repository.AssetIterator assetIterator = repository.getAssetsBySearch("Jumbo",new Type("mit.edu","search","keyword"),null);
+                while (assetIterator.hasNextAsset())
+                {
+                    org.osid.repository.Asset asset = assetIterator.nextAsset();
+                    System.out.println("Asset " + asset.getDisplayName());
+					
+                    System.out.println("Description " + asset.getDescription());
+					System.out.println("Id " + asset.getId().getIdString());
+					
+					org.osid.repository.RecordIterator recordIterator = asset.getRecords();
+					while (recordIterator.hasNextRecord()) {
+						org.osid.repository.Record sourceRecord = recordIterator.nextRecord();
+						org.osid.repository.PartIterator partIterator = sourceRecord.getParts();
+						while (partIterator.hasNextPart()) {
+							org.osid.repository.Part sourcePart = partIterator.nextPart();
+							if (sourcePart.getValue() != null) {
+								System.out.println();
+								System.out.println("Part Type: " + sourcePart.getPartStructure().getType().getKeyword());
+								System.out.println("Value: " + sourcePart.getValue());
+							}
+						}
+					}
+                }
+                System.out.println();
+            }
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+
         
     }
     
