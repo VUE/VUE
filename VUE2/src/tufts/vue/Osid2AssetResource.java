df8bd69fcd9fbae37312789a6badb8ecdaeeb08d
Scott Fraize <scott.fraize@gmail.com>
2006-08-03 05:32:19 +0000

    comment
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@3651 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/Osid2AssetResource.java
+++ VUE2/src/tufts/vue/Osid2AssetResource.java
@@ -36,6 +36,7 @@ public class Osid2AssetResource extends MapResource
     private static final org.osid.shared.Type LargeImagePartType = new edu.tufts.vue.util.Type("mit.edu","partStructure","largeImage");
 
 
+    // SEE Fedora Impl: edu/tufts/osidimpl/repository/fedora_2_0/ImageRecordStructure.java
     private static final org.osid.shared.Type FedoraImagePartType = // TODO: temporary hack for Fedora (also: note differing authority conventions!
         new edu.tufts.vue.util.Type("edu.mit","partStructure","mediumImage");
     // TODO: Google Global is currently using edu.mit authority also for URL part type...
