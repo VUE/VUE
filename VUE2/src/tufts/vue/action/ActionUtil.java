30f12cf7c63e895e98517b3a56cfdf2cb1813a35
Scott Fraize <scott.fraize@gmail.com>
2006-08-10 21:33:03 +0000

    added code to guess the encoding of 2nd generation save files (3rd gen is all US-ASCII, 2nd gen was unspecified, thus would depend on platform and localization)
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@3676 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/action/ActionUtil.java
+++ VUE2/src/tufts/vue/action/ActionUtil.java
@@ -18,6 +18,7 @@
 
 package tufts.vue.action;
 
+import tufts.Util;
 import tufts.vue.VueUtil;
 import tufts.vue.VUE;
 import tufts.vue.LWMap;
@@ -51,7 +52,7 @@ import java.io.*;
  * A class which defines utility methods for any of the action class.
  * Most of this code is for save/restore persistance thru castor XML.
  *
- * @version $Revision: 1.43 $ / $Date: 2006-07-31 18:31:36 $ / $Author: sfraize $
+ * @version $Revision: 1.44 $ / $Date: 2006-08-10 21:33:03 $ / $Author: sfraize $
  * @author  Daisuke Fujiwara
  * @author  Scott Fraize
  */
@@ -62,20 +63,20 @@ public class ActionUtil {
     private final static URL XML_MAPPING_UNVERSIONED =  VueResources.getURL("mapping.lw.version_none");
     private final static URL XML_MAPPING_OLD_RESOURCES =VueResources.getURL("mapping.lw.version_resource_fix");
 
-    //private final static String OUTPUT_ENCODING = "UTF-8";
+    private final static String VUE_COMMENT_START = "<!-- Do Not Remove:";
     private final static String OUTPUT_ENCODING = "US-ASCII";
+    private final static String DEFAULT_WINDOWS_ENCODING = "windows-1252"; // (a.k.a Cp1252) for reading pre ASCII enforced save files from Windows
+    private final static String DEFAULT_MAC_ENCODING = "UTF-8"; // "MacRoman" not supported on Windows platform
+    private final static String DEFAULT_INPUT_ENCODING = "UTF-8"; // safest default input encoding
 
-    // Note: the encoding format of the incoming file will normally
-    // either be UTF-8 for older VUE save files, or US-ASCII for newer
-    // files.  In any case, the encoding is indicated in the <?xml>
-    // tag at the top of the file, and castor handles adjusting for
-    // it.  If we want to write UTF-8 files, we have to be sure the
-    // stream that's created to write the file is created with the
-    // same encoding, or we sometimes get problems, depending on the
-    // platform.  We always READ (unmarshall) via a UTF-8 stream, no
-    // matter what, as US-ASCII will pass through a UTF-8 stream
-    // untouched, and it will handle UTF-8 if that turns out to be the
-    // encoding.
+    // Note: the encoding format of the incoming file will normally either be UTF-8 for
+    // older VUE save files, or US-ASCII for newer files.  In any case, the encoding is
+    // indicated in the <?xml> tag at the top of the file, and castor handles adjusting
+    // for it.  If we want to write UTF-8 files, we have to be sure the stream that's
+    // created to write the file is created with the same encoding, or we sometimes get
+    // problems, depending on the platform.  We always READ (unmarshall) via a UTF-8
+    // stream, no matter what, as US-ASCII will pass through a UTF-8 stream untouched,
+    // and it will handle UTF-8 if that turns out to be the encoding.
     
     public ActionUtil() {}
     
@@ -306,25 +307,32 @@ public class ActionUtil {
         try {  
             final String path = file.getAbsolutePath().replaceAll("%20"," ");
             final Writer writer;
-            if (OUTPUT_ENCODING.equals("UTF-8") || OUTPUT_ENCODING.equals("UTF8"))
+            if (OUTPUT_ENCODING.equals("UTF-8") || OUTPUT_ENCODING.equals("UTF8")) {
                 writer = new OutputStreamWriter(new FileOutputStream(path), OUTPUT_ENCODING);
-            else
-                writer = new FileWriter(path); // we can use the local encoding if writing in US-ASCII (pretty much any encoding)
+            } else {
+                writer = new FileWriter(path);
+                // For the actual file writer we can use the default encoding because
+                // we're marshalling specifically in US-ASCII.  E.g., because we direct
+                // castor to fully encode any special characters via
+                // setEncoding("US-ASCII"), we'll only have ASCII chars to write anyway,
+                // and any default encoding will handle that...
+                
+            }
             
-            writer.write("<!-- Do Not Remove:"
+            writer.write(VUE_COMMENT_START
                          + " VUE mapping "
                          + "@version(" + XML_MAPPING_CURRENT_VERSION_ID + ")"
                          + " " + XML_MAPPING_DEFAULT
                          + " -->\n");
-            writer.write("<!-- Do Not Remove:"
-                         + " Saved " + new java.util.Date()
+            writer.write(VUE_COMMENT_START
+                         + " Saved date " + new java.util.Date()
                          + " by " + VUE.getSystemProperty("user.name")
                          + " on platform " + VUE.getSystemProperty("os.name")
                          + " " + VUE.getSystemProperty("os.version")
                          + " in JVM " + VUE.getSystemProperty("java.runtime.version")
                          + " -->\n");
-            writer.write("<!-- Do Not Remove:"
-                         + " Saved by " + tufts.vue.Version.WhatString
+            writer.write(VUE_COMMENT_START
+                         + " Saving version " + tufts.vue.Version.WhatString
                          + " -->\n");
             if (DEBUG.CASTOR || DEBUG.IO) System.out.println("Wrote VUE header to " + writer);
             marshaller = new Marshaller(writer);
@@ -412,64 +420,99 @@ public class ActionUtil {
     }
 
     /** Unmarshall a LWMap from the given URL (XML map data) */
+    /*
     public static LWMap unmarshallMap(java.net.URL url)
         throws java.io.IOException
     {
         //return unmarshallMap(url, getDefaultMapping());
         return unmarshallMap(url, null);
     }
+    */
 
     /** Unmarshall a LWMap from the given URL using the given mapping */
+    /*
     private static LWMap unmarshallMap(java.net.URL url, Mapping mapping)
         throws java.io.IOException
     {
-        LWMap map = null;
+        return unmarshallMap(url, mapping, DEFAULT_INPUT_ENCODING);
+    }
+    */
+    
+    public static LWMap unmarshallMap(java.net.URL url)
+        throws java.io.IOException
+    {
+        // We scan for lines at top of file that are comments.  If there are NO comment lines, the
+        // file is of one of our original save formats that is not versioned, and that may need
+        // special processing for the Resource class to Resource interface change over.  If there
+        // are comments, the version instance of the string "@version(##)" will set the version ID
+        // to ##, and we'll use the mapping appropriate for that version of the save file.
 
-        // We scan for lines at top of file that are comments.  If
-        // there are NO comment lines, the file is of one of our
-        // original save formats that is not versioned, and that may
-        // need special processing for the Resource class to Resource
-        // interface change over.  If there are comments, the version
-        // instance of the string "@version(##)" will set the version
-        // ID to ##, and we'll use the mapping appropriate for that
-        // version of the save file.
+        // We ALWAYS read with an input encoding of UTF-8, even if the XML was written with a
+        // US-ASCII encoding.  This is because pure ascii will translate fine through UTF-8, but in
+        // case it winds up being that the XML was written out my the marshaller with a UTF-8
+        // encoding, we're covered.
 
-        // We ALWAYS read with an input encoding of UTF-8, even if the
-        // XML was written with a US-ASCII encoding.  This is because
-        // pure ascii will translate fine through UTF-8, but in case
-        // it winds up being that the XML was written out my the
-        // marshaller with a UTF-8 encoding, we're covered.
-
-        // (tho maybe with very old save files with platform specific
-        // encodings, (e.g, MacRoman or windows-1255/Cp1255) we'll 
-        // lose a special char here or there, such as left-quote / right-quote).
+        // (tho maybe with very old save files with platform specific encodings, (e.g, MacRoman or
+        // windows-1255/Cp1255) we'll lose a special char here or there, such as left-quote /
+        // right-quote).
         
-        if (DEBUG.CASTOR || DEBUG.IO) System.out.println("\nUNMARSHALLING: " + url);
+        if (DEBUG.CASTOR || DEBUG.IO) System.out.println("\nSCANNING: " + url);
         
-        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
-        String line;
-        int commentCount = 0;
-        boolean oldFormat = false;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+        
+        String firstNonCommentLine;
         String versionID = null;
+        boolean savedOnWindowsPlatform = false;
+        boolean savedOnMacPlatform = false;
+        String guessedEncoding = null;
+        Mapping mapping = null;
+    
+        // We need to skip past the comments to position the reader at the <?xml line for
+        // unmarshalling to start.  Also, we look at these comments to determine version of the
+        // mapping to use, as well as if it's a pre VUE 1.5 (August 2006) save file, in which case
+        // we must guess an encoding, and re-open the file using an InputStreamReader with the
+        // proper encoding.
+
         for (;;) {
-            reader.mark(2048);
-            line = reader.readLine();
+            reader.mark(2048); // a single comment line can't be longer than this...
+            String line = reader.readLine();
             if (line == null) {
                 System.err.println("Unexpected end-of-stream in [" + url + "]");
                 throw new java.io.IOException("end of stream in " + url);
             }
-            if (DEBUG.CASTOR || DEBUG.IO) System.out.println("Top of file[" + line + "]");
-            if (!line.startsWith("<!--"))
+            if (DEBUG.CASTOR || DEBUG.IO) System.out.println("Scanning[" + line + "]");
+            if (line.startsWith("<!--") == false) {
+                // we should have just hit thie "<?xml ..." line -- done with comments
+                firstNonCommentLine = line;
                 break;
-            commentCount++;
-            // if we're being given an override mapping (or we already found one),
-            // don't bother to search for one based on the version.
-            if (mapping != null)
-                continue;
+            }
+
+            if (line.startsWith(VUE_COMMENT_START + " Saved")) {
+
+                // The "saved on platform" comments were never expected to be used functionally
+                // (only for debug), so determining if the save file was written on a Windows box
+                // it's not 100% reliable: e.g., if a user somehow had the name "platform Windows",
+                // we would mistake this "saved by" for a "saved on", but we're just going to take
+                // this risk -- this is just a workaround backward compat hack because castor
+                // wasn't naming the real encoding in it's XML output (turns out it was always
+                // puting UTF-8), even if it was using the default Windows encoding of
+                // Cp1252/windows-1252.
+                
+                //if (DEBUG.IO) System.out.println("scanning for Windows platform...");
+                if (line.indexOf("platform Windows") > 0) {
+                    if (DEBUG.IO) System.out.println(url + " was saved in the Windows environment");
+                    savedOnWindowsPlatform = true;
+                } else if (line.indexOf("platform Mac") > 0) {
+                    if (DEBUG.IO) System.out.println(url + " was saved in the Mac environment");
+                    savedOnMacPlatform = true;
+                }
+            }
+            
+            
             // Scan the comment line for a version tag to base our mapping on:
-            int i = line.indexOf("@version(");
-            if (i >= 0) {
-                String s = line.substring(i);
+            int idx;
+            if ((idx = line.indexOf("@version(")) >= 0) {
+                String s = line.substring(idx);
                 //System.out.println("Found version start:" + s);
                 int x = s.indexOf(')');
                 if (x > 0) {
@@ -488,39 +531,103 @@ public class ActionUtil {
                     }
                 }
             }
-        } 
-        reader.reset();
-        if (versionID == null) {
-            oldFormat = true;
-            if (DEBUG.CASTOR || DEBUG.IO) System.out.println("Save file is of old pre-versioned type.");
-            if (mapping == null)
-                mapping = getMapping(XML_MAPPING_UNVERSIONED);
         }
+        reader.close();
+
+        if (firstNonCommentLine.startsWith("<?xml")) {
+            // Check to see if we need to use a non-default input encoding.
+            // NOTE: We make sure we only attempt guessedEncoding if the given encoding is
+            // the default input encoding: otherwise assume we're here recursively,
+            // after already guessing at an encoding (otherwise, we'll loop, and blow stack)
+            if (DEBUG.IO) System.out.println("XML head [" + firstNonCommentLine + "]");
             
+            if (firstNonCommentLine.indexOf("encoding=\"UTF-8\"") > 0) {
+                
+                // If encoding is UTF-8, this a 2nd generation save file (mapping is
+                // versioned, but not all US-ASCII encoding): the actual encoding is
+                // unknown: make a guess as how to best handle it.  This is our rule: if
+                // we're on the SAME platform as the save file, assuming the local
+                // encoding (assume it's the same user, on the same machine, and the
+                // current default system encoding is the same one that was active when
+                // the file was originally saved).  If we're on a different platform,
+                // assume a default encoding for that platform.
+                
+                if (Util.isWindowsPlatform()) {
+                    if (savedOnWindowsPlatform)
+                        guessedEncoding = Util.getDefaultPlatformEncoding();
+                    else if (savedOnMacPlatform)
+                        guessedEncoding = DEFAULT_MAC_ENCODING;
+                    else
+                        guessedEncoding = DEFAULT_WINDOWS_ENCODING;
+                } else if (Util.isMacPlatform()) {
+                    if (savedOnMacPlatform)
+                        guessedEncoding = Util.getDefaultPlatformEncoding();
+                    else if (savedOnWindowsPlatform)
+                        guessedEncoding = DEFAULT_WINDOWS_ENCODING;
+                    else
+                        guessedEncoding = DEFAULT_MAC_ENCODING;
+                }
+                VUE.Log.info(url + "; assuming " + guessedEncoding + " charset encoding");
+                // Note: doing this is a real tradeoff amongst bugs: any old save file that had
+                // fancy unicode characters in UTF, such as something in a japanese charset, will
+                // be screwed by this, so we optimizing for what we think is the most likely case.
+                // if this becomes a real problem, we could introduce a special convert dialog.
+                
+            }
+        } else {
+            VUE.Log.warn("Missing XML header in [" + firstNonCommentLine + "]");
+        }
+
+        boolean oldFormat = false;
+
+        if (versionID == null && mapping == null) {
+            oldFormat = true;
+            VUE.Log.info(url + "; save file is of old pre-versioned type.");
+            mapping = getMapping(XML_MAPPING_UNVERSIONED);
+        }
+        
+        if (mapping == null)
+            mapping = getDefaultMapping();        
+
+        String encoding = guessedEncoding == null ? DEFAULT_INPUT_ENCODING : guessedEncoding;
+
+        return unmarshallMap(url, mapping, encoding, false);
+    }
+
+
+    private static LWMap unmarshallMap(final java.net.URL url, Mapping mapping, String charsetEncoding, boolean oldFormat)
+        throws java.io.IOException
+    {
+        LWMap map = null;
+
+        if (DEBUG.CASTOR || DEBUG.IO) System.out.println("UNMARSHALLING: " + url + " charset=" + charsetEncoding);
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), charsetEncoding));
+
+        // Skip over comments to get to start of XML
+
+        for (;;) {
+            reader.mark(2048); // a single comment line can't be longer than this...
+            String line = reader.readLine();
+            if (line == null) {
+                System.err.println("Unexpected end-of-stream in [" + url + "]");
+                throw new java.io.IOException("end of stream in " + url);
+            }
+            if (line.startsWith("<!--") == false) {
+                // we should have just hit thie "<?xml ..." line -- done with comments
+                break;
+            }
+            if (DEBUG.CASTOR || DEBUG.IO) System.out.println("Skipping[" + line + "]");
+        }
+
+        // Reset the reader to the start of the last line read, which should be the <?xml line,
+        // which is what castor needs to see at start (it can't handle ignoring comments...)
+        reader.reset();
+
+        final String sourceName = url.toString();
+
         try {
-            Unmarshaller unmarshaller = getDefaultUnmarshaller(mapping, url.toString());
-
-            /*
-            
-            Unmarshaller unmarshaller = new Unmarshaller(); // todo: can cache this with it's mapping set
-            unmarshaller.setIgnoreExtraAttributes(true);
-            unmarshaller.setIgnoreExtraElements(true);
-            unmarshaller.setValidation(false);
-            //unmarshaller.setWhitespacePreserve(true); // doesn't affect elements!  (e.g. <notes> foo bar </notes>)
-            // HOWEVER: castor 0.9.7 now automatically encodes/decodes white space for attributes...
-            //unmarshaller.setLogWriter(new PrintWriter(System.out));
-            Logger logger = new Logger(System.err);
-            logger.setPrefix("Castor " + url);
-            unmarshaller.setLogWriter(logger);
-            //unmarshaller.setLogWriter(new Logger.getSystemLogger());
-            // not a good sign: above getSystemLogger is decl public in castor code, but not public in doc, and non-existent in jar
-
-            if (DEBUG.XML) unmarshaller.setDebug(true);
-
-            unmarshaller.setUnmarshalListener(new VueUnmarshalListener());
-            unmarshaller.setMapping(mapping);
-
-            */
+            Unmarshaller unmarshaller = getDefaultUnmarshaller(mapping, sourceName);
 
             // unmarshall the map:
             
@@ -531,13 +638,7 @@ public class ActionUtil {
                     System.err.println("ActionUtil.unmarshallMap: " + me);
                     System.err.println("Attempting specialized MapResource mapping for old format.");
                     // NOTE: delicate recursion here: won't loop as long as we pass in a non-null mapping.
-                    return unmarshallMap(url, getMapping(XML_MAPPING_OLD_RESOURCES));
-                    /*
-                    unmarshaller.setMapping(loadMapping(XML_MAPPING_OLD));
-                    reader.reset(); // todo: need to make sure buffer is big enough for rollback!
-                    // okay, stream has been closed: would it make any difference if didn't use the url.openStream? (File instead)
-                    map = (LWMap) unmarshaller.unmarshal(new InputSource(reader));
-                    */
+                    return unmarshallMap(url, getMapping(XML_MAPPING_OLD_RESOURCES), charsetEncoding, true);
                 } else
                     throw me;
             }
@@ -559,4 +660,5 @@ public class ActionUtil {
         
         return map;
     }
+
 }
