f9d80933ad281db6390d44b0e86f1cbebb81965d
Scott Fraize <scott.fraize@gmail.com>
2006-10-03 18:25:46 +0000

    exteded to be able to unpack jar:file: resources from the running jar file so they can be displayed in external viewers (e.g., release notes in the web browser)
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@3767 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/action/ShowURLAction.java
+++ VUE2/src/tufts/vue/action/ShowURLAction.java
@@ -1,24 +1,83 @@
 package tufts.vue.action;
 
+import tufts.vue.VueUtil;
+import tufts.vue.DEBUG;
+import tufts.vue.VUE;
+import java.net.URL;
+import java.io.File;
+
 /**
  * Display a given URL in an external browser.
  */
 public class ShowURLAction extends tufts.vue.VueAction
 {
-    private String url;
+    private String urlString = null;
+    private URL url = null;
+    private String localName = null; // only used if we need to unpack from a jar
     
-    public ShowURLAction(String url) {
-        this(url, url);
+    public ShowURLAction(String urlString) {
+        this(urlString, urlString);
     }
-    public ShowURLAction(String name, String url) {
-        super(name);
+    public ShowURLAction(String label, String urlString) {
+        super(label);
+        this.urlString = urlString;
+    }
+
+    /**
+     * If the given URL is a jar:file URL (which a browser can't acccess),
+     * unpack it into the users VUE config dir (e.g., ~/.vue), and
+     * display it from there.  However, if the file was already
+     * unpacked and created there, don't bother to unpack it again. 
+     */
+    public ShowURLAction(String label, URL url, String fileName) {
+        super(label);
         this.url = url;
+        this.localName = fileName;
     }
     
     public void act()
     {
+        String browserURL = null;
+        
+        if (url == null) {
+            browserURL = urlString;
+        } else {
+            
+            if (DEBUG.IO) VUE.Log.info("ShowURLAction: raw url: " + url);
+            if (DEBUG.IO) out("PROTOCOL: " + url.getProtocol());
+
+            if ("jar".equals(url.getProtocol())) {
+
+                if (DEBUG.IO) out("Dealing with [" + localName + "]");
+                File dir = VueUtil.getDefaultUserFolder();
+                File localFile = new File(dir, this.localName);
+                
+                try {                
+                    if (DEBUG.IO) out("Looking for " + localFile);
+                    if (localFile.exists() && localFile.length() > 0) {
+                        if (DEBUG.IO) out("EXISTS: " + localFile);
+                    } else {
+                        if (DEBUG.IO) out("COPYING FILE to " + localFile);
+                        if (!localFile.exists() && !localFile.createNewFile())
+                            throw new Error("failed to create " + localFile);
+                        VueUtil.copyURL(url, localFile);
+                    }
+
+                    browserURL = localFile.toURL().toString();
+                    if (DEBUG.IO) out("LOCAL URL: " + browserURL);
+                    
+                } catch (Throwable t) {
+                    VUE.Log.error("Failed to display content for " + url + " in " + localFile);
+                    t.printStackTrace();
+                    return;
+                }
+            } else {
+                browserURL = url.toString();
+            }
+        }
+        
         try {
-            tufts.vue.VueUtil.openURL(url);
+            tufts.vue.VueUtil.openURL(browserURL);
         } catch (Exception ex) {
             //System.out.println("ShowURLAction " + this + " failed to display [" + url + "] on " + ae);
             throw new RuntimeException("ShowURLAction " + this + " failed to display [" + url + "]", ex);
