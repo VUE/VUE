260f73d7bd652737792be1a49016ada88a54d25b
Anoop Kumar <anoopkum@gmail.com>
2006-10-11 17:58:19 +0000

    utilities
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@3779 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/edu/tufts/vue/compare/Util.java
@@ -0,0 +1,49 @@
+/*
+ * Util.java
+ *
+ * Created on October 5, 2006, 12:17 PM
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2003-2006
+ * Tufts University. All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ *
+ * @author akumar03
+ */
+package edu.tufts.vue.compare;
+
+import tufts.vue.*;
+public class Util {
+    // returns a map for concept map comparison
+    public static LWMap getMap() {
+        LWMap map = new LWMap("Cities");
+        LWNode n1 = new LWNode("Boston");
+        LWNode n2 = new LWNode("Miami");
+        LWNode n3 = new LWNode("Denver");
+        n1.setLocation(100, 30);
+        n2.setLocation(100, 100);
+        n3.setLocation(50, 180);
+        map.addNode(n1);
+        map.addNode(n2);
+        map.addNode(n3);
+        LWLink k1 = new LWLink(n1, n2);
+        LWLink k2 = new LWLink(n2, n3);
+        LWLink k3 = new LWLink(n3, n1);
+        map.addLink(k1);
+        map.addLink(k2);
+        //map.addLink(k3);
+        return map;
+    }
+}
