6e84d8e2a259956ca6fc43183b50ac4116749d29
Anoop Kumar <anoopkum@gmail.com>
2006-10-23 20:18:52 +0000

    started creating a framework for testing VUE using JUnit.  This Test will be automatically executed when automatic build is run.
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@3859 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/test/src/edu/tufts/vue/VUETest.java
@@ -0,0 +1,39 @@
+/*
+ * VUETest.java
+ *
+ * Created on October 23, 2006, 4:16 PM
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2003-2006
+ * Tufts University. All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ *
+ * @author akumar03
+ */
+package edu.tufts.vue;
+
+
+import junit.framework.*;
+
+import tufts.vue.*;
+public class VUETest {
+    
+    public static Test suite() {
+        TestSuite suite = new TestSuite("All tests that will be performed automatically");
+        suite.addTestSuite(edu.tufts.vue.compare.TestConnectivityMatrix.class);
+        return suite;
+    }
+    
+}
