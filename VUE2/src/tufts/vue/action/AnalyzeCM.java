0c993ce6bec69385b72f957210b6eaa912d286df
Anoop Kumar <anoopkum@gmail.com>
2006-11-07 02:07:31 +0000

    Action to analyze concept maps.  Method need to be written
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@3881 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/tufts/vue/action/AnalyzeCM.java
@@ -0,0 +1,45 @@
+/*
+ * AnalyzeCM.java
+ *
+ * Created on November 6, 2006, 11:53 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author akumar03
+ */
+
+
+package tufts.vue.action;
+
+import tufts.vue.*;
+import edu.tufts.vue.compare.*;
+
+import java.io.*;
+import java.util.*;
+
+
+import javax.swing.*;
+import java.awt.event.*;
+
+
+public class AnalyzeCM extends VueAction {
+    
+    /** Creates a new instance of AnalyzeCM */
+    public AnalyzeCM(String label) {
+        super(label);
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        try {
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+    
+    
+}
