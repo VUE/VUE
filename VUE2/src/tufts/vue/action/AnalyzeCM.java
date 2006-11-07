26b5071756e84a65d8f299c4d997c407b58668d4
Anoop Kumar <anoopkum@gmail.com>
2006-11-07 21:51:33 +0000

    the method is complete. needs some cleanup and ui
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@3888 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/action/AnalyzeCM.java
+++ VUE2/src/tufts/vue/action/AnalyzeCM.java
@@ -35,6 +35,37 @@ public class AnalyzeCM extends VueAction {
     
     public void actionPerformed(ActionEvent e) {
         try {
+            ArrayList<ConnectivityMatrix> list = new ArrayList();
+            LWMap referenceMap = null;
+            Iterator<LWMap> i =   VUE.getLeftTabbedPane().getAllMaps();
+            while(i.hasNext()) {
+                LWMap map = i.next();
+                if(referenceMap == null)
+                    referenceMap = map;
+                list.add(new ConnectivityMatrix(map));
+                System.out.println("Map:"+map.getLabel());
+            }
+            VoteAggregate voteAggregate = new VoteAggregate(list);
+            System.out.println(voteAggregate);
+            LWMap aggregate = new LWMap("Vote Aggreate");
+            Iterator children = referenceMap.getNodeIterator();
+            while(children.hasNext()) {
+                LWNode node = (LWNode)((LWComponent)children.next()).duplicate();
+                aggregate.addNode(node);
+                Iterator children2 = referenceMap.getNodeIterator();
+                while(children2.hasNext()) {
+                    LWNode node2 = (LWNode)children2.next();
+                    if(node2 != node) {
+                        int c = voteAggregate.getConnection(node.getLabel(),node2.getLabel());
+                        if(c >0) {
+                            aggregate.addLink(new LWLink(node,node2));
+                            
+                        }
+                    }
+                }
+            }
+            VUE.displayMap(aggregate);
+            
         } catch(Exception ex) {
             ex.printStackTrace();
         }
