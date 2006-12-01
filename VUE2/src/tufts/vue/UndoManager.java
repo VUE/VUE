2850444edf293086818f7b24e7260cfd50b12259
Scott Fraize <scott.fraize@gmail.com>
2006-12-01 18:10:59 +0000

    comments
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@3931 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/UndoManager.java
+++ VUE2/src/tufts/vue/UndoManager.java
@@ -34,8 +34,8 @@ import javax.swing.Action;
  * stuffed into an un-named Undo action if they attempt an undo while
  * there are unmarked changes.
  *
+ * @version $Revision: 1.86 $ / $Date: 2006/11/16 14:20:48 $ / $Author: sfraize $
  * @author Scott Fraize
- * @version $Revision:  $ / $Date:  $ / $Author: sfraize $
  */
 
 public class UndoManager
