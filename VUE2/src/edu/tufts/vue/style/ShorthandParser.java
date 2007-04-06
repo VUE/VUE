0a8ef6bf762f2c9ce6f9d957fa12ab2eba7de4c6
Anoop Kumar <anoopkum@gmail.com>
2007-04-06 14:47:26 +0000

    a class with static methods to parse CSS file
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@4318 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/edu/tufts/vue/style/ShorthandParser.java
@@ -0,0 +1,48 @@
+/*
+ * ShorthandParser.java
+ *
+ * Created on April 6, 2007, 10:32 AM
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2003-2007
+ * Tufts University. All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ *
+ * @author akumar03
+ *
+ * This class contains a set of static methods to handle shorthands allowed in CSS
+ * Examples: "blue"  color needs to be parsed to #00000FF
+ */
+package edu.tufts.vue.style;
+
+import java.awt.*;
+public class ShorthandParser {
+    public static final float DEFAULT_FONT_SIZE = 10;
+    
+    public static Float parseFontSize(String value){
+        float fv = DEFAULT_FONT_SIZE;
+        if(value.endsWith("pt")) {
+            fv = Float.parseFloat(value.substring(0,value.length()-2)) ;
+        }else if(value.endsWith("px")) {
+            fv = Float.parseFloat(value.substring(0,value.length()-2)) ;
+        } else  {
+            fv = Float.parseFloat(value);
+        }
+        
+        Float f = new Float(fv);
+        return f;
+    }
+    
+}
