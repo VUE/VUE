939e42b993aa2af45d500ad867b8bfd9a36f5349
Anoop Kumar <anoopkum@gmail.com>
2007-04-20 15:23:24 +0000

    extends org.osid.shared.Type, the purpose is to differentiate rdfs and owl types
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@4477 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/edu/tufts/vue/ontology/OntologyType.java
@@ -0,0 +1,45 @@
+/*
+ * OntologyType.java
+ *
+ * Created on April 20, 2007, 11:33 AM
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2003-2007
+ * Tufts University. All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ *
+ * @author akumar03
+ */
+
+package edu.tufts.vue.ontology;
+
+
+public class OntologyType  extends org.osid.shared.Type{
+    public static final org.osid.shared.Type RDFS_TYPE = new OntologyType("RDFS type","edu.tufts","rdfs");
+    public static final org.osid.shared.Type OWL_TYPE =  new OntologyType("OWL type","edu.tufts","owl");
+    public OntologyType(String authority
+            , String domain
+            , String keyword) {
+        super(authority,domain,keyword);
+    }
+    public OntologyType(String authority
+            , String domain
+            , String keyword
+            , String description) {
+        super(authority,domain,keyword,description);
+    }
+    
+    
+}
