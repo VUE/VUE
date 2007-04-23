865c0d4d3678c45eaa6a47e3cbac4aebe3bbce18
Anoop Kumar <anoopkum@gmail.com>
2007-04-23 16:05:03 +0000

    OWLLOntology extends ontology
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@4491 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/ontology/OWLLOntology.java
+++ VUE2/src/edu/tufts/vue/ontology/OWLLOntology.java
@@ -13,10 +13,45 @@
  */
 package edu.tufts.vue.ontology;
 
-public class OWLLOntology {
-    
+import edu.tufts.vue.style.*;
+
+import java.util.*;
+import java.net.*;
+
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.ontology.*;
+import com.hp.hpl.jena.util.iterator.*;
+import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.shared.PrefixMapping;
+import com.hp.hpl.jena.util.iterator.Filter;
+
+public class OWLLOntology extends Ontology {
+     OntModel m = ModelFactory.createOntologyModel(OntModelSpec.OWL_LITE_MEM, null);
     /** Creates a new instance of OWLLOntology */
     public OWLLOntology() {
+        
+    }
+     public OWLLOntology(URL ontUrl) {
+        List<OntType> types = new ArrayList<OntType>();
+        m.read(ontUrl.toString());
+        setBase(ontUrl.toString());
+        ExtendedIterator iter;
+        readOntTypes(m.listNamedClasses(),types,ontUrl.toString());
+        readOntTypes( m.listOntProperties(),types,ontUrl.toString());
+        setOntTypes(types);
+    }
+    
+    public OWLLOntology(URL ontUrl,URL cssUrl) {
+        List<OntType> types = new ArrayList<OntType>();
+        m.read(ontUrl.toString());
+        CSSParser parser = new CSSParser();
+        Map<String,Style> styleMap = parser.parseToMap(cssUrl);
+        ExtendedIterator iter = m.listOntProperties();
+        setBase(ontUrl.toString());
+        readOntTypes(iter,types,styleMap,ontUrl.toString());
+         readOntTypes(m.listObjectProperties(),types,styleMap,ontUrl.toString());
+        readOntTypes(m.listNamedClasses(),types,styleMap,ontUrl.toString());
+        setOntTypes(types);
     }
     
     /**
