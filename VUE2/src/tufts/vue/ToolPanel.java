db7992211120c31d828a09e0fd1018eb6a045239
Scott Fraize <scott.fraize@gmail.com>
2007-05-01 04:41:00 +0000

    cleanup
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@4572 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/ToolPanel.java
+++ VUE2/src/tufts/vue/ToolPanel.java
@@ -7,7 +7,7 @@ import javax.swing.border.*;
  * Replacement for old LWCToolPanel, mainly for backward compat with some old code.
  * Ultimately, we probably don't need this class at all.  -- SMF 2007-04-30
  *
- * @version $Revision: 1.1 $ / $Date: 2007-05-01 04:38:07 $ / $Author: sfraize $  
+ * @version $Revision: 1.2 $ / $Date: 2007-05-01 04:41:00 $ / $Author: sfraize $  
  */
 
 public class ToolPanel extends JPanel
@@ -17,13 +17,12 @@ public class ToolPanel extends JPanel
     public ToolPanel()
     {
          setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-         
-         setBorder(new EmptyBorder(2,1,2,1));//t,l,b,r
+         setBorder(new EmptyBorder(0,2,0,3));//t,l,b,r
          
          setOpaque(false);
          mBox = new JPanel();
          mBox.setLayout(new java.awt.GridBagLayout());
-         tufts.vue.gui.GUI.applyToolbarColor(mBox);
+         //tufts.vue.gui.GUI.applyToolbarColor(mBox); // was only for the old perma-docked toolbar
          buildBox();
          add(mBox);
     }
