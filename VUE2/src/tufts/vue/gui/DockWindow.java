68958b6f03ebfc5c8ac6f78e007358bbe49e3a71
Scott Fraize <scott.fraize@gmail.com>
2007-05-01 05:05:17 +0000

    fixed missing content arg
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@4576 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/gui/DockWindow.java
+++ VUE2/src/tufts/vue/gui/DockWindow.java
@@ -57,7 +57,7 @@ import edu.tufts.vue.preferences.implementations.WindowPropertiesPreference;
  * want it within these Windows.  Another side effect is that the cursor can't be
  * changed anywhere in the Window when it's focusable state is false.
 
- * @version $Revision: 1.92 $ / $Date: 2007-05-01 04:30:22 $ / $Author: sfraize $
+ * @version $Revision: 1.93 $ / $Date: 2007-05-01 05:05:17 $ / $Author: sfraize $
  * @author Scott Fraize
  */
 
@@ -244,7 +244,7 @@ public class DockWindow extends javax.swing.JWindow
 
     public DockWindow(String title, Window owner, JComponent content, boolean asToolbar)
     {
-    	this(title,owner,null,false,true);
+    	this(title,owner,content,false,true);
     }
     public DockWindow(String title, Window owner) {
         this(title, owner, null, false,true);
