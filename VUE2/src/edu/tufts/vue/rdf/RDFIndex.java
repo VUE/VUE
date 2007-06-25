509be169205ad9daf63c9fc6a4ce0196a6761617
Anoop Kumar <anoopkum@gmail.com>
2007-06-25 16:03:51 +0000

    RDFIndex class will support indexing and search of RDF
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@5183 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/edu/tufts/vue/rdf/RDFIndex.java
@@ -0,0 +1,41 @@
+/*
+ * RDFIndex.java
+ *
+ * Created on June 25, 2007, 12:02 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author akumar03
+ */
+
+package edu.tufts.vue.rdf;
+
+import java.util.*;
+import java.io.*;
+import tufts.vue.*;
+
+import com.hp.hpl.jena.rdf.model.impl.*;
+
+public class RDFIndex {
+    public void index(LWMap map) {
+        
+    }
+    
+    public List search(String keyword) {
+        
+        return null;
+    }
+    
+    public void save() {
+        
+    }
+    
+    public void read() {
+        
+    }
+    
+}
