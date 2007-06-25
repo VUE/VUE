f77b823ed8ac4ad34a1aa668d63bbaebc5993167
Anoop Kumar <anoopkum@gmail.com>
2007-06-25 17:07:15 +0000

    datasource for RDF
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@5187 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/edu/tufts/vue/rss/RSSDataSource.java
@@ -0,0 +1,36 @@
+/*
+ * RSSDatasource.java
+ *
+ * Created on June 25, 2007, 1:10 PM
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2003-2007
+ * Tufts University. All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ *
+ * @author akumar03
+ */
+
+package edu.tufts.vue.rss;
+
+import tufts.vue.*;
+
+public class RSSDataSource  extends VueDataSource{
+    
+    /** Creates a new instance of RSSDatasource */
+    public RSSDataSource() {
+    }
+    
+}
