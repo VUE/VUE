a6425ff12605cda30a868626d107268c8c8b7e38
Mike Korcynski <mkorcy@gmail.com>
2007-07-24 17:20:03 +0000

    unused, initial check in of integer preference.
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@5336 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/edu/tufts/vue/preferences/implementations/IntegerPreference.java
@@ -0,0 +1,96 @@
+package edu.tufts.vue.preferences.implementations;
+
+import java.util.prefs.Preferences;
+
+import javax.swing.JComponent;
+
+import edu.tufts.vue.preferences.generics.BasePref;
+import edu.tufts.vue.preferences.interfaces.VuePreference;
+
+public class IntegerPreference extends BasePref
+{
+
+	private String category;
+	private String name;
+	private String key;
+	private String description;
+	private Integer defaultValue;
+	private Integer previousValue;
+	private Preferences p = Preferences.userNodeForPackage(getPrefRoot());
+	
+	public static IntegerPreference create(String category, String key, String name, String desc, Integer defaultValue, boolean showInUI)
+	{
+		return new IntegerPreference(category,key,name,desc,defaultValue,showInUI);
+	}
+	
+	//show in UI defaults to true
+	public static IntegerPreference create(String category, String key, String name, String desc, Integer defaultValue)
+	{
+		return new IntegerPreference(category,key,name,desc,defaultValue,true);
+	}
+	
+	protected IntegerPreference(String category, String key, String name, String desc, Integer defaultValue, boolean showInUI)
+	{
+		this.category=category;
+		this.key = key;
+		this.name = name;
+		this.description = desc;
+		this.defaultValue = defaultValue;
+		if (showInUI)
+			edu.tufts.vue.preferences.PreferencesManager.registerPreference(this);
+	}
+		
+	public Integer getDefaultValue()
+	{
+		return defaultValue;
+	}
+	public String getDescription() { 
+		return description;
+	}
+
+	public String getMessage()
+	{
+		return name;
+	}
+	public String getTitle() {
+		return name;
+	}
+
+	public String getCategoryKey() { 
+		return category;
+	}
+
+	public String getPrefName() {
+		return category + "." + key;
+	}
+
+	public JComponent getPreferenceUI() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Integer getPreviousValue()
+	{
+		if (previousValue == null)
+			return getDefaultValue();
+		else
+			return previousValue;
+	}
+
+	public Integer getValue(){
+		Integer i = (p.getInt(getPrefName(), getDefaultValue().intValue()));
+		return i;
+	
+	}
+
+	public void setValue(Object o)
+	{
+		setValue((Integer)o);
+	}
+	public void setValue(Integer i)
+	{
+		previousValue = getValue(); 		
+		p.putInt(getPrefName(), i.intValue());
+		_fireVuePrefEvent();
+	}
+}
