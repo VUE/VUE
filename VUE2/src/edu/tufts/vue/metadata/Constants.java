c8797c5d0bf2ac1147e21cdbb66320003d822042
Anoop Kumar <anoopkum@gmail.com>
2007-07-30 15:24:07 +0000

    constants for vue metadata in case the ontology doesn't load properly.
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@5354 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/edu/tufts/vue/metadata/Constants.java
@@ -0,0 +1,36 @@
+/*
+ * Constants.java
+ *
+ * Created on July 27, 2007, 4:23 PM
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2003-2007
+ * Tufts University. All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ *
+ * @author akumar03
+ */
+package edu.tufts.vue.metadata;
+
+public class Constants {
+   public static final String ONTOLOGY_URL  = "http://vue.tufts.edu/ontology/vue.rdfs";
+   public static final String RESOURCE_URL = "http://vue.tufts.edu/rdf/resource/";
+   public static final String ID = "id";
+   public static final String LABEL = "label";
+   public static final String CHILD  = "child";
+   public static final String AUTHOR = "author";
+   public static final String TAG ="tag";
+    
+}
