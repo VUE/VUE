4bf17ca011e21c18f7564455cef692a91a92490f
dan heller <dan.heller@tufts.edu>
2007-07-31 15:36:25 +0000

    first draft of search capability - to enable change constant in InspectorPane (same as for metadata edit panel)
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@5372 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/edu/tufts/vue/metadata/ui/MetadataSearchGUI.java
@@ -0,0 +1,65 @@
+
+/*
+ *
+ * * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2003-2007
+ * Tufts University. All rights reserved.</p>
+ *
+ *
+ */
+
+package edu.tufts.vue.metadata.ui;
+
+import edu.tufts.vue.metadata.action.*;
+import java.awt.BorderLayout;
+import java.awt.event.*;
+import java.net.*;
+import javax.swing.*;
+import tufts.vue.*;
+
+import java.util.*;
+
+/*
+ * MetadataSearchGUI.java
+ *
+ * Will likely replace MapInspector in all its incarnations
+ * and locations within VUE (this GUI supports the new
+ * metadata format)
+ *
+ * Created on July 19, 2007, 1:31 PM
+ *
+ * @author dhelle01
+ */
+public class MetadataSearchGUI extends JPanel {
+    
+    private JTextField searchField;
+    private JTable searchTable;
+    private JButton searchButton;
+    private List<URI> found = null;
+    private List<List<URI>> finds = null;
+    
+    public MetadataSearchGUI() 
+    {
+        setUpOneLineSearch();      
+    }
+    
+    public void setUpOneLineSearch()
+    {
+        setLayout(new BorderLayout());
+        searchField = new JTextField();
+        JPanel buttonPanel = new JPanel(new BorderLayout());
+        searchButton = new JButton(new SearchAction(searchField));
+        buttonPanel.add(BorderLayout.EAST,searchButton);
+        add(BorderLayout.NORTH,searchField);
+        add(buttonPanel);
+    }
+        
+}
