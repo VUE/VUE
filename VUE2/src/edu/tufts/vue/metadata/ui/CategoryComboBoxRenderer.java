43397411c54cb852f22f2cbdf52b651db28642c0
Anoop Kumar <anoopkum@gmail.com>
2007-08-17 20:44:40 +0000

    renderer for combobox
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@5472 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/edu/tufts/vue/metadata/ui/CategoryComboBoxRenderer.java
@@ -0,0 +1,66 @@
+/*
+ * CategoryComboBoxRenderer.java
+ *
+ * Created on August 17, 2007, 2:45 PM
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2003-2007
+ * Tufts University. All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ *
+ * @author akumar03
+ */
+package edu.tufts.vue.metadata.ui;
+
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+
+import edu.tufts.vue.metadata.*;
+import edu.tufts.vue.ontology.*;
+
+public class CategoryComboBoxRenderer extends DefaultListCellRenderer {
+    
+    /** Creates a new instance of CategoryComboBoxRenderer */
+    public CategoryComboBoxRenderer() {
+    }
+    public Component getListCellRendererComponent(
+            JList list,
+            Object value,
+            int index,
+            boolean isSelected,
+            boolean cellHasFocus) {
+        
+        if(value != null){
+            if((value instanceof edu.tufts.vue.ontology.OntType)) {
+                setText(((OntType)value).getLabel());
+                if (isSelected) {
+                    setBackground(list.getSelectionBackground());
+                    setForeground(list.getSelectionForeground());
+                } else {
+                    setBackground(list.getBackground());
+                    setForeground(list.getForeground());
+                }
+                
+                
+            } else {
+                setText(value.toString());
+            }
+        }
+        
+        return this;
+    }
+    
+}
