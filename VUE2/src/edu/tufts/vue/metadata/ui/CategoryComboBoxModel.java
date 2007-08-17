11580160c23fb3dddb2bd5098dcfafae0c441a7f
Anoop Kumar <anoopkum@gmail.com>
2007-08-17 20:44:10 +0000

    uses separator
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@5471 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/metadata/ui/CategoryComboBoxModel.java
+++ VUE2/src/edu/tufts/vue/metadata/ui/CategoryComboBoxModel.java
@@ -38,7 +38,7 @@ public class CategoryComboBoxModel extends DefaultComboBoxModel {
     EditCategoryItem editCategory = new EditCategoryItem();
     /** Creates a new instance of CategoryComboBoxModel */
     public CategoryComboBoxModel() {
-        vueCategoryModel = new CategoryModel();
+        vueCategoryModel = tufts.vue.VUE.getCategoryModel();
     }
     public Object getSelectedItem() {
         return selectedItem;
@@ -68,9 +68,9 @@ public class CategoryComboBoxModel extends DefaultComboBoxModel {
         }
         for(Ontology ont: vueCategoryModel) {
             if(i <ont.getOntTypes().size()) {
-                return ont.getOntTypes().get(i).getLabel();
+                return ont.getOntTypes().get(i);
             } else if(i == ont.getOntTypes().size()) {
-                return new String("------------");
+                return new ComboBoxSeparator();
             } else {
                 i -= ont.getOntTypes().size();
                 i--; // for the divider int the middle;
