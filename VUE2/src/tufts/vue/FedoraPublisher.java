8bef9d96c67706f48754caa5d352a526a2bd73ef
Anoop Kumar <anoopkum@gmail.com>
2007-10-02 23:01:44 +0000

    updated DC datastream
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@5884 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/FedoraPublisher.java
+++ VUE2/src/tufts/vue/FedoraPublisher.java
@@ -33,6 +33,9 @@ import java.text.SimpleDateFormat;
 
 import javax.activation.MimetypesFileTypeMap;
 
+
+import edu.tufts.vue.metadata.VueMetadataElement;
+
 //required for publishing to Fedora
 import fedora.client.FedoraClient;
 import fedora.client.utility.ingest.AutoIngestor;
@@ -53,16 +56,24 @@ public class FedoraPublisher {
     public static final String OWNER_KEY  = "%OWNER%";
     public static final String TITLE_KEY = "%TITLE%";
     public static final String MIME_TYPE = "%MIME_TYPE%";
+    public static final String DC_MIME_TYPE_KEY = "%DC_MIME_TYPE%";
+    public static final String DC_LABEL_KEY = "%DC_LABEL%";
+    public static final String DC_ID_KEY = "%DC_ID%";
     public static final String FORMAT = "foxml1.0";
     public static final String COMMENT = "Automatic Publish From VUE";
     public static final String VUE_MIME_TYPE ="application/vue";
+    public static final String XML_MIME_TYPE ="text/xml";
+    public static final String DC_LABEL = "Dublin Core Metadata";
     public static final String VUE_FORMAT_URL = "http://vue.tufts.edu/docs/vueformat/";
     public static final String MAP_DS = "map.vue";
     public static final String RESOURCE_DS = "RESOURCE";
+    public static final String DC_DS = "DC";
     public static final String VUE_CM =  "tufts/vue/map/generic";
     public static final String OTHER_CM = "tufts/vue/other";
     public static final String RESULT_FIELDS[] = {"pid"};
     public static final String FILE_PREFIX = "file://";
+    public static final String DC_URL = VueResources.getString("metadata.dublincore.url");
+        
     private static Map<String,String> foxmls = new HashMap<String,String>();
     static String foxml;
     /** Creates a new instance of FedoraExporter */
@@ -73,7 +84,7 @@ public class FedoraPublisher {
         String mapPid = "vue:"+map.getURIString().substring(map.getURIString().lastIndexOf("/")+1);
         //TODO: Marchall Map before uploading it to repository
         File file =map.getFile();
-        uploadObjectToRepository(protocol,host,port, userName,password,file, VueResources.getFile("fedora.cm.vue"),VUE_CM,(new MimetypesFileTypeMap().getContentType(file)),mapPid,file.getName(),MAP_DS);
+        uploadObjectToRepository(protocol,host,port, userName,password,file, VueResources.getFile("fedora.cm.vue"),VUE_CM,(new MimetypesFileTypeMap().getContentType(file)),mapPid,file.getName(),MAP_DS,map);
     }
     
     public static void uploadMapAll(String protocol, String host, int port, String userName, String password,LWMap map) {
@@ -86,10 +97,10 @@ public class FedoraPublisher {
                     URLResource resource = (URLResource) component.getResource();
                     //   System.out.println("Component:"+component+"file:" +resource.getSpec()+" has file:"+resource.getSpec().startsWith(FILE_PREFIX));
                     if(resource.isLocalFile()) {
-                        String pid = "vue:"+component.getURIString().substring(map.getURIString().lastIndexOf("/")+1);  
+                        String pid = "vue:"+component.getURIString().substring(map.getURIString().lastIndexOf("/")+1);
                         File file = new File(resource.getSpec());
                         System.out.println("LWComponent:"+component.getLabel() + "Resource: "+resource.getSpec()+"File:"+file+" exists:"+file.exists()+" MimeType"+new MimetypesFileTypeMap().getContentType(file));
-                        uploadObjectToRepository(protocol,host,port, userName,password,file, VueResources.getFile("fedora.cm.other"),OTHER_CM,(new MimetypesFileTypeMap().getContentType(file)),pid,file.getName(),RESOURCE_DS);
+                        uploadObjectToRepository(protocol,host,port, userName,password,file, VueResources.getFile("fedora.cm.other"),OTHER_CM,(new MimetypesFileTypeMap().getContentType(file)),pid,file.getName(),RESOURCE_DS,component);
                         //Replace the link for resouce in the map
                         String ingestUrl =  "http://"+host+":8080/fedora/get/"+pid+"/RESOURCE";
                         resource.setSpec(ingestUrl);
@@ -103,7 +114,7 @@ public class FedoraPublisher {
         }
     }
     
-    public static void uploadObjectToRepository(String protocol, String host, int port, String userName, String password,File file, File contentModel,String cm,String mimeType,String pid,String label,String dsName) {
+    public static void uploadObjectToRepository(String protocol, String host, int port, String userName, String password,File file, File contentModel,String cm,String mimeType,String pid,String label,String dsName,LWComponent component) {
         try{
             System.setProperty("javax.net.ssl.trustStore", VueUtil.getDefaultUserFolder()+"\\truststore");
             System.setProperty("javax.net.ssl.trustStorePassword","tomcat");
@@ -116,23 +127,27 @@ public class FedoraPublisher {
             conds[0].setOperator(ComparisonOperator.eq);
             conds[0].setValue(pid);
             query.setConditions(conds);
+            String dcXML= getDC(component,label,pid);
             FieldSearchResult result = af.findObjects(RESULT_FIELDS,1,query);
             Uploader uploader = new Uploader(protocol, host, port, userName, password);
             String uploadId = uploader.upload(file);
             if(result.getResultList().length  >0 ) {
+                // updating the content
                 fc.getAPIM().modifyDatastreamByReference(pid, dsName,  null,label, mimeType, VUE_FORMAT_URL, uploadId, null,null,  COMMENT,true);
+                //updating the DC metadata
+                fc.getAPIM().modifyDatastreamByValue(pid,DC_DS,null,DC_LABEL,XML_MIME_TYPE,DC_URL,dcXML.getBytes(),null,null,COMMENT,true);
             } else {
                 // reading the foxml if it doesn't exist
-               if(!foxmls.containsKey(contentModel)) {
+                if(!foxmls.containsKey(contentModel)) {
                     BufferedReader r = new BufferedReader(new FileReader(contentModel));
                     String line = new String();
                     foxml = "";
                     while((line = r.readLine())!=null) {
                         foxml += line+"\n";
                     }
-               } else {
-                   foxml = foxmls.get(contentModel);
-               }
+                } else {
+                    foxml = foxmls.get(contentModel);
+                }
                 SimpleDateFormat  formatter =  new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'.000Z'");
                 Date currentTime = new Date();
                 String dateString = formatter.format(currentTime);
@@ -140,15 +155,18 @@ public class FedoraPublisher {
                 ingestFoxml = ingestFoxml.replace("%OWNER%",userName);
                 ingestFoxml = ingestFoxml.replaceAll("%CREATE_DATE%",dateString);
                 ingestFoxml = ingestFoxml.replace("%PID%",pid);
-               ingestFoxml = ingestFoxml.replace("%TITLE%",label);
+                ingestFoxml = ingestFoxml.replace("%TITLE%",label);
                 ingestFoxml = ingestFoxml.replace("%CONTENT_MODEL%",cm);
                 ingestFoxml = ingestFoxml.replace("%UPLOAD%",uploadId);
-                ingestFoxml = ingestFoxml.replace("%DC%",getBasicDC(label,pid));
+                ingestFoxml = ingestFoxml.replace("%DC%",dcXML);
+                ingestFoxml = ingestFoxml.replace("%DC_ID%",DC_DS);
+                ingestFoxml = ingestFoxml.replace("%DC_LABEL%",DC_LABEL);
+                ingestFoxml = ingestFoxml.replace("%DC_MIME_TYPE%",XML_MIME_TYPE);
                 ingestFoxml = ingestFoxml.replace("%MIME_TYPE%",mimeType);
                 System.out.println(ingestFoxml);
-                BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\temp\\IngestTest.xml"));
-                writer.write(ingestFoxml);
-                writer.close();
+                //BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\temp\\IngestTest.xml"));
+                //writer.write(ingestFoxml);
+                //writer.close();
                 StringBufferInputStream s = new StringBufferInputStream(ingestFoxml);
                 AutoIngestor.ingestAndCommit(fc.getAPIA(), fc.getAPIM(), s,FORMAT, COMMENT);
             }
@@ -156,11 +174,20 @@ public class FedoraPublisher {
             ex.printStackTrace();
         }
     }
-    public static String getBasicDC(String title,String identifier) {
+   
+    public static String getDC(LWComponent c,String title,String identifier) {
         String dc = new String();
-        dc = "<dc:title>"+title+"</dc:title>";
+        dc +="<oai_dc:dc xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:oai_dc=\"http://www.openarchives.org/OAI/2.0/oai_dc/\">";
+        dc += "<dc:title>"+title+"</dc:title>";
         dc += "<dc:identifier>"+identifier+"</dc:identifier>";
+         for(VueMetadataElement element: c.getMetadataList().getMetadata()) {
+             if(element.getKey().contains(DC_URL)) {
+                String key = "dc:"+element.getKey().substring(DC_URL.length()+1);
+                
+                dc += "<"+key+">"+element.getValue()+"</"+key+">";
+            }
+        }
+         dc += "</oai_dc:dc>";
         return dc;
     }
-    
 }
