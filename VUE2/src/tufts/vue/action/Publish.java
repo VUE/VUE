ec47a8fa7eec466e9ace846b70e3d7925a83e31d
Anoop Kumar <anoopkum@gmail.com>
2007-10-11 22:52:06 +0000

    publish action
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@5986 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/action/Publish.java
+++ VUE2/src/tufts/vue/action/Publish.java
@@ -10,7 +10,7 @@
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
  * the specific language governing rights and limitations under the License.</p>
  *
- * <p>The entire file consists of original code.  Copyright &copy; 2003, 2004 
+ * <p>The entire file consists of original code.  Copyright &copy; 2003, 2004
  * Tufts University. All rights reserved.</p>
  *
  * -----------------------------------------------------------------------------
@@ -25,13 +25,13 @@
 package tufts.vue.action;
 
 import javax.swing.AbstractAction;
+import javax.swing.*;
 import java.io.*;
 import tufts.vue.*;
-import java.util.Iterator;
-import java.util.Vector;
+import java.util.*;
 import java.net.*;
 import org.apache.commons.net.ftp.*;
-import java.awt.Frame;
+import java.awt.*;
 
 import fedora.server.management.FedoraAPIM;
 import fedora.server.utilities.StreamUtility;
@@ -42,10 +42,12 @@ import fedora.server.utilities.StreamUtility;
  * @author  akumar03
  */
 public class Publish extends VueAction  {
+    public static final org.osid.shared.Type fedoraRepositoryType = new edu.tufts.vue.util.Type("tufts.edu","repository","fedora_2_2");
+    private static JMenu menu;
     public static final String LABEL = "Publish";
     /** Creates a new instance of Publish */
- 
-   
+    
+    
     String fileName;
     String tempMETSfileName;
     File activeMapFile;
@@ -56,14 +58,18 @@ public class Publish extends VueAction  {
     public Publish(String label) {
         super(label);
         this.label  = label;
+         //edu.tufts.vue.dsm.impl.VueDataSourceManager.getInstance().addDataSourceListener(this);
+   
     }
+    
+    
     /*
     public Publish(Frame owner,String label) {
         super(label);
         this.owner = owner;
         this.label  = label;
     }
-    */
+     */
     public void act() {
         try {
             Publisher publisher = new Publisher(VUE.getDialogParentAsFrame(), LABEL);
@@ -74,5 +80,5 @@ public class Publish extends VueAction  {
             throw new RuntimeException(ex);
         }
     }
-        
+    
 }
