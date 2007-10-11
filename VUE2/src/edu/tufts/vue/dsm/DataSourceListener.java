3563c8700ff458023e07a40596265145b2fdf7fd
Anoop Kumar <anoopkum@gmail.com>
2007-10-11 13:26:23 +0000

    listener for datasource changes
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@5977 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/edu/tufts/vue/dsm/DataSourceListener.java
@@ -0,0 +1,19 @@
+/*
+ * VueDataSourceListener.java
+ *
+ * Created on October 10, 2007, 4:00 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author akumar03
+ */
+package edu.tufts.vue.dsm;
+
+
+public interface DataSourceListener {
+    public void changed(DataSource[] datasources);
+}
