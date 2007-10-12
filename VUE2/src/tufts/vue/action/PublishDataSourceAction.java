18b908133a3ada533b8a7359f8691d6aef69b76f
Anoop Kumar <anoopkum@gmail.com>
2007-10-12 15:48:04 +0000

    actions created through factory
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@5990 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/tufts/vue/action/PublishDataSourceAction.java
@@ -0,0 +1,48 @@
+/*
+ * PublishDataSourceAction.java
+ *
+ * Created on October 12, 2007, 11:56 AM
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2003-2007
+ * Tufts University. All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ *
+ * @author akumar03
+ */
+
+package tufts.vue.action;
+
+import tufts.vue.*;
+
+public class PublishDataSourceAction extends VueAction   {
+     public static final String LABEL = "Publish";
+    /** Creates a new instance of PublishDataSourceAction */
+    private edu.tufts.vue.dsm.DataSource dataSource;
+    PublishDataSourceAction(edu.tufts.vue.dsm.DataSource dataSource) {
+        super(dataSource.getRepositoryDisplayName());
+        this.dataSource = dataSource;
+    }
+    public void act() {
+        try {
+            Publisher publisher = new Publisher(VUE.getDialogParentAsFrame(), LABEL);
+        } catch (Exception ex) {
+            VueUtil.alert(null, ex.getMessage(), "Publish Error");
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+    }
+    
+}
---
+++ VUE2/src/tufts/vue/action/PublishDataSourceTypeAction.java
@@ -0,0 +1,46 @@
+/*
+ * PublishDataSourceTypeAction.java
+ *
+ * Created on October 12, 2007, 11:56 AM
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2003-2007
+ * Tufts University. All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ *
+ * @author akumar03
+ */
+package tufts.vue.action;
+
+import java.util.*;
+
+import tufts.vue.*;
+
+public class PublishDataSourceTypeAction extends VueAction{
+    private static  Map<org.osid.shared.Type,String> typeLabelMap;
+    
+    PublishDataSourceTypeAction(org.osid.shared.Type type) {
+        super(getTypeLabelMap().get(type));
+    }
+
+    private static Map<org.osid.shared.Type,String> getTypeLabelMap() {
+        if(typeLabelMap == null) {
+            typeLabelMap = new HashMap<org.osid.shared.Type,String> ();
+            typeLabelMap.put(edu.tufts.vue.dsm.DataSourceTypes.FEDORA_REPOSITORY_TYPE,"Fedora 2.2");
+            typeLabelMap.put(edu.tufts.vue.dsm.DataSourceTypes.SAKAI_REPOSITORY_TYPE,"Sakai");
+        }
+        return typeLabelMap;
+    }
+}
