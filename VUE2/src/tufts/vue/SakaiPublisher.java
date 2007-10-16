bbd0a847f7bc650a0f90c687b3c3dbcea4152cfe
Anoop Kumar <anoopkum@gmail.com>
2007-10-16 13:01:09 +0000

    skeleton class, methods need to be implemented
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@6013 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/tufts/vue/SakaiPublisher.java
@@ -0,0 +1,38 @@
+/*
+ * SakaiPublisher.java
+ *
+ * Created on October 15, 2007, 5:03 PM
+ *
+ * <p><b>License and Copyright: </b>The contents of this file are subject to the
+ * Mozilla Public License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.</p>
+ *
+ * <p>The entire file consists of original code.  Copyright &copy; 2003-2007
+ * Tufts University. All rights reserved.</p>
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ *
+ * @author akumar03
+ */
+
+package tufts.vue;
+
+public class SakaiPublisher {
+    
+    /** Creates a new instance of SakaiPublisher */
+    public static void uploadMap(edu.tufts.vue.dsm.DataSource dataSource,Object sakaiFolder, LWMap map) {
+        
+    }
+    
+    public static void uploadMapAll(edu.tufts.vue.dsm.DataSource dataSource,Object sakaiFolder, LWMap map) {
+        
+    }
+}
