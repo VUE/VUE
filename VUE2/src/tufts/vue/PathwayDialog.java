e1d355619da5bc5d479a1b9edc7083c06f9c0a7c
Mike Korcynski <mkorcy@gmail.com>
2007-10-17 02:53:57 +0000

    vue-662 when adding a new pathway make it active
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@6059 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/PathwayDialog.java
+++ VUE2/src/tufts/vue/PathwayDialog.java
@@ -95,7 +95,8 @@ public class PathwayDialog extends JDialog implements ActionListener, KeyListene
                 dialog.setVisible(true);
             } else {
             	LWPathway path = new LWPathway(pathLabel);
-                VUE.getActiveMap().addPathway(path);                             
+                VUE.getActiveMap().addPathway(path);
+                VUE.setActive(LWPathway.class, this, path);
                 dispose();
             }
         }
