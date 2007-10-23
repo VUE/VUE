e37636a0c0edeb70665e232d97760c2254f373b6
Mike Korcynski <mkorcy@gmail.com>
2007-10-23 19:25:31 +0000

    extended popupfactory
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@6243 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/gui/VuePopupFactory.java
+++ VUE2/src/tufts/vue/gui/VuePopupFactory.java
@@ -10,26 +10,28 @@ import javax.swing.MenuElement;
 import javax.swing.Popup;
 import javax.swing.PopupFactory;
 import javax.swing.SwingUtilities;
-/*
- * one day we should come back here and add drop shadows to the menus like they appear
- * on the mac, that'd be a nice touch.
- */
+
 public class VuePopupFactory extends PopupFactory {
     private static final int offset = 5;
-
+    private PopupFactory wrapped;
     private static VuePopupFactory fact;
     
+    public VuePopupFactory (PopupFactory wrapped) {
+        this.wrapped = wrapped;
+    }
+
     public Popup getPopup(Component owner, Component contents, int x, int y) throws IllegalArgumentException {
         if (contents == null) {
             throw new IllegalArgumentException();
         }
+      
         return VueHeavyweightPopup.getInstance (owner, contents, new Point(x,y));
     }
     
     public static VuePopupFactory getInstance()
     {
     	if (fact == null)
-    		fact = new VuePopupFactory();
+    		fact = new VuePopupFactory(PopupFactory.getSharedInstance());
     	
     	return fact;
     }
