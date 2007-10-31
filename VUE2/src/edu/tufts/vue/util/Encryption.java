ba674f5e02729074cb7de55b03612669c26b2450
Anoop Kumar <anoopkum@gmail.com>
2007-10-31 21:53:06 +0000

    encrypts password
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@6452 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/util/Encryption.java
+++ VUE2/src/edu/tufts/vue/util/Encryption.java
@@ -29,35 +29,75 @@ import javax.crypto.Cipher;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
 import java.security.InvalidKeyException;
+import org.apache.commons.codec.binary.Hex;
+
+import tufts.vue.*;
+
+import java.io.*;
 
 
 public class Encryption {
     
     private static String algorithm = "DESede";
+    private static final String KEY_FILE = "vue.key";
     private static Key key = null;
     private static Cipher cipher = null;
-    public static byte[] encrypt(String input) throws Exception    {
-        if(key == null) key = KeyGenerator.getInstance(algorithm).generateKey();
-        if(cipher == null) cipher = Cipher.getInstance(algorithm);
-        
-        cipher.init(Cipher.ENCRYPT_MODE, key);
-        byte[] inputBytes = input.getBytes();
-        return cipher.doFinal(inputBytes);
+    private static Hex hex = new Hex();
+    
+    public static String encrypt(String input)    {
+        try {
+            if(cipher == null) cipher = Cipher.getInstance(algorithm);
+            cipher.init(Cipher.ENCRYPT_MODE, getKey());
+            byte[] inputBytes = input.getBytes();
+            byte[] outputBytes = cipher.doFinal(inputBytes);
+            String output  = new String(hex.encode(outputBytes));
+            return output;
+        }catch(Exception ex) {
+            ex.printStackTrace();
+            return input ;
+        }
     }
     
-    public static String decrypt(byte[] encryptionBytes) throws  Exception {
-        if(key == null) key = KeyGenerator.getInstance(algorithm).generateKey();
-        if(cipher == null) cipher = Cipher.getInstance(algorithm);
-        
-        cipher.init(Cipher.DECRYPT_MODE, key);
-        byte[] recoveredBytes =
-                cipher.doFinal(encryptionBytes);
-        String recovered =
-                new String(recoveredBytes);
-        return recovered;
+    public static String decrypt(String encrypted)  {
+        try {
+            
+            if(cipher == null) cipher = Cipher.getInstance(algorithm);
+            cipher.init(Cipher.DECRYPT_MODE, getKey());
+            byte[] encryptedBytes = hex.decode(encrypted.getBytes());
+            byte[] recoveredBytes =  cipher.doFinal(encryptedBytes);
+            String recovered = new String(recoveredBytes);
+            return recovered;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return encrypted;
+        }
     }
     
-    
+    private static synchronized Key getKey() {
+        try {
+            if(key == null) {
+                File keyFile = new File(VueUtil.getDefaultUserFolder().getAbsolutePath()+File.separator+KEY_FILE);
+                if(keyFile.exists()){
+                    ObjectInputStream is = new ObjectInputStream(new FileInputStream(keyFile));
+                    key = (Key)is.readObject();
+                    is.close();
+                } else { 
+                    key = KeyGenerator.getInstance(algorithm).generateKey();
+                    ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(keyFile));
+                    os.writeObject(key);
+                    os.close();
+                }
+                return key;
+            } else {
+                return key;
+            }
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        
+    }
 }
