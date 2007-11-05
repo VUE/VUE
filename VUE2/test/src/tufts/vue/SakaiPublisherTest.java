07ca83cae51e9683db8d7cd3b9552ec9d7a5edf6
Peter Wilkins <pwilkins@MIT.EDU>
2007-11-05 14:11:10 +0000

    initial version
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@6554 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/test/src/tufts/vue/SakaiPublisherTest.java
@@ -0,0 +1,65 @@
+package tufts.vue;
+
+import edu.tufts.osidimpl.repository.sakai.Type;
+import edu.tufts.vue.dsm.DataSource;
+import junit.framework.TestCase;
+
+/**
+ * @author  akumar03
+ * @version $Revision: 1.1 $ / $Date: 2007-11-05 14:11:10 $ / $Author: peter $
+ */
+
+public class SakaiPublisherTest extends TestCase {
+
+	edu.tufts.vue.dsm.DataSourceManager _dsMgr = null;
+	edu.tufts.vue.dsm.DataSource _ds = null;  // this will be the first Sakai datasource found
+	org.osid.shared.Type SakaiRepositoryType = 
+		new edu.tufts.vue.util.Type("sakaiproject.org","repository","contentHosting");
+	
+	public SakaiPublisherTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		_dsMgr = edu.tufts.vue.dsm.impl.VueDataSourceManager.getInstance();
+		((edu.tufts.vue.dsm.impl.VueDataSourceManager)_dsMgr).load();
+		DataSource[] datasources = _dsMgr.getDataSources();
+		for( int i = 0; i < datasources.length ; i++ )
+		{
+			//System.out.println(datasources[i].getRepositoryType().toString());
+			if( datasources[i].getRepositoryType().isEqual(SakaiRepositoryType) )
+			{
+				_ds = datasources[i];
+				System.out.println( "found this Sakai datasource: " + _ds.getRepositoryType().toString() );
+				break;
+			}
+		}
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testUploadMap() {
+		fail("Not yet implemented");
+	}
+
+	public void testUploadMapAll() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetSessionId() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetServerId() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCookieString() {
+		SakaiPublisher.getCookieString(_ds);
+	}
+
+}
