2505bb6ff09d24e95bb921972b4f5862625ba687
dan heller <dan.heller@tufts.edu>
2007-11-13 18:32:49 +0000

    VUE-815 add style is available in options menu (not needed with ontology checkbox/name)
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@6664 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/ontology/ui/OntologyList.java
+++ VUE2/src/edu/tufts/vue/ontology/ui/OntologyList.java
@@ -18,16 +18,19 @@
 
 package edu.tufts.vue.ontology.ui;
 
-import java.awt.Font;
-
+import javax.swing.border.Border;
 import javax.swing.Box;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-import tufts.vue.VueConstants;
+//not currently needed: VUE-815
+//import tufts.vue.VueConstants;
 import tufts.vue.gui.CheckBoxRenderer;
 import tufts.vue.gui.GUI;
 
 
-/*
+/**
+ *
  * OntologyList.java
  *
  * Created on April 9, 2007, 11:36 AM
@@ -39,7 +42,6 @@ public class OntologyList extends javax.swing.JList implements OntologySelection
     public OntologyList(OntologyViewer viewer)
     {
         super(new OntologyListModel());
-       // setLayout(new java.awt.BorderLayout());
         setCellRenderer(new OntologyListRenderer());
     }
     
@@ -49,44 +51,21 @@ public class OntologyList extends javax.swing.JList implements OntologySelection
     }
     
     public void ontologySelected(OntologySelectionEvent ose)
-    {
-        
-        /*int selection = OntologyBrowser.getBrowser().getSelectedIndex(ose.getSelection());
-        if(selection > 0 && selection < getModel().getSize())
-        {    
-          setSelectedIndex(selection);
-          repaint();
-        }
-        
-        System.out.println("Ontology List -- ontology selected - " + selection);*/
-        
+    {   
         edu.tufts.vue.ontology.Ontology ont = ose.getSelection().getOntology();
         
         for(int i=0;i<getModel().getSize();i++)
         {
             if(getModel().getElementAt(i).equals(ont))
                 setSelectedIndex(i);
-        }
-        
+        }   
     }
     
     public static class OntologyListModel extends javax.swing.DefaultListModel
     {
         
-       /* public void addListDataListener(javax.swing.event.ListDataListener listener)
-        {
-            
-        }
-        
-        public void removeListDataListener(javax.swing.event.ListDataListener listener)
-        {
-            
-        }*/
-        
         public Object getElementAt(int index)
         {
-            //String displayString = (edu.tufts.vue.ontology.OntManager.getOntManager().getOntList().get(index)).toString();
-            //return displayString.substring(8);
             if(index > edu.tufts.vue.ontology.OntManager.getOntManager().getOntList().size() - 1 )
                 return null;
             else
@@ -107,79 +86,73 @@ public class OntologyList extends javax.swing.JList implements OntologySelection
     
     public static class OntologyListRenderer extends javax.swing.DefaultListCellRenderer
     {
-        //private javax.swing.JLabel mLabel = new javax.swing.DefaultListCellRenderer();
-        private javax.swing.border.Border dividerBorder = new tufts.vue.gui.DashBorder(java.awt.Color.LIGHT_GRAY,false,true,false,false);
-        private javax.swing.JLabel importLabel = new javax.swing.JLabel("Add Style Sheet");
+        private Border dividerBorder = new tufts.vue.gui.DashBorder(java.awt.Color.LIGHT_GRAY,false,true,false,false);
+        private Border emptyBorder = javax.swing.BorderFactory.createEmptyBorder();
+
+       //currently disabled as per VUE-815 
+       //private javax.swing.JLabel importLabel = new javax.swing.JLabel("Add Style Sheet");
         
         private CheckBoxRenderer mCheckBox = new CheckBoxRenderer();
-        /*mRow.add(Box.createHorizontalStrut(GUI.WidgetInsets.left));
-        mRow.add(mCheckBox);
-        mRow.add(Box.createHorizontalStrut(GUI.WidgetInsets.left));
-        mRow.add(mLabel);
-        mRow.add(mIconLabel);
-        mRow.add(Box.createHorizontalStrut(GUI.WidgetInsets.right));*/
-        public java.awt.Component getListCellRendererComponent(javax.swing.JList list,Object value,int index,boolean isSelected,boolean hasFocus)
+        
+        private JPanel renderer;
+        private JLabel label;
+        
+        public OntologyListRenderer()
         {
-            //javax.swing.JPanel panel = new javax.swing.JPanel(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
-            //javax.swing.JPanel panel = new javax.swing.JPanel(new java.awt.BorderLayout());
-        	Font f = importLabel.getFont();
-        	
-        		
-        	
-        	importLabel.setFont(VueConstants.FONT_NARROW);
-            javax.swing.JPanel panel = new javax.swing.JPanel()
+            renderer = new JPanel()
             {
                public java.awt.Dimension getPreferredSize()
                {
                    return new java.awt.Dimension(100,23);
                }
             };
-           // panel.setPreferredSize(new javax.swing.DefaultListCellRenderer().getPreferredSize());
-            panel.setLayout(new javax.swing.BoxLayout(panel,javax.swing.BoxLayout.X_AXIS));
             
-            //panel.setBorder(javax.swing.BorderFactory.createMatteBorder(0,0,1,0,new java.awt.Color(200,200,200)));
+            renderer.setLayout(new javax.swing.BoxLayout(renderer,javax.swing.BoxLayout.X_AXIS));
+            
+            renderer.setOpaque(true);
+            
+            label = new JLabel();
+            
+            renderer.add(Box.createHorizontalStrut(GUI.WidgetInsets.left));
+            renderer.add(mCheckBox);           
+            renderer.add(Box.createHorizontalStrut(GUI.WidgetInsets.left));
+            renderer.add(label);
+        }
+
+        public java.awt.Component getListCellRendererComponent(javax.swing.JList list,Object value,int index,boolean isSelected,boolean hasFocus)
+        {	
+            //currently disabled as per VUE-815
+            //importLabel.setFont(VueConstants.FONT_NARROW);
+            
             if(index!=0)
-              panel.setBorder(dividerBorder);
-            
-            //panel.setOpaque(true);
-            // panel.setBackground(java.awt.Color.BLUE);
+              renderer.setBorder(dividerBorder);
+            else
+              renderer.setBorder(emptyBorder);
             
             edu.tufts.vue.ontology.Ontology ontology = (edu.tufts.vue.ontology.Ontology)value;
-         /*   
-            String base = ontology.getBase();
-            String baseWithoutFileType = base.substring(0,base.lastIndexOf("."));
-            String displayString = "";
-            int baseIndex = baseWithoutFileType.lastIndexOf("/");
-            if(baseIndex>0)
-                displayString = baseWithoutFileType.substring(baseIndex+1);
-            else
-                displayString = baseWithoutFileType;
-          */
-            javax.swing.JLabel label = new javax.swing.JLabel(ontology.getLabel());
+
+            label.setText(ontology.getLabel());
             
             mCheckBox.setSelected(ontology.isEnabled());
-            panel.add(Box.createHorizontalStrut(GUI.WidgetInsets.left));
-            panel.add(mCheckBox);           
-            panel.add(Box.createHorizontalStrut(GUI.WidgetInsets.left));
-            panel.add(label);
      
-            panel.setOpaque(true);
             if(value == list.getSelectedValue())
             {
-                panel.setBackground(tufts.vue.gui.GUI.getTextHighlightColor());
+                renderer.setBackground(tufts.vue.gui.GUI.getTextHighlightColor());
             }
             else
             {
-                panel.setBackground(new java.awt.Color(255,255,255));
+                renderer.setBackground(new java.awt.Color(255,255,255));
             }
             
-            if(ontology.getStyle() == null)
+            // next lines left here in case we ever want to add the import label back in 
+            // note: also uncomment lines out above- search for VUE-815
+            /*if(ontology.getStyle() == null)
             {   
                 panel.add(javax.swing.Box.createHorizontalGlue());
                 importLabel.setBorder(javax.swing.BorderFactory.createEmptyBorder(0,0,0,5));
                 panel.add(importLabel);
-            }
-            return panel;
+            }*/
+            return renderer;
         }
     }
     
