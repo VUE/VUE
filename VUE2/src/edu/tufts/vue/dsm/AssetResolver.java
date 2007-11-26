b578a49f66a59de9075221b182360820f2f78637
Peter Wilkins <pwilkins@MIT.EDU>
2007-11-26 20:35:49 +0000

    Replace Mozilla license with ECL 2.0 license
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@6803 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/dsm/AssetResolver.java
+++ VUE2/src/edu/tufts/vue/dsm/AssetResolver.java
@@ -1,22 +1,18 @@
-package edu.tufts.vue.dsm;
-
 /*
- * -----------------------------------------------------------------------------
- *
- * <p><b>License and Copyright: </b>The contents of this file are subject to the
- * Mozilla Public License Version 1.1 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License
- * at <a href="http://www.mozilla.org/MPL">http://www.mozilla.org/MPL/.</a></p>
- *
- * <p>Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.</p>
- *
- * <p>The entire file consists of original code.  Copyright &copy; 2006 
- * Tufts University. All rights reserved.</p>
- *
- * -----------------------------------------------------------------------------
+ * Copyright 2003-2007 Tufts University  Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ * 
+ * http://www.osedu.org/licenses/ECL-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
  */
+package edu.tufts.vue.dsm;
 
 /**
 Repository OSID Assets may be present in VUE maps or other constructs.  Since the
