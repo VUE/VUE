6d24c6b7f6d6323889f930ffef1b544bab6dd025
Peter Wilkins <pwilkins@MIT.EDU>
2007-11-26 20:01:31 +0000

    Replaced Mozilla license with ECL 2.0 license
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@6801 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/shape/Order1.java
+++ VUE2/src/tufts/vue/shape/Order1.java
@@ -1,8 +1,16 @@
 /*
- * @(#)Order1.java	1.9 03/01/23
- *
- * Copyright 2003 Sun Microsystems, Inc. All rights reserved.
- * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * Copyright 2003-2007 Tufts University  Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ * 
+ * http://www.osedu.org/licenses/ECL-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
  */
 
 package tufts.vue.shape;
