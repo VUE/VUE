1122dc3c7c641449fbfbb4bb4b3250b065b73fc2
Anoop Kumar <anoopkum@gmail.com>
2008-02-26 20:36:33 +0000

    added dc url
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@7072 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/osidimpl/repository/fedora_2_2/SubjectPartStructure.java
+++ VUE2/src/edu/tufts/osidimpl/repository/fedora_2_2/SubjectPartStructure.java
@@ -18,7 +18,7 @@ public class SubjectPartStructure
 implements org.osid.repository.PartStructure
 {
     private String displayName = "Subject";
-    private String description = "Subject";
+    private String description = "http://purl.org/dc/elements/1.1/#Subject"; 
     private String idString = "Subject.PartStructureId";
     private org.osid.shared.Id id = null;
     private org.osid.shared.Type type = new Type("mit.edu","partStructure","subject","Subject");
