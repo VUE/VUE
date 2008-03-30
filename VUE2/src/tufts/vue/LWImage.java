5e0f7e5cc635c4d776882ec8cf376c36928c5dce
Mike Korcynski <mkorcy@gmail.com>
2008-03-30 22:45:19 +0000

    vue-1093
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@7233 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/LWImage.java
+++ VUE2/src/tufts/vue/LWImage.java
@@ -598,7 +598,7 @@ public class LWImage extends
 			If below stops autoShapeToAspect from being called with default data,
 			as well as cases where it'd be moot anyway.
 		*/
-  //      if (!(w == h && mImageAspect == 1.0))
+        if (!(w == h && mImageAspect == 1.0))
         	autoShapeToAspect();
         
         //setAspect(aspect); // LWComponent too paternal for us right now
@@ -617,7 +617,7 @@ public class LWImage extends
             	if ((DEBUG.Enabled || DEBUG.IMAGE) && newSize.width != width || newSize.height !=height)
             		System.out.println("autoShapeToAspect in:" + width + "," + height + " out newSize: " + newSize.width + "," + newSize.height);
             	//if (DEBUG.IMAGE) out("autoShapeToAspect out: " + newSize);
-            	//setSize(newSize.width, newSize.height);
+            	setSize(newSize.width, newSize.height);
         }
     }
 
