b03b8b8768d65bceaf49868427c499e2ee233c9a
Mike Korcynski <mkorcy@gmail.com>
2008-04-22 02:33:17 +0000

    modified html output, works in safari, ie, ff..not quite what i wanted as IE7 refuses to correctly obey display:none applied to links, and refuses to apply opacity to elements unless they're laid out, this feels like a hack because of it, and should be revisited later.  old html output broken for various reasons, the biggie being firefox does not support hover alt tags like IE did.
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@7526 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/action/ImageMap.java
+++ VUE2/src/tufts/vue/action/ImageMap.java
@@ -15,16 +15,15 @@
 
 package tufts.vue.action;
 
-import javax.imageio.*;
-import java.awt.image.*;
+
 import java.io.*;
+import java.util.Iterator;
 import java.awt.*;
-import javax.swing.*;
 import java.awt.geom.Rectangle2D;
 import tufts.vue.*;
 
 /**
- * @version $Revision: 1.18 $ / $Date: 2007-11-29 20:27:00 $ / $Author: dan $ *
+ * @version $Revision: 1.19 $ / $Date: 2008-04-22 02:33:17 $ / $Author: mike $ *
  * @author  Jay Briedis
  */
 public class ImageMap extends VueAction {
@@ -74,31 +73,84 @@ public class ImageMap extends VueAction {
        createHtml(imageName, fileName);
     }
     
-    private String computeImageMapArea(LWContainer container)
+  
+    private int nodeCounter = 0;
+    private String writeOutCss(String imageName,LWContainer container)
     {
         String out = new String();
          
-        java.util.Iterator iter =  container.getNodeIterator();
+        //java.util.Iterator iter =  container.getNodeIterator();
+        Iterator iter = container.getAllDescendents(LWComponent.ChildKind.PROPER).iterator();
+       
         
         while(iter.hasNext())
         {
-            LWNode node = (LWNode)iter.next();
+        	LWNode node = null;
+        	
+            LWComponent comp = (LWComponent)iter.next();
+            
+            if (comp instanceof LWNode && (!(comp instanceof LWGroup)))
+            	node = (LWNode)comp;
+            else
+            	continue;
             
             if (node.hasChildren())
-              out += computeImageMapArea((LWContainer)node);
+              out += writeOutCss(imageName,(LWContainer)node);
+                
+            int groupX=0;
+            int groupY=0;
+            
+            if (node.getParent() instanceof LWGroup)
+            {
+            	groupX =(int)node.getParent().getX();
+            	groupY =(int)node.getParent().getY();
+            }	
+            String res = "";
+            int ox = (int)node.getX() + groupX -  xOffset;
+            int oy = (int)node.getY() + groupY -  yOffset;
+            int ow = (int)node.getWidth();
+            int oh = (int)node.getHeight();
+                        
+            out+="#map a.node" + nodeCounter +"{\n";
+            out+="	top:"+ oy + "px;\n";
+            out+="	left:"+ ox + "px;\n";
+            out+="	width:" +ow + "px;\n";
+            out+="	height:" + oh + "px;\n";
+    		out+="}\n\n";
+    	//	System.out.println("Write out CSS : " + nodeCounter + " "+ node.getLabel());            
+    		nodeCounter++;
+        }
+        
+        return out;
+    }
+    private String writeOutLi(LWContainer container)
+    {
+        String out = new String();
+         
+        //java.util.Iterator iter =  container.getNodeIterator();
+        Iterator iter = container.getAllDescendents(LWComponent.ChildKind.PROPER).iterator();
+        
+        while(iter.hasNext())
+        {
+        	LWNode node = null;
+        	
+            LWComponent comp = (LWComponent)iter.next();
+            
+            if (comp instanceof LWNode && (!(comp instanceof LWGroup)))
+            	node = (LWNode)comp;
+            else
+            	continue;
+            
+            if (node.hasChildren())
+              out += writeOutLi((LWContainer)node);
             
             String shape = "rect";
             String altLabel = null;
             
-            if ((altLabel = node.getNotes()) == null)
-              altLabel = "No Notes";
+            if ((altLabel = node.getNotes()) == null)            	
+            	altLabel = "No Notes";
             
             String res = "";
-            int ox = (int)node.getX() -  xOffset;
-            int oy = (int)node.getY() -  yOffset;
-            int ow = (int)node.getWidth();
-            int oh = (int)node.getHeight();
-            
             String href = "";
             
             if(node.getResource() != null){
@@ -114,14 +166,14 @@ public class ImageMap extends VueAction {
             if(res.equals("null")) href = "nohref";
             else href = "href=\"" + res + "\" target=\"_blank\"";
             
-            out += "<area " + href
-                +" alt=\""+ altLabel
-                +"\" shape=\""+shape
-                +"\" coords=\""+ox
-                +","+oy
-                +","+(ox + ow)
-                +","+(oy + oh)
-                +"\">\n\n";
+            	if (href.equals("nohref"))
+            		out+="<li><a class=\"node" + nodeCounter+"\" href=\"#\" ><span><b>";
+            	else
+            		out+="<li><a class=\"node" + nodeCounter+"\"" + " " + href+" ><span><b>";            		
+            	out+=altLabel +"\n";
+            	out+="</b><br></span></a></li>\n";
+    		//System.out.println("Write out CSS : " + nodeCounter + " "+ node.getLabel());      
+            nodeCounter++;
         }
          
         return out;
@@ -131,20 +183,57 @@ public class ImageMap extends VueAction {
         
         LWMap currentMap = VUE.getActiveMap();   
         Rectangle2D bounds = currentMap.getBounds();
-        xOffset = (int)bounds.getX(); 
-        yOffset = (int)bounds.getY();
+        xOffset = (int)bounds.getX()-30; 
+        yOffset = (int)bounds.getY()-30;
         Dimension size = new Dimension((int)bounds.getWidth(), (int)bounds.getHeight());
         
         String out = "<HTML><HEAD><TITLE>" + currentMap.getLabel();
-        out += "</TITLE></HEAD><BODY>";
-        out += "<img src=\""+imageName
-        	+"\" border=0 usemap=\"#map\">";
-            //+"\" border=0 usemap=\"#map\" HEIGHT="+size.height+" WIDTH="+size.width+">";
-        out += "<map name=\"map\">\n";
-
-        out += computeImageMapArea(currentMap);
+        out += "</TITLE>";
+        //write out css
+    
+        out +="<style type=\"text/css\">\n";
+        out+="#map {\n";
+        out+="	margin:0;\n";
+        out+="	padding:0;\n";
+        out+="	width:"+(int)currentMap.getWidth()+"px;\n";
+        out+="	height:"+(int)currentMap.getHeight()+"px;\n";
+        out+="	background:url(" + imageName +") top left no-repeat #fff;\n";
+        out+="	font-family:arial, helvetica, sans-serif;\n";
+        out+="	font-size:8pt;}\n\n";
+        out+="#map li {\n";
+        out+="	margin:0;\n";
+        out+="	padding:0;\n";
+        out+="	list-style:none;}\n\n";
+        out+="#map li a {\n";
+        out+="	position:absolute;\n";
+        out+="	width:50px;\n";
+        out+="	height:50px;\n";
+        out+="	display:block;\n";
+        out+="	text-decoration:none;\n";
+        out+="	color:#000;}\n\n";
+        out+="#map li a span { width:300px;\n";
+        out+="	display:block;\n";
+        out+="	filter: alpha(opacity=0);\n";
+        out+="	opacity:0.0; }\n\n";
+        out+="#map li a:hover span {\n";
+        out+="	position:relative;\n";
+        out+="	display:block;\n";
+        out+="	width:350px;\n";
+        out+="	left:1px;\n";
+        out+="	top:1px;\n";
+        out+="	border:1px solid #000;\n";
+        out+="	background:#fff;\n";
+        out+="	padding:5px;\n";
+        out+="	filter:alpha(opacity=80);\n";
+        out+="	opacity:0.8;}\n\n";
+  		
+        out += writeOutCss(imageName,currentMap);
+        out += "</style></head><body>\n";
+        out += "<ul id =\"map\">";
+        nodeCounter =0;
+        out += writeOutLi(currentMap);
         
-        out += "\n</map></BODY></HTML>"; 
+        out += "\n</ul></body></html>"; 
         
         //write out to the selected file
         try{
