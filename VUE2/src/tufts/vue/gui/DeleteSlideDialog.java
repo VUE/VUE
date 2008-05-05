d0a40dd06d57ca1abf06e80e7676af82a946eae2
Mike Korcynski <mkorcy@gmail.com>
2008-05-05 18:03:22 +0000

    initial checkin
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@7615 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/tufts/vue/gui/DeleteSlideDialog.java
@@ -0,0 +1,21 @@
+package tufts.vue.gui;
+
+import java.awt.Frame;
+import java.awt.Point;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import edu.tufts.vue.preferences.implementations.ShowAgainDialog;
+
+public class DeleteSlideDialog extends ShowAgainDialog{
+
+	private final JPanel panel = new JPanel();    	
+
+	 public DeleteSlideDialog(Frame parentFrame)
+	 {		 	 
+		super(parentFrame,"deleteSlide","Delete Pathway");
+	    panel.add(new JLabel("By deleting this slide, your work on this slide will be lost."));
+	    setContentPanel(panel);
+	 }
+}
