ce5516dc6693b5fd3c4aa8a55f16a996ec9896f4
Mike Korcynski <mkorcy@gmail.com>
2008-06-30 20:53:48 +0000

    udpated copyright 2008
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@7941 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/action/ImageConversion.java
+++ VUE2/src/tufts/vue/action/ImageConversion.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2007 Tufts University  Licensed under the
+ * Copyright 2003-2008 Tufts University  Licensed under the
  * Educational Community License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License. You may
  * obtain a copy of the License at
@@ -24,7 +24,7 @@ import javax.swing.*;
 import tufts.vue.*;
 
 /**
- * @version $Revision: 1.25 $ / $Date: 2008-06-30 20:44:59 $ / $Author: sfraize $ *
+ * @version $Revision: 1.26 $ / $Date: 2008-06-30 20:53:48 $ / $Author: mike $ *
  * @author  Daisuke Fujiwara
  */
 
