8f454616de27116210223c95ea14c51fb341c362
Anoop Kumar <anoopkum@gmail.com>
2008-08-27 15:58:09 +0000

    refactoring layout
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@8121 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/edu/tufts/vue/layout/Layout.java
@@ -0,0 +1,43 @@
+/*
+ * Layout.java
+ *
+ * Created on August 6, 2008, 1:08 PM
+ *
+ * Copyright 2003-2008 Tufts University  Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+
+/**
+ *
+ * @author akumar03
+ */
+
+package edu.tufts.vue.layout;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+import tufts.vue.*;
+import edu.tufts.vue.dataset.*;
+
+public abstract class Layout {
+    public  double  MAP_SIZE = 500;
+    public  int MAX_SIZE =5000;
+    /** Creates a new instance of Layout */
+    public Layout() {
+    }
+    public abstract LWMap createMap(Dataset ds,String mapName) throws Exception;
+    
+}
