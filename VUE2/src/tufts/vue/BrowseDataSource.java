f9232ece33113fc6280ecbdd53151f857fae1754
Scott Fraize <scott.fraize@gmail.com>
2008-09-16 11:55:40 +0000

    generic openAddress for data sources & authentication cookie handling
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@8137 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/BrowseDataSource.java
+++ VUE2/src/tufts/vue/BrowseDataSource.java
@@ -23,21 +23,32 @@ package tufts.vue;
  * on the configuration.  E.g., a local directory, a list of user favorites, a remote FTP
  * site, an RSS feed, etc.
  * 
- * @version $Revision: 1.1 $ / $Date: 2008-07-07 21:01:13 $ / $Author: sfraize $
+ * @version $Revision: 1.2 $ / $Date: 2008-09-16 11:55:40 $ / $Author: sfraize $
  * @author  rsaigal
  * @author  sfraize
  */
 
 import tufts.vue.DEBUG;
 
+import java.util.Map;
+import java.util.List;
+import java.net.URL;
+import java.net.URLConnection;
 import javax.swing.JComponent;
+
+import edu.tufts.vue.ui.ConfigurationUI;
 
 public abstract class BrowseDataSource implements DataSource
 {
     private static final org.apache.log4j.Logger Log = org.apache.log4j.Logger.getLogger(BrowseDataSource.class);
 
+    public static final String AUTHENTICATION_COOKIE_KEY = "url_authentication_cookie";
+
+    //protected static final String JIRA_SFRAIZE_COOKIE = "seraph.os.cookie=LkPlQkOlJlHkHiEpGiOiGjJjFi"; // TODO: test hack -- remove
+    
     private String displayName = "(unconfigured)";
     private String address;
+    private String authenticationCookie;
     private String Id;
     private boolean isAutoConnect;
     private boolean isIncludedInSearch;
@@ -53,6 +64,23 @@ public abstract class BrowseDataSource implements DataSource
         setDisplayName(name);
     }
 
+    /** parameter block that can be used for generating XML in EditLibraryPanel to add fields to the data-source UI */
+    public static class ConfigField {
+        public String key;
+        public String title;
+        public String description;
+        public String value;
+        public int uiControl = ConfigurationUI.SINGLE_LINE_CLEAR_TEXT_CONTROL;
+        public int maxLen;
+
+        public ConfigField(String k, String t, String d, String v) {
+            key = k;
+            title = t;
+            description = d;
+            value = v;
+        }
+    }
+
     // todo: to persist extra properties (e.g., authentication keys) add a getPropertyList for
     // castor that returns PropertyEntry's to persist extra key/values.  Could use PropertyMap and
     // add a convert to list (URLResource just does this manually when requested for persistance by
@@ -63,6 +91,9 @@ public abstract class BrowseDataSource implements DataSource
      * to add additional properties of their own.  This is used by EditLibraryPanel to
      * pass the result of user property edits back into the VueDataSource.
      */
+    // todo: this is really just a bean interface: would be much easier to handle that way
+    // using freely availble libraries (e.g., apache), and we could avoid yet enother
+    // set of property key/value dispatch code.
     public void setConfiguration(java.util.Properties p) {
         String val = null;
         
@@ -78,6 +109,29 @@ public abstract class BrowseDataSource implements DataSource
         } catch (Throwable t) {
             Log.error("val=" + val, t);
         }
+        try {
+            if ((val = p.getProperty(AUTHENTICATION_COOKIE_KEY)) != null)
+                setAuthenticationCookie(val);
+        } catch (Throwable t) {
+            Log.error("val=" + val, t);
+        }
+        
+    }
+
+    public java.util.List<ConfigField> getConfigurationUIFields() {
+        List<ConfigField> fields = new java.util.ArrayList();
+
+        fields.add(new ConfigField("address",
+                                   "Address",
+                                   getTypeName() + " URL",
+                                   getAddress()));
+            
+        fields.add(new ConfigField(AUTHENTICATION_COOKIE_KEY,
+                                   "Authentication",
+                                   "Any required authentication cookie (optional)",
+                                   getAuthenticationCookie()));
+
+        return fields;
     }
 
     public String getTypeName() {
@@ -85,6 +139,8 @@ public abstract class BrowseDataSource implements DataSource
     }
     
     public final void setAddress(String newAddress) {
+        if (newAddress != null)
+            newAddress = newAddress.trim();
         out("setAddress[" + newAddress + "]");
         if (newAddress != null && !newAddress.equals(address)) {
             this.address = newAddress;
@@ -105,6 +161,21 @@ public abstract class BrowseDataSource implements DataSource
     public final String getAddress() {
         return this.address;
     }
+
+    public void setAuthenticationCookie(String s) {
+        Log.debug("setAuthenticationCookie[" + s + "]");
+        if (s == authenticationCookie || (s != null && s.equals(authenticationCookie))) {
+            return;
+        } else {
+            authenticationCookie = s;
+            unloadViewer();
+        }
+    }
+    
+    public String getAuthenticationCookie() {
+        return authenticationCookie;
+    }
+    
 
     /** impl's may override to provide a count of the items in the view
      * @return -1 by defaut
@@ -230,6 +301,68 @@ public abstract class BrowseDataSource implements DataSource
     public void setIncludedInSearch(boolean included) {
         this.isIncludedInSearch = included;
     }
+
+    protected URLConnection openAddress() {
+
+        String addressText = getAddress();
+
+        Log.debug("openAddress " + addressText);
+        
+        if (addressText.toLowerCase().startsWith("feed:"))
+            addressText = "http:" + addressText.substring(5);
+        
+        URL address = null;
+        
+        try {
+            address = new URL(addressText);
+        } catch (Throwable t) {
+            throw new DataSourceException("Bad address in " + getClass().getSimpleName(), t);
+        }
+        
+        Map<String,List<String>> headers = null;
+
+        try {
+            if (DEBUG.Enabled) Log.debug("opening " + address);
+            URLConnection conn = address.openConnection(); 
+            conn.setRequestProperty("User-Agent","Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.8) Gecko/20071008 Firefox/2.0.0.8");
+            
+            if (getAuthenticationCookie() != null)
+                conn.setRequestProperty("Cookie", getAuthenticationCookie());
+            
+            if (DEBUG.Enabled) Log.debug("request-properties: " + conn.getRequestProperties());
+            
+            conn.connect();
+
+            if (DEBUG.Enabled) {
+
+                Log.debug("connected; fetching headers [" + conn + "]");
+                
+                final StringBuilder buf = new StringBuilder(512);
+                
+                buf.append("headers [" + conn + "];\n");
+                
+                headers = conn.getHeaderFields();
+                
+                List<String> response = headers.get(null);
+                if (response != null)
+                    buf.append(String.format("%20s: %s\n", "HTTP-RESPONSE", response));
+                
+                for (Map.Entry<String,List<String>> e : headers.entrySet()) {
+                    if (e.getKey() != null)
+                        buf.append(String.format("%20s: %s\n", e.getKey(), e.getValue()));
+                }
+                
+                Log.debug(buf);
+                
+            }
+
+            return conn;
+            
+        } catch (java.io.IOException io) {
+            throw new DataSourceException(null, io);
+        }
+    }
+    
     
     @Override
     public final String toString() {
