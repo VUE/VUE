fb5bd930110da4491cab87518ac41e648105531a
Anoop Kumar <anoopkum@gmail.com>
2008-11-26 19:53:15 +0000

    stretch - works for nodes within a selection
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@8382 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/layout/StretchLayout.java
+++ VUE2/src/edu/tufts/vue/layout/StretchLayout.java
@@ -36,18 +36,35 @@ public class StretchLayout extends Layout {
     
     public void layout(LWSelection selection) {
         // compute the bounding rectangle of selection
-        int X_PADDING = 80; // applied only on top and left
-        int Y_PADDING = 40;
+        int X_PADDING = 40; // applied only on top and left
+        int Y_PADDING = 20;
         int MAX_X_MOVE =80;
         int MAX_Y_MOVE = 40;
+        double MAX_SHIFT_PCT = .1;
+        double SHIFT_RANGE_PCT = 1;
         Rectangle2D boundingRect = selection.getBounds();
         double maxX = boundingRect.getMaxX();
         double maxY = boundingRect.getMaxY();
         double minX = boundingRect.getMinX();
         double minY = boundingRect.getMinY();
-        double xRange =  Math.abs(maxX-minX);
-        double yRange =  Math.abs(maxY-minY);
+        final double centerX = boundingRect.getCenterX();
+        final double centerY = boundingRect.getCenterY();
+        double xRange =  Math.abs(maxX-minX)/2;
+        double yRange =  Math.abs(maxY-minY)/2;
+        // determine the axis a & b of the ellipse that bounds the rectangle
+        double angleTopLeft = Math.atan((centerY-minY)/(centerX-minX));
+        double a =  xRange;
+        double b = yRange;
+        double ptEllipseX = centerX - a*Math.cos(angleTopLeft);
+        double ptEllipseY = centerY - b*Math.sin(angleTopLeft);
+        double distPt = Point2D.distance(ptEllipseX,ptEllipseY,centerX,centerY);
+        double distTopLeft = Point2D.distance(minX,minY,centerX,centerY);
+        double ratio = distTopLeft/distPt;
+        double outerA = a* ratio;
+        double outerB = b*ratio;
         
+        System.out.println("ELLIPSE: a:"+a+" b: "+b+" xRrange:" + xRange+" yRange:"+yRange+" ratio:"+ratio);
+        System.out.printf("center: %.2f,%.2f\n",centerX,centerY);
         Point2D topLeft =  new Point();
         topLeft.setLocation(minX,minY );
         Point2D bottomLeft =  new Point();
@@ -76,49 +93,31 @@ public class StretchLayout extends Layout {
                 Point2D location = c.getLocation();
                 double newX = location.getX();
                 double newY = location.getY();
+                double y = location.getY();
+                double x =location.getX();
+                double angle = Math.atan2(centerY-y,x-centerX);
                 
                 if(boundingRect.contains(location)) {
-                    if(slopeDown.relativeCCW(location)>0) {
-                        if(slopeUp.relativeCCW(location)>0)  {
-                            newY = minY -Y_PADDING;
-                        } else {
-                            newX = maxX;
-                        }
-                    } else {
-                        if(slopeUp.relativeCCW(location)>0) {
-                            newX = minX-X_PADDING;
-                        } else{
-                            newY = maxY;
-                        }
-                    }
-                    
-                    System.out.println("Setting new location for: "+c.getLabel());
+                    newX = centerX-X_PADDING+outerA*Math.cos(angle);
+                    newY = centerY-Y_PADDING-outerB*Math.sin(angle);
+                    System.out.println("Setting new location for: "+c.getLabel()+" angle:"+Math.toDegrees(angle));
+                      System.out.printf("x:%3.2f,y:%3.2f,newX:%3.2f,newY:%3.2f\n",x,y,newX,newY);
                 } else {
-                    double y = location.getY();
-                    double x =location.getX();
-                    double topDistance = Math.abs(y-minY);
-                    double bottomDistance = Math.abs(y-maxY);
-                    double leftDistance = Math.abs(x-minX);
-                    double rightDistance = Math.abs(x-maxX);
-                    if(topDistance<bottomDistance && topDistance< yRange && x> minX && x < maxX) {
-                        double dY = (yRange - topDistance)*MAX_Y_MOVE/yRange;
-                        newY  -= dY;
-                        System.out.println("Moving top: "+c.getLabel()+"y= "+y+" newY "+newY+" dY"+dY);
-                    } 
-                    if (bottomDistance<topDistance && bottomDistance<yRange && x>minX &&  x<maxX) {
-                        double dY = (yRange - bottomDistance)*MAX_Y_MOVE/yRange;
-                        newY += dY;
-                        System.out.println("Moving bottom: "+c.getLabel()+"y= "+y+" newY "+newY+" dY"+dY);  
-                    } 
-                    if(leftDistance<rightDistance&& leftDistance<xRange && x<minX  ){
-                        double dX = (xRange-leftDistance)* MAX_X_MOVE/xRange;
-                        newX -= dX;
-                    } 
-                    if(rightDistance<leftDistance && rightDistance<xRange && y>minY && y<maxY) {
-                        double dX = (xRange-rightDistance)* MAX_X_MOVE/xRange;
-                        newX += dX;
+                    /*
+                    double distance = Point2D.distance(x,y,centerX,centerY);
+                    double xInnerEllipse = centerX+a*Math.cos(angle);
+                    double yInnerEllipse = centerY+b*Math.sin(angle);
+                    double distEllipse = Point2D.distance(x,y,xInnerEllipse,yInnerEllipse);
+                    double distPoint = Point2D.distance(xInnerEllipse,yInnerEllipse,centerX,centerY);
+                    System.out.printf("distPoint:%.2f,distEllipse:%.2f,%s\n",distPoint,distEllipse,c.getLabel());
+                    if(distEllipse<SHIFT_RANGE_PCT*distPoint) {
+                        double factor =1+ (SHIFT_RANGE_PCT -distEllipse/distPoint)*MAX_SHIFT_PCT/SHIFT_RANGE_PCT;
+                        newX = centerX+outerA*factor*Math.cos(angle);
+                       newY = centerY+outerB*factor*Math.sin(angle);
+                       System.out.println("Shifting node: "+c.getLabel());
+                       System.out.printf("factor: %3.2f, x:%3.2f,y:%3.2f,newX:%3.2f,newY:%3.2f ",factor,x,y,newX,newY);
                     }
-                    
+                      */               
                 }
                 c.setLocation(newX,newY);
             }
