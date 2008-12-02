a07b43e9d1bf03182cdc0e025852e3619fad1729
Anoop Kumar <anoopkum@gmail.com>
2008-12-02 18:25:59 +0000

    works decently needs few more tweaks
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@8388 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/layout/StretchLayout.java
+++ VUE2/src/edu/tufts/vue/layout/StretchLayout.java
@@ -40,8 +40,9 @@ public class StretchLayout extends Layout {
         int Y_PADDING = 20;
         int MAX_X_MOVE =80;
         int MAX_Y_MOVE = 40;
-        double MAX_SHIFT_PCT = .1;
-        double SHIFT_RANGE_PCT = 1;
+        double MAX_SHIFT = 1.2;
+        double SHIFT_RANGE  = 1.5;
+        double EXPAND = 1.25; // expands the ellipse by a factor of 1.1
         Rectangle2D boundingRect = selection.getBounds();
         double maxX = boundingRect.getMaxX();
         double maxY = boundingRect.getMaxY();
@@ -60,8 +61,8 @@ public class StretchLayout extends Layout {
         double distPt = Point2D.distance(ptEllipseX,ptEllipseY,centerX,centerY);
         double distTopLeft = Point2D.distance(minX,minY,centerX,centerY);
         double ratio = distTopLeft/distPt;
-        double outerA = a* ratio;
-        double outerB = b*ratio;
+        double outerA = a*ratio*EXPAND;
+        double outerB = b*ratio*EXPAND;
         
         System.out.println("ELLIPSE: a:"+a+" b: "+b+" xRrange:" + xRange+" yRange:"+yRange+" ratio:"+ratio);
         System.out.printf("center: %.2f,%.2f\n",centerX,centerY);
@@ -100,9 +101,26 @@ public class StretchLayout extends Layout {
                 if(boundingRect.contains(location)) {
                     newX = centerX-X_PADDING+outerA*Math.cos(angle);
                     newY = centerY-Y_PADDING-outerB*Math.sin(angle);
-                    System.out.println("Setting new location for: "+c.getLabel()+" angle:"+Math.toDegrees(angle));
-                      System.out.printf("x:%3.2f,y:%3.2f,newX:%3.2f,newY:%3.2f\n",x,y,newX,newY);
+//                    System.out.println("Setting new location for: "+c.getLabel()+" angle:"+Math.toDegrees(angle));
+//                      System.out.printf("x:%3.2f,y:%3.2f,newX:%3.2f,newY:%3.2f\n",x,y,newX,newY);
                 } else {
+                    double xInnerEllipse = centerX+a*Math.cos(angle);
+                    double yInnerEllipse = centerY-b*Math.sin(angle);
+                    double xOuterEllipse = centerX+outerA*Math.cos(angle);
+                    double yOuterEllipse = centerY-outerB*Math.sin(angle);
+                    
+                    double distInner = Point2D.distance(centerX,centerY,xInnerEllipse,yInnerEllipse);
+                    double distOuter = Point2D.distance(centerX,centerY,xOuterEllipse,yOuterEllipse);
+                    double distPoint = Point2D.distance(centerX,centerY,x,y);
+                    if(distPoint < SHIFT_RANGE*distOuter) {
+                        double factor = MAX_SHIFT+ (distPoint-distInner)*(SHIFT_RANGE-MAX_SHIFT)/(SHIFT_RANGE*distOuter-distInner);
+                        double newA = outerA*factor;
+                        double newB = outerB*factor;
+                        newX = centerX-X_PADDING+newA*Math.cos(angle);
+                        newY = centerY-Y_PADDING-newB*Math.sin(angle);
+                        
+                    }
+                    
                     /*
                     double distance = Point2D.distance(x,y,centerX,centerY);
                     double xInnerEllipse = centerX+a*Math.cos(angle);
@@ -117,7 +135,7 @@ public class StretchLayout extends Layout {
                        System.out.println("Shifting node: "+c.getLabel());
                        System.out.printf("factor: %3.2f, x:%3.2f,y:%3.2f,newX:%3.2f,newY:%3.2f ",factor,x,y,newX,newY);
                     }
-                      */               
+                     */
                 }
                 c.setLocation(newX,newY);
             }
