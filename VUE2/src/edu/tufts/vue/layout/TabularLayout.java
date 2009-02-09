45e7a502b6a59247af03644eb871b5dcfe3b1239
Anoop Kumar <anoopkum@gmail.com>
2009-02-09 21:41:13 +0000

    adjust the layout based on the size of largest node
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@8666 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/layout/TabularLayout.java
+++ VUE2/src/edu/tufts/vue/layout/TabularLayout.java
@@ -16,13 +16,11 @@
  * or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-
 /**
  *
  * @author akumar03
  */
 package edu.tufts.vue.layout;
-
 
 import java.io.*;
 import java.net.*;
@@ -40,57 +38,66 @@ import edu.tufts.vue.dataset.*;
  * a square grid. Eventually it may be possible to define the number of rows, 
  * columns and cell spacing.
  */
-
-
 public class TabularLayout extends Layout {
-    
+
+    public static final double X_COL_SIZE = 100;
+    public static final double Y_COL_SIZE = 30;
+    public static final double X_SPACING = 15;
+    public static final double Y_SPACING = 12;
+    public static final int DEFAULT_COL = 4;
+
     /** Creates a new instance of TabularLayout */
     public TabularLayout() {
     }
-    public   LWMap createMap(Dataset ds,String mapName) throws Exception {
+
+    public LWMap createMap(Dataset ds, String mapName) throws Exception {
         LWMap map = new LWMap(mapName);
         return map;
     }
-    
-    public   void layout(LWSelection selection) throws Exception {
-        double minX =10000;
-        double minY = 10000;
+
+    public void layout(LWSelection selection) throws Exception {
+        double minX = Double.POSITIVE_INFINITY;
+        double minY = Double.POSITIVE_INFINITY;
+        int mod = DEFAULT_COL;
+        double xAdd = X_COL_SIZE; // default horizontal distance between the nodes
+        double  yAdd = Y_COL_SIZE; //default vertical distance between nodes
         int count = 0;
         int total = 0;
-        int mod = 4;
-        int xAdd = 100;
-        int yAdd = 50;
         Iterator<LWComponent> i = selection.iterator();
-        
         while (i.hasNext()) {
             LWComponent c = i.next();
-            if(c instanceof LWNode) {
-                LWNode node = (LWNode)c;
-                minX = node.getLocation().getX()<minX?node.getLocation().getX():minX;
-                minY =node.getLocation().getY()<minY?node.getLocation().getY():minY;
+            if (c instanceof LWNode) {
+                LWNode node = (LWNode) c;
+                minX = node.getLocation().getX() < minX ? node.getLocation().getX() : minX;
+                minY = node.getLocation().getY() < minY ? node.getLocation().getY() : minY;
+                xAdd = xAdd > node.getWidth() ? xAdd : node.getWidth();
+                yAdd = yAdd > node.getHeight() ? yAdd : node.getHeight();
                 total++;
 //               System.out.println(node.getLabel()+"X= "+node.getLocation().getX()+" Y= "+node.getLocation().getY()+" MIN: "+minX+" : "+minY);
             }
         }
+        xAdd += X_SPACING; // spacing between nodes
+        yAdd += Y_SPACING; // vertical spacing
         double x = minX;
         double y = minY;
-        mod = (int)Math.ceil(Math.sqrt((double)total));
+        mod = (int) Math.ceil(Math.sqrt((double) total));
         i = selection.iterator();
         while (i.hasNext()) {
             LWComponent c = i.next();
-            if(c instanceof LWNode) {
-                LWNode node = (LWNode)c;
+            if (c instanceof LWNode) {
+                LWNode node = (LWNode) c;
                 total++;
-                if(count%mod ==0) {
-                    if(count!=0) y += yAdd;
-                    x =minX;
+                if (count % mod == 0) {
+                    if (count != 0) {
+                        y += yAdd;
+                    }
+                    x = minX;
                 } else {
-                    x+= xAdd;
+                    x += xAdd;
                 }
                 count++;
-                node.setLocation(x,y);
+                node.setLocation(x, y);
             }
         }
     }
-    
 }
