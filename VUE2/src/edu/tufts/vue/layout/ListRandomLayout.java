6e5bdfe22bc3f056488a6557011d8bfb7b88f10b
Anoop Kumar <anoopkum@gmail.com>
2009-02-10 18:27:48 +0000

    code to avoid overlapping nodes
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@8675 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/layout/ListRandomLayout.java
+++ VUE2/src/edu/tufts/vue/layout/ListRandomLayout.java
@@ -16,13 +16,11 @@
  * or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-
 /**
  *
  * @author akumar03
  */
 package edu.tufts.vue.layout;
-
 
 import java.io.*;
 import java.net.*;
@@ -39,83 +37,83 @@ import edu.tufts.vue.dataset.*;
  * by default the area of the square is propotional to the number of nodes in 
  * selection. 
  */
-
 public class ListRandomLayout extends Layout {
+
     public static String DEFAULT_METADATA_LABEL = "default";
-    double nodeSize = 100; // assume the node size to be 100. this can be calculated or set
-        
+    public static final double FACTOR = 1.50;
+    public static final int MAX_COLLISION_CHECK = 20; // check number of times to check for collisions
+
     /** Creates a new instance of ListRandomLayout */
     public ListRandomLayout() {
     }
-    
-    
-    public LWMap createMap(Dataset ds,String mapName) throws Exception{
-        Map<String,LWNode> nodeMap = new HashMap<String,LWNode>();
-        Map<String,Integer> repeatMap = new HashMap<String,Integer>();
+
+    public LWMap createMap(Dataset ds, String mapName) throws Exception {
+        Map<String, LWNode> nodeMap = new HashMap<String, LWNode>();
+        Map<String, Integer> repeatMap = new HashMap<String, Integer>();
         LWMap map = new LWMap(mapName);
         int count = 0;
         // set map size of the map
         double rowCount = ds.getRowList().size();
-        double goodSize =  (int)Math.sqrt(rowCount)*100;
-        MAP_SIZE = MAP_SIZE>goodSize?MAP_SIZE:goodSize;
-        
-        
-        for(ArrayList<String> row: ds.getRowList()) {
+        double goodSize = (int) Math.sqrt(rowCount) * 100;
+        MAP_SIZE = MAP_SIZE > goodSize ? MAP_SIZE : goodSize;
+
+
+        for (ArrayList<String> row : ds.getRowList()) {
             String node1Label = row.get(0);
             LWNode node1;
-            if(!nodeMap.containsKey(node1Label)) {
+            if (!nodeMap.containsKey(node1Label)) {
                 node1 = new LWNode(node1Label);
-                 for(int i=1;i<row.size();i++ ) {
+                for (int i = 1; i < row.size(); i++) {
                     String value = row.get(i);
-                    String key = ((ds.getHeading()==null) || ds.getHeading().size() <i)?DEFAULT_METADATA_LABEL:ds.getHeading().get(i);
+                    String key = ((ds.getHeading() == null) || ds.getHeading().size() < i) ? DEFAULT_METADATA_LABEL : ds.getHeading().get(i);
                     //                   System.out.println("i="+i+" key="+key+" value ="+value);
-                    
+
                     VueMetadataElement vm = new VueMetadataElement();
                     vm.setKey(key);
                     vm.setValue(value);
                     vm.setType(VueMetadataElement.CATEGORY);
                     node1.getMetadataList().addElement(vm);
                 }
-                if(ds.getHeading().size()>1 && ds.getHeading().get(1).equals("resource")) {
+                if (ds.getHeading().size() > 1 && ds.getHeading().get(1).equals("resource")) {
                     Resource resource = node1.getResourceFactory().get(new File(row.get(1)));
                     node1.setResource(resource);
                 }
                 // special hack to demo the dataset laurie baise dataset
-                if(ds.getHeading().size()>6 && ds.getHeading().get(6).equals("Actual")) {
-                    if(row.get(6).equalsIgnoreCase("A")) {
+                if (ds.getHeading().size() > 6 && ds.getHeading().get(6).equals("Actual")) {
+                    if (row.get(6).equalsIgnoreCase("A")) {
                         node1.setFillColor(Color.CYAN);
                     }
                 }
-                nodeMap.put(node1Label,node1);
+                nodeMap.put(node1Label, node1);
                 node1.layout();
                 map.add(node1);
             } else {
                 node1 = nodeMap.get(node1Label);
             }
-            
-            node1.setLocation(MAP_SIZE*Math.random(),MAP_SIZE*Math.random());
-            
+
+            node1.setLocation(MAP_SIZE * Math.random(), MAP_SIZE * Math.random());
+
         }
         return map;
     }
-    
+
     /** It takes a map and scatters the nodes randomly
      * @param map the map for layout
      */
     public void layout(LWMap map) {
-        Iterator<LWNode> nodeIterator =  map.getChildIterator();
-        while(nodeIterator.hasNext()) {
+        Iterator<LWNode> nodeIterator = map.getChildIterator();
+        while (nodeIterator.hasNext()) {
             LWNode node = nodeIterator.next();
-            node.setLocation(MAP_SIZE*Math.random(),MAP_SIZE*Math.random());
+            node.setLocation(MAP_SIZE * Math.random(), MAP_SIZE * Math.random());
         }
     }
-    
+
     public void layout(java.util.List<LWNode> nodeList) {
-        for(LWNode node: nodeList) {
-            node.setLocation(MAP_SIZE*Math.random(),MAP_SIZE*Math.random());
+        for (LWNode node : nodeList) {
+            node.setLocation(MAP_SIZE * Math.random(), MAP_SIZE * Math.random());
         }
     }
-    
+
     public void layout(LWSelection selection) {
         // determine the left corner of the selection and and use that as the 
         // center for layout.  We assume the selection is a rectangle. 
@@ -123,25 +121,72 @@ public class ListRandomLayout extends Layout {
         double minX = Double.POSITIVE_INFINITY;
         double minY = Double.POSITIVE_INFINITY;
         int total = 0;
-        Iterator<LWComponent> i = selection.iterator(); 
+        Iterator<LWComponent> i = selection.iterator();
         while (i.hasNext()) {
             LWComponent c = i.next();
-            if(c instanceof LWNode) {
-                LWNode node = (LWNode)c;
-                minX = node.getLocation().getX()<minX?node.getLocation().getX():minX;
-                minY =node.getLocation().getY()<minY?node.getLocation().getY():minY;
+            if (c instanceof LWNode) {
+                LWNode node = (LWNode) c;
+                minX = node.getLocation().getX() < minX ? node.getLocation().getX() : minX;
+                minY = node.getLocation().getY() < minY ? node.getLocation().getY() : minY;
                 total++;
-             }
+            }
         }
-        double x = minX;
-        double y = minY;
-        double size = Math.sqrt(total)* nodeSize;
+
+        double xSize = Math.sqrt(total) * (X_COL_SIZE + X_SPACING) * FACTOR;
+        double ySize = Math.sqrt(total) * (Y_COL_SIZE + Y_SPACING) * FACTOR;
         i = selection.iterator();
-         while (i.hasNext()) {
+        while (i.hasNext()) {
             LWComponent c = i.next();
-            if(c instanceof LWNode) {
-                LWNode node = (LWNode)c;
-                node.setLocation(minX+size*(Math.random()-0.5),minY+size*(Math.random()-0.5)); 
+            if (c instanceof LWNode) {
+                LWNode node = (LWNode) c;
+                double x = minX + xSize * (Math.random() - 0.5);
+                double y = minY + ySize * (Math.random() - 0.5);
+                int col_count = 0;
+                boolean flag = true;
+                while (flag && col_count < MAX_COLLISION_CHECK) {
+
+                    LWComponent overlapComponent;
+                    if (VUE.getActiveViewer().pickNode((float) x, (float) y) != null) {
+                         x = minX + xSize * (Math.random() - 0.5);
+                         y = minY + ySize * (Math.random() - 0.5);
+ //                       overlapComponent = VUE.getActiveViewer().pickNode((float) x, (float) y);
+//                        x = overlapComponent.getX() + overlapComponent.getWidth() + X_SPACING;
+//                        y = overlapComponent.getY() + overlapComponent.getHeight() + Y_SPACING;
+//                         System.out.println(node.getLabel() + " collided with " + overlapComponent.getLabel() + " set:" + x + "," + y);
+                    } else if (VUE.getActiveViewer().pickNode((float) x + node.getWidth(), (float) y + node.getHeight()) != null) {
+                          x = minX + xSize * (Math.random() - 0.5);
+                         y = minY + ySize * (Math.random() - 0.5);
+//                         overlapComponent = VUE.getActiveViewer().pickNode((float) x + node.getWidth(), (float) y + node.getHeight());
+//                        x = overlapComponent.getX() + overlapComponent.getWidth() + X_SPACING;
+//                        y = overlapComponent.getY() + overlapComponent.getHeight() + Y_SPACING;
+ //                       System.out.println(node.getLabel() + " collided with " + overlapComponent.getLabel() + " set:" + x + "," + y);
+
+                    }   else if (VUE.getActiveViewer().pickNode((float) x + node.getWidth(), (float) y ) != null) {
+                          x = minX + xSize * (Math.random() - 0.5);
+                         y = minY + ySize * (Math.random() - 0.5);
+//                       overlapComponent = VUE.getActiveViewer().pickNode((float) x + node.getWidth(), (float) y );
+//                        x = overlapComponent.getX() + overlapComponent.getWidth() + X_SPACING;
+//                        y = overlapComponent.getY() + overlapComponent.getHeight() + Y_SPACING;
+ //                       System.out.println(node.getLabel() + " collided with " + overlapComponent.getLabel() + " set:" + x + "," + y);
+
+                    }   else if (VUE.getActiveViewer().pickNode((float) x , (float) y + node.getHeight()) != null) {
+                          x = minX + xSize * (Math.random() - 0.5);
+                         y = minY + ySize * (Math.random() - 0.5);
+//                        overlapComponent = VUE.getActiveViewer().pickNode((float) x, (float) y + node.getHeight());
+//                        x = overlapComponent.getX() + overlapComponent.getWidth() + X_SPACING;
+//                        y = overlapComponent.getY() + overlapComponent.getHeight() + Y_SPACING;
+ //                      System.out.println(node.getLabel() + " collided with " + overlapComponent.getLabel() + " set:" + x + "," + y);
+
+                    } else {
+                        flag = false;
+                    }
+
+
+                    col_count++;
+//                    System.out.println("Node: "+node.getLabel()+" count:"+col_count);
+                   
+                }
+                node.setLocation(x, y);
             }
         }
     }
