31075633f22995879e172cc2a5ac4dbf6317cc93
Anoop Kumar <anoopkum@gmail.com>
2009-02-11 21:43:08 +0000

    doesn't move when layout is applied
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@8709 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/layout/ListRandomLayout.java
+++ VUE2/src/edu/tufts/vue/layout/ListRandomLayout.java
@@ -120,6 +120,9 @@ public class ListRandomLayout extends Layout {
         // also compute the total number of nodes in the selection
         double minX = Double.POSITIVE_INFINITY;
         double minY = Double.POSITIVE_INFINITY;
+        double maxX = Double.NEGATIVE_INFINITY;
+        double maxY = Double.NEGATIVE_INFINITY;
+
         double xAdd = X_COL_SIZE; // default horizontal distance between the nodes
         double yAdd = Y_COL_SIZE; //default vertica
         int total = 0;
@@ -130,65 +133,39 @@ public class ListRandomLayout extends Layout {
                 LWNode node = (LWNode) c;
                 minX = node.getLocation().getX() < minX ? node.getLocation().getX() : minX;
                 minY = node.getLocation().getY() < minY ? node.getLocation().getY() : minY;
-                 xAdd = xAdd > node.getWidth() ? xAdd : node.getWidth();
+                maxX = node.getLocation().getX() > maxX ? node.getLocation().getX() : maxX;
+                maxY = node.getLocation().getY() > maxY ? node.getLocation().getY() : maxY;
+                xAdd = xAdd > node.getWidth() ? xAdd : node.getWidth();
                 yAdd = yAdd > node.getHeight() ? yAdd : node.getHeight();
                 total++;
             }
         }
 
-        double xSize = Math.sqrt(total) * (xAdd+ X_SPACING) * FACTOR;
+        double xSize = Math.sqrt(total) * (xAdd + X_SPACING) * FACTOR;
         double ySize = Math.sqrt(total) * (yAdd + Y_SPACING) * FACTOR;
         i = selection.iterator();
         while (i.hasNext()) {
             LWComponent c = i.next();
             if (c instanceof LWNode) {
                 LWNode node = (LWNode) c;
-                double x = minX + xSize * (Math.random() - 0.5);
-                double y = minY + ySize * (Math.random() - 0.5);
+                double centerX = (minX + maxX) / 2;
+                double centerY = (minY + maxY) / 2;
+                double x = centerX + xSize * (Math.random() - 0.5);
+                double y = centerY + ySize * (Math.random() - 0.5);
                 int col_count = 0;
                 boolean flag = true;
                 while (flag && col_count < MAX_COLLISION_CHECK) {
 
                     LWComponent overlapComponent;
-                    if (VUE.getActiveViewer().pickNode((float) x, (float) y) != null) {
-                         x = minX + xSize * (Math.random() - 0.5);
-                         y = minY + ySize * (Math.random() - 0.5);
- //                       overlapComponent = VUE.getActiveViewer().pickNode((float) x, (float) y);
-//                        x = overlapComponent.getX() + overlapComponent.getWidth() + X_SPACING;
-//                        y = overlapComponent.getY() + overlapComponent.getHeight() + Y_SPACING;
-//                         System.out.println(node.getLabel() + " collided with " + overlapComponent.getLabel() + " set:" + x + "," + y);
-                    } else if (VUE.getActiveViewer().pickNode((float) x + node.getWidth(), (float) y + node.getHeight()) != null) {
-                          x = minX + xSize * (Math.random() - 0.5);
-                         y = minY + ySize * (Math.random() - 0.5);
-//                         overlapComponent = VUE.getActiveViewer().pickNode((float) x + node.getWidth(), (float) y + node.getHeight());
-//                        x = overlapComponent.getX() + overlapComponent.getWidth() + X_SPACING;
-//                        y = overlapComponent.getY() + overlapComponent.getHeight() + Y_SPACING;
- //                       System.out.println(node.getLabel() + " collided with " + overlapComponent.getLabel() + " set:" + x + "," + y);
-
-                    }   else if (VUE.getActiveViewer().pickNode((float) x + node.getWidth(), (float) y ) != null) {
-                          x = minX + xSize * (Math.random() - 0.5);
-                         y = minY + ySize * (Math.random() - 0.5);
-//                       overlapComponent = VUE.getActiveViewer().pickNode((float) x + node.getWidth(), (float) y );
-//                        x = overlapComponent.getX() + overlapComponent.getWidth() + X_SPACING;
-//                        y = overlapComponent.getY() + overlapComponent.getHeight() + Y_SPACING;
- //                       System.out.println(node.getLabel() + " collided with " + overlapComponent.getLabel() + " set:" + x + "," + y);
-
-                    }   else if (VUE.getActiveViewer().pickNode((float) x , (float) y + node.getHeight()) != null) {
-                          x = minX + xSize * (Math.random() - 0.5);
-                         y = minY + ySize * (Math.random() - 0.5);
-//                        overlapComponent = VUE.getActiveViewer().pickNode((float) x, (float) y + node.getHeight());
-//                        x = overlapComponent.getX() + overlapComponent.getWidth() + X_SPACING;
-//                        y = overlapComponent.getY() + overlapComponent.getHeight() + Y_SPACING;
- //                      System.out.println(node.getLabel() + " collided with " + overlapComponent.getLabel() + " set:" + x + "," + y);
-
+                    if ((VUE.getActiveViewer().pickNode((float) x, (float) y) != null) || (VUE.getActiveViewer().pickNode((float) x + node.getWidth(), (float) y + node.getHeight()) != null) || (VUE.getActiveViewer().pickNode((float) x, (float) y + node.getHeight()) != null) || (VUE.getActiveViewer().pickNode((float) x + node.getWidth(), (float) y) != null)) {
+                        x = centerX + xSize * (Math.random() - 0.5);
+                        y = centerY + ySize * (Math.random() - 0.5);
                     } else {
                         flag = false;
                     }
-
-
                     col_count++;
 //                    System.out.println("Node: "+node.getLabel()+" count:"+col_count);
-                   
+
                 }
                 node.setLocation(x, y);
             }
