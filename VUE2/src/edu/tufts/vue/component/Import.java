906656b857b147be50d9697ac7f697104ca2ff62
Anoop Kumar <anoopkum@gmail.com>
2009-02-23 22:36:34 +0000

    a simple compoment to generate a map with random layout from dataset
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@8781 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/edu/tufts/vue/component/Import.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+ * Import.java
+ *
+ * Created on Feb 20, 2009
+ *
+ * Copyright 2003-2009 Tufts University  Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+/**
+ *
+ * @author akumar03
+ */
+package edu.tufts.vue.component;
+
+import tufts.vue.ds.*;
+import edu.tufts.vue.dataset.Dataset;
+import edu.tufts.vue.layout.*;
+import tufts.vue.action.SaveAction;
+import tufts.vue.action.ActionUtil;
+import tufts.vue.*;
+import java.io.File;
+
+public class Import {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    public static void main(String[] args) throws Exception {
+        String inputFile = args[0];
+        String outputFile = args[1];
+        Dataset ds = new Dataset();
+        ds.setFileName(inputFile);
+        ds.loadDataset();
+        Layout layout = new ListRandomLayout();
+        LWMap map = layout.createMap(ds, "ClubZora");
+        map.setFile(new File(outputMap));
+        ActionUtil.marshallMap(new File(outputMap), map);
+
+
+    }
+}
