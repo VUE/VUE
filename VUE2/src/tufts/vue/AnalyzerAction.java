32efb8fcb0b2c2d1291a1acb0d73fb2198a6f1d7
Mike Korcynski <mkorcy@gmail.com>
2009-05-13 02:25:09 +0000

    changes to analyzers
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@9115 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/AnalyzerAction.java
+++ VUE2/src/tufts/vue/AnalyzerAction.java
@@ -27,15 +27,37 @@ import edu.tufts.vue.mbs.LWComponentAnalyzer;
 import edu.tufts.vue.mbs.OpenCalaisAnalyzer;
 import edu.tufts.vue.mbs.YahooAnalyzer;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.*;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.KeyEvent;
 import javax.swing.Action;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.cookie.CookiePolicy;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.params.HttpMethodParams;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.osid.repository.Asset;
 import org.osid.repository.AssetIterator;
 import org.osid.repository.RepositoryException;
 
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
+
 import tufts.Util;
+import tufts.vue.Actions.LWCAction;
 import tufts.vue.gui.GUI;
 import tufts.vue.gui.Widget;
 
@@ -58,6 +80,7 @@ public class AnalyzerAction extends Actions.LWCAction {
                 && s.first() instanceof LWNode); // todo: a have capability check (free-layout?  !isLaidOut() ?)
     }
     private final static int maxLabelLineLength =30;
+    
     public static void addResultsToMap(List<Resource> resourceList,LWComponent centerComponent)
     {
     	 //*** MAP BASED SEARCHING ***
@@ -93,6 +116,8 @@ public class AnalyzerAction extends Actions.LWCAction {
         LayoutAction.search.act(comps);
     	
     }
+    
+ 
     public void act(LWComponent c) 
     {
     	
@@ -101,7 +126,7 @@ public class AnalyzerAction extends Actions.LWCAction {
     		//System.out.println("SEARCH ALREADY RUNNING!!!!!!!!!!!");
     	//	return;
     		
-    	//}
+    	//}    	
     	List<AnalyzerResult> list = analyzer.analyze(c);
     	Iterator<AnalyzerResult> i = list.iterator();
     	VUE.getActiveViewer().getSelection().clear();
@@ -131,12 +156,12 @@ public class AnalyzerAction extends Actions.LWCAction {
     	
     	if (query.equals(""))
     		query = c.getLabel();
-    	if (!hasResults)
-    	{
-    		VueUtil.alert(VueResources.getString("dialog.analyzeerror.message"), VueResources.getString("dialog.analyzeerror.title"));
+    	//if (!hasResults)
+    	//{
+    	//	VueUtil.alert(VueResources.getString("dialog.analyzeerror.message"), VueResources.getString("dialog.analyzeerror.title"));
     	
     		//just use the label from the node, it didn't go right.
-    	}
+    	//}
     	
     	VUE.getDRBrowser().getDataSourceViewer().queryEditor.setCriteria(query);
     	VUE.getDRBrowser().getDataSourceViewer().mapBasedSearch(c);    	
@@ -148,7 +173,92 @@ public class AnalyzerAction extends Actions.LWCAction {
     public static final AnalyzerAction yahoo = new AnalyzerAction(new YahooAnalyzer(),VueResources.getString("analyzeaction.yahoocontent"),null);
     public static final AnalyzerAction calais = new AnalyzerAction(new OpenCalaisAnalyzer(),VueResources.getString("analyzeaction.performmapbased"),null);
     public static final AutoTaggerAction calaisAutoTagger = new AutoTaggerAction(new OpenCalaisAnalyzer(),VueResources.getString("analyzeaction.autotag"),null);
- 
+    public static final SemanticMapAction semanticMapAction = new SemanticMapAction(new OpenCalaisAnalyzer(),"semantic map",null);
+    public static final Action luckyImageAction =
+        new LWCAction(VueResources.getString("luckimage")) {
+            public void act(LWComponent c) {
+            	JSONArray _ja;
+            	try {
+        	
+            		HttpClient client = new HttpClient();
+        			GetMethod method = new GetMethod("http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=093a05d30b0be54c8b18d227fec80b6b&per_page=1&tag_mode=any&sort=relevance&format=json&text=" + URLEncoder.encode(c.getLabel()));
+        			method.getParams().setCookiePolicy(CookiePolicy.BROWSER_COMPATIBILITY); 
+        			client.getParams().setParameter(HttpMethodParams.USER_AGENT,
+        											"Visual Understanding Environment http://vue.tufts.edu");
+        											// Send GET request
+        			int statusCode = client.executeMethod(method);
+        			
+        			if (statusCode != HttpStatus.SC_OK) {
+        				System.err.println("Method failed: " + method.getStatusLine());
+        			}
+        			InputStream rstream = null;
+        			
+        			// Get the response body
+        			rstream = method.getResponseBodyAsStream();
+        			
+        			// Process the response from Yahoo! Web Services
+        			BufferedReader br = new BufferedReader(new InputStreamReader(rstream));
+        			String jsonString = "";
+        			String line;
+        			while ((line = br.readLine()) != null) {
+        				jsonString += line;
+        			}
+        			br.close();
+        			
+        			// Construct a JSONObject from a source JSON text string.
+        			// A JSONObject is an unordered collection of name/value pairs. Its external
+        			// form is a string wrapped in curly braces with colons between the names
+        			// and values, and commas between the values and names.
+        			jsonString = jsonString.substring(14);
+        			jsonString = jsonString.substring(0,jsonString.length()-1);
+        			JSONObject jo = new JSONObject(jsonString);
+
+        			// A JSONArray is an ordered sequence of values. Its external form is a
+        	        // string wrapped in square brackets with commas between the values.
+        			
+        	        // Get the JSONObject value associated with the search result key.
+        	        jo = jo.getJSONObject("photos");
+        			
+        	        //System.out.println(jo.toString());
+        			
+        	        // Get the JSONArray value associated with the Result key
+        	        _ja = jo.getJSONArray("photo");
+        			
+        	        // Get the number of search results in this set
+        	        int resultCount = _ja.length();
+        			//System.out.println("results: " + resultCount);
+        			
+        			java.util.Vector vector = new java.util.Vector();
+        			
+        			for (int i=0; i < resultCount; i++) {
+        				
+        				JSONObject resultObject = _ja.getJSONObject(i);
+        				
+        				String id = null;
+        				String owner = null;
+        				String secret = null;
+        				String server = null;
+        				Integer farm = null;
+        				String title = null;
+        				
+        				id = (String)resultObject.getString("id");
+        				owner = (String)resultObject.get("owner");
+        				secret = (String)resultObject.getString("secret");
+        				server = (String)resultObject.get("server");				
+        				farm = (Integer)resultObject.get("farm");
+        				title = (String)resultObject.get("title");       
+        				String big = "http://farm" + farm + ".static.flickr.com/" + server +"/" + id + "_" + secret +".jpg";
+        				c.setResource(URLResource.create(new URL(big)));
+        			}
+        			
+        		} catch (Throwable ex) {
+        			ex.printStackTrace();
+        		}
+
+             return;   
+            }
+    };
+    
     public static final Action[] ANALYZER_ACTIONS = {
         yahoo,
         calais,
@@ -160,10 +270,121 @@ public class AnalyzerAction extends Actions.LWCAction {
 	//	JMenu calaisMenu = new JMenu("Node Analysis");
     	analyzeNodeMenu.add(calais);
     	analyzeNodeMenu.add(calaisAutoTagger);
+    	//analyzeNodeMenu.add(semanticMapAction);
+    	analyzeNodeMenu.add(luckyImageAction);
 //    	analyzeNodeMenu.add(calaisMenu);
 		
 	}
 	
+	static class SemanticMapAction extends Actions.LWCAction {
+		   // private static final org.apache.log4j.Logger Log = org.apache.log4j.Logger.getLogger(AnalyzerAction.class);
+		    private LWComponentAnalyzer analyzer = null;
+		    
+		    private SemanticMapAction(LWComponentAnalyzer analyzer, String name,KeyStroke keyStroke) {
+		        super(name,keyStroke);
+		        this.analyzer = analyzer;
+		    }
+		  //  boolean mayModifySelection() { return true; }
+		    
+		    
+	        
+		    public void act(LWComponent c) 
+		    {
+		    	//bags of components to add to map
+		    	java.util.List<LWComponent> subCatcomps = new ArrayList<LWComponent>();
+		    	java.util.List<LWComponent> categoryComps = new ArrayList<LWComponent>();
+		    	
+		    	//Analyze the resource and get a multimap of types and values
+		    	Multimap<String,AnalyzerResult> list = analyzer.analyzeResource(c);
+		    	
+		    	
+		    	Color joinNodeColor = new Color(8,119,192);
+		    	Color leafNodeColor = new Color(157,219,83);
+		    			    
+		    	Iterator<String> categoryIterator = list.keySet().iterator();
+		    	boolean hasResults = false;
+		    	HashMap<LWNode,Collection> categoryChildren = new HashMap<LWNode,Collection>();
+		    	while (categoryIterator.hasNext())
+		    	{		
+		    		String key = categoryIterator.next();
+		    		
+		    		//Make a node for the key connect it to the central node.
+		    		//TODO
+
+		    		Collection al =  list.get(key);
+		    		tufts.vue.LWNode node = new tufts.vue.LWNode(key);
+		    		node.setLocation(c.getLocation());
+		    		tufts.vue.LWLink link = new tufts.vue.LWLink(c,node);
+		    		
+		    		if (al.size() > 1)
+		    		{
+		    		//	node.setFillColor(new Color())
+		    			node.setFillColor(joinNodeColor);
+		    			categoryComps.add(node);
+		    			categoryComps.add(link);
+		    			categoryChildren.put(node, al);
+		    		}
+		    		else
+		    		{
+		    			Iterator analyzerResults = al.iterator();
+		    			AnalyzerResult res = (AnalyzerResult)analyzerResults.next();
+		    			tufts.vue.LWNode singleNode = new tufts.vue.LWNode(res.getValue());
+			    		tufts.vue.LWLink singleLink = new tufts.vue.LWLink(c,singleNode);
+			    		singleNode.setFillColor(leafNodeColor);
+			    		singleLink.setLabel(key);
+			    		Font f = node.getFont();
+	    				Font derive = f.deriveFont(((float)(10+res.getCount())));
+	    				node.setFont(derive);
+	    				link.setStrokeColor(getColorFromRelevance(res.getRelevance()));
+			    		
+	    				//Topic is a weird category so just skip it.
+			    		if (!key.equals("Topic"))
+			    		{categoryComps.add(singleNode);
+			    		categoryComps.add(singleLink);}			    		
+		    		}
+		    	}
+		    		LWMap active = VUE.getActiveMap();
+				    active.addChildren(categoryComps);			    
+				    LayoutAction.circle.act(categoryComps);
+				     
+				    Iterator nodeIterator = categoryChildren.keySet().iterator();
+				    while (nodeIterator.hasNext())
+				    {
+				    	subCatcomps.clear();
+				    	LWNode joinNode = (LWNode) nodeIterator.next();
+				    	
+				    	Collection subCategories = categoryChildren.get(joinNode);
+				    	Iterator subCatIt = subCategories.iterator();
+				    	while (subCatIt.hasNext())
+				    	{
+				    		AnalyzerResult res = (AnalyzerResult) subCatIt.next();
+				    		tufts.vue.LWNode node = new tufts.vue.LWNode(res.getValue());
+				    		tufts.vue.LWLink link = new tufts.vue.LWLink(joinNode,node);
+				    		node.setLocation(joinNode.getLocation());
+				    		node.setFillColor(leafNodeColor);
+				    		Font f = node.getFont();
+		    				Font derive = f.deriveFont(((float)(10+res.getCount())));
+		    				node.setFont(derive);
+		    				link.setStrokeColor(getColorFromRelevance(res.getRelevance()));
+				    		subCatcomps.add(node);
+				    		subCatcomps.add(link);				    						    		
+				    	}
+				    
+					    active.addChildren(subCatcomps);			    
+					    LayoutAction.search.act(subCatcomps);
+				    	
+				    
+				    }		  	
+		    }
+		    
+		    public Color getColorFromRelevance(double relevance)
+		    {
+		    	
+		    	double component = 255 - (255 * relevance);
+		    	java.awt.Color c = new java.awt.Color((int)component,(int)component,(int)component);
+		    	return c;
+		    }
+		}
 	static class AutoTaggerAction extends Actions.LWCAction {
 	   // private static final org.apache.log4j.Logger Log = org.apache.log4j.Logger.getLogger(AnalyzerAction.class);
 	    private LWComponentAnalyzer analyzer = null;
