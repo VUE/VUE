e0a51566a8f2d248daa27ec3afd2caa73d47d8a1
Brian Goodmon <Brian.Goodmon@tufts.edu>
2009-06-02 23:00:11 +0000

    Layout changes, etc.
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@9230 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/PrototypePanel.java
+++ VUE2/src/tufts/vue/PrototypePanel.java
@@ -22,39 +22,52 @@ import tufts.vue.gui.DockWindow;
 import tufts.vue.gui.GUI;
 
 public class PrototypePanel extends JPanel implements ActionListener, ChangeListener {
-	public static final long	serialVersionUID = 1;
-    public final static int		HALF_GUTTER = 4;
-    public final static String	ZOOM_IN = "Fit Selection In Window",
-    							ZOOM_OUT = "Fit Map In Window";
-	protected static JCheckBox	zoomLockCheckBox = null;
-	protected static JButton	zoomButton = null;
-    protected static JSlider	opacitySlider = null;
+	public static final long		serialVersionUID = 1;
+	protected static final boolean	DEBUG = false;
+	protected static final int		HALF_GUTTER = 4;
+	protected static final String	ZOOM_IN = "Fit Selection",
+    								ZOOM_OUT = "Fit Map";
+	protected static JCheckBox		zoomLockCheckBox = null;
+	protected static JButton		zoomButton = null;
+    protected static JSlider		fadeSlider = null;
+    protected static JLabel			fadeLabel = null;
 
 	public PrototypePanel(DockWindow dw) {
 		GridBagConstraints	constraints = new GridBagConstraints();
-		JPanel				contents = new JPanel();
-		Insets				insets = new Insets(HALF_GUTTER, HALF_GUTTER, HALF_GUTTER, HALF_GUTTER);
+		JPanel				zoomPanel = new JPanel(),
+							linePanel = null,
+							fadePanel = new JPanel();
+		Insets				halfGutterInsets = new Insets(HALF_GUTTER, HALF_GUTTER, HALF_GUTTER, HALF_GUTTER);
 
-		contents.setLayout(new GridBagLayout());
+		setLayout(new GridBagLayout());
+		zoomPanel.setLayout(new GridBagLayout());
+		fadePanel.setLayout(new GridBagLayout());
 
 		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.weightx = 0.0;
+		constraints.weighty = 0.0;
 		constraints.gridx = 0;
 		constraints.gridy = 0;
 		constraints.gridheight = 1;
 		constraints.gridwidth = 1;
-		constraints.insets = insets;
+		constraints.insets = halfGutterInsets;
 		
 		zoomButton = new JButton(ZOOM_IN);
 //		zoomButton.setAction(tufts.vue.Actions.ZoomToSelection);
 		zoomButton.addActionListener(this);
-		contents.add(zoomButton, constraints);
+		zoomPanel.add(zoomButton, constraints);
 
 		zoomLockCheckBox = new JCheckBox("Lock On");
 		zoomLockCheckBox.addChangeListener(this);
 		constraints.gridx = 1;
-		contents.add(zoomLockCheckBox, constraints);
+		zoomPanel.add(zoomLockCheckBox, constraints);
 
-		opacitySlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 100);
+		fadeLabel = new JLabel("Opacity");
+
+        constraints.gridx = 0;
+		fadePanel.add(fadeLabel, constraints);
+
+		fadeSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 100);
 
 /*      JLabel		label0 = new JLabel("0");
         JLabel		label25 = new JLabel("25");
@@ -81,28 +94,73 @@ public class PrototypePanel extends JPanel implements ActionListener, ChangeList
         labelTable.put(new Integer( 75 ), label75);
         labelTable.put(new Integer( 100 ), label100);
         
-        opacitySlider.setLabelTable(labelTable); */
-        opacitySlider.setLabelTable(opacitySlider.createStandardLabels(25));
-        //opacitySlider.setSnapToTicks(true);
-        opacitySlider.setPaintLabels(true);
-        opacitySlider.setMajorTickSpacing(25);
-        opacitySlider.setPaintTicks(false);
-        opacitySlider.setPreferredSize(new Dimension(130,35));
-        opacitySlider.addChangeListener(this);
+        fadeSlider.setLabelTable(labelTable); */
+		fadeSlider.setLabelTable(fadeSlider.createStandardLabels(25));
+        //fadeSlider.setSnapToTicks(true);
+		fadeSlider.setPaintLabels(true);
+		fadeSlider.setMajorTickSpacing(25);
+		fadeSlider.setPaintTicks(false);
+		fadeSlider.setPreferredSize(new Dimension(130,35));
+		fadeSlider.addChangeListener(this);
 
-        constraints.gridx = 0;
+        constraints.gridx = 1;
+		fadePanel.add(fadeSlider, constraints);
+
+	    constraints.fill = GridBagConstraints.BOTH;
+		constraints.weightx = 1.0;
+		constraints.weighty = 1.0;
+		constraints.insets = new Insets(HALF_GUTTER, HALF_GUTTER, 0, HALF_GUTTER);
+		add(zoomPanel, constraints);
+
+        linePanel = new JPanel() {
+        	public static final long		serialVersionUID = 1;
+            protected void paintComponent(java.awt.Graphics g) {
+                if (isOpaque()) {
+                	g.setColor(getBackground());
+                    g.fillRect(0, 0, getWidth(), getHeight());
+                }
+
+                g.setColor(java.awt.Color.DARK_GRAY);
+                g.drawLine(HALF_GUTTER, getHeight() / 2, getWidth() - HALF_GUTTER - 1, getHeight() / 2);
+            }
+        };
+
 		constraints.gridy = 1;
-		contents.add(opacitySlider, constraints);
+		constraints.insets = new Insets(0, HALF_GUTTER, 0, HALF_GUTTER);
+		add(linePanel, constraints);
 
-	    constraints.fill = GridBagConstraints.NONE;
-		constraints.gridx = 0;
-		setLayout(new GridBagLayout());
-		add(contents, constraints);
+		constraints.gridy = 2;
+		constraints.insets = new Insets(0, HALF_GUTTER, HALF_GUTTER, HALF_GUTTER);
+		add(fadePanel, constraints);
 
 		dw.setContent(this);
 
 		validate();
+
+		// Set zoom button's minimum size to fit the longer of its two labels...
+		zoomButton.setText(ZOOM_IN);
+		zoomButton.setPreferredSize(zoomButton.getSize());
+
+		// and then set zoom button's label to be appropriate to the selection.
 		setButtonTitle();
+
+		if (DEBUG) {
+			zoomLockCheckBox.setBackground(Color.BLUE);
+			zoomLockCheckBox.setOpaque(true);
+			zoomButton.setBackground(Color.BLUE);
+			zoomButton.setOpaque(true);
+		    fadeLabel.setBackground(Color.BLUE);
+		    fadeLabel.setOpaque(true);
+		    fadeSlider.setBackground(Color.BLUE);
+		    fadeSlider.setOpaque(true);
+		    zoomPanel.setBackground(Color.ORANGE);
+		    zoomPanel.setOpaque(true);
+		    linePanel.setBackground(Color.ORANGE);
+		    linePanel.setOpaque(true);
+		    fadePanel.setBackground(Color.ORANGE);
+		    fadePanel.setOpaque(true);
+		}
+
 		setVisible(true);
 	}
 
@@ -140,7 +198,7 @@ public class PrototypePanel extends JPanel implements ActionListener, ChangeList
 	}
 
 	public static double getAlpha() {
-		return (VUE.getSelection().size() == 0 ? 1.0 : ((double)opacitySlider.getValue()) / 100.0);
+		return (VUE.getSelection().size() == 0 ? 1.0 : ((double)fadeSlider.getValue()) / 100.0);
 	}
 
 	/* ActionListener method -- button has been clicked */
@@ -154,7 +212,7 @@ public class PrototypePanel extends JPanel implements ActionListener, ChangeList
 
 		if (source == zoomLockCheckBox) {
 			zoomIfLocked();
-		} else if (source == opacitySlider) {
+		} else if (source == fadeSlider) {
 			VUE.getActiveViewer().repaint();
 		}
 	}
