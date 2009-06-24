a5913d6957cda3917ea27f98c1370c5dfcb6afd9
Brian Goodmon <Brian.Goodmon@tufts.edu>
2009-06-24 15:03:01 +0000

    To fix VUE-1437 -- the Image Size Preference panel's slider becomes obscured when the panel is made narrow.  Also made the description text wrap.
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@9321 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/preferences/generics/GenericSliderPreference.java
+++ VUE2/src/edu/tufts/vue/preferences/generics/GenericSliderPreference.java
@@ -21,16 +21,10 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.util.Hashtable;
 import java.util.prefs.Preferences;
 
 import javax.swing.*;
 import javax.swing.event.ChangeListener;
-
-import edu.tufts.vue.preferences.PreferenceConstants;
-import edu.tufts.vue.preferences.interfaces.VuePreference;
 
 /**
  * @author Mike Korcynski
@@ -38,97 +32,117 @@ import edu.tufts.vue.preferences.interfaces.VuePreference;
  */
 public abstract class GenericSliderPreference extends BasePref implements ChangeListener
 {
-	
+	private static final boolean DEBUG = false;
 	private String message;
 	private JSlider slider = new JSlider();
 	private Object previousValue = null;
-	
+
 	public GenericSliderPreference()
 	{
 		Preferences p = Preferences.userNodeForPackage(getPrefRoot());
 		slider.setBackground(Color.WHITE);
-        
 	}
-			
 
 	public JSlider getSlider()
 	{
 		return slider;
 	}
-	
+
 	public abstract String getTitle();
 	public abstract String getDescription();
-	
+
 	public JComponent getPreferenceUI() {
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.WHITE);
 		GridBagLayout gbl = new GridBagLayout();
 		panel.setLayout(gbl);
+
 		JLabel titleLabel = new JLabel(getTitle());
 		Font f = titleLabel.getFont().deriveFont(Font.BOLD);
 		titleLabel.setFont(f);
-		JLabel descLabel = new JLabel(getDescription());
+
+		JTextArea descTextArea = new JTextArea(getDescription());
+		final Font defaultFont = panel.getFont();
+		descTextArea.setFont(defaultFont);
+		descTextArea.setColumns(30);
+		descTextArea.setLineWrap(true);
+		descTextArea.setWrapStyleWord(true);
+
 		GridBagConstraints gbConstraints = new GridBagConstraints();
-	    
 		gbConstraints.gridx = 0;
-        gbConstraints.gridy = 0;
-        gbConstraints.gridwidth = 1;
-        gbConstraints.fill=GridBagConstraints.HORIZONTAL;
-        gbConstraints.anchor=GridBagConstraints.NORTHWEST;
-        gbConstraints.weightx=1;
-        gbConstraints.weighty=0;
-        gbConstraints.insets = new Insets(15,10,2,2);
-        
-        panel.add(titleLabel, gbConstraints);
-    
-		gbConstraints.gridx = 0;
-		gbConstraints.gridy = 1;
-		panel.add(descLabel, gbConstraints);
-		
-		gbConstraints.gridx=0;
-		gbConstraints.gridy=2;
+		gbConstraints.gridy = 0;
+		gbConstraints.gridwidth = 1;
+		gbConstraints.gridheight = 1;
+		gbConstraints.fill=GridBagConstraints.HORIZONTAL;
+		gbConstraints.anchor=GridBagConstraints.FIRST_LINE_START;
 		gbConstraints.weightx=1;
-        gbConstraints.weighty=1;
-        gbConstraints.insets = new Insets(15,30,15,30);
-        
-        JPanel booleanPanel = new JPanel();
-        booleanPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        booleanPanel.setBackground(Color.WHITE);
-        
-        
-        booleanPanel.add(slider);
-        booleanPanel.add(new JLabel(getMessage()));
-        panel.add(booleanPanel, gbConstraints);
-	return panel;
+		gbConstraints.weighty=0;
+		gbConstraints.insets = new Insets(15,10,2,10);
+		panel.add(titleLabel, gbConstraints);
+
+		gbConstraints.gridy = 1;
+		panel.add(descTextArea, gbConstraints);
+
+		JPanel booleanPanel = new JPanel();
+		String msg = getMessage();
+		JLabel msgLabel = new JLabel(msg);
+
+		booleanPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		booleanPanel.setBackground(Color.WHITE);
+		booleanPanel.add(slider);
+		booleanPanel.add(msgLabel);
+
+		gbConstraints.gridy=2;
+		gbConstraints.fill=GridBagConstraints.BOTH;
+		gbConstraints.weighty=1;
+		gbConstraints.insets = new Insets(15,30,15,30);
+		panel.add(booleanPanel, gbConstraints);
+
+		if (DEBUG) {
+			panel.setBackground(Color.GREEN);
+			titleLabel.setOpaque(true);
+			titleLabel.setBackground(Color.CYAN);
+			descTextArea.setOpaque(true);
+			descTextArea.setBackground(Color.MAGENTA);
+			slider.setOpaque(true);
+			slider.setBackground(Color.YELLOW);
+			msgLabel.setOpaque(true);
+			msgLabel.setBackground(Color.ORANGE);
+			booleanPanel.setOpaque(true);
+			booleanPanel.setBackground(Color.BLUE);
+
+			if (DEBUG && msg == null) {
+				msgLabel.setText("test message");
+			}
+		}
+
+		return panel;
 	}
-	
+
 	public String getMessage(){
 		return message;
 	}
 
 	public Object getPreviousValue()
 	{
-		if (previousValue == null)
-			return getDefaultValue();
-		else
-			return previousValue;
+		return (previousValue == null ? getDefaultValue() : previousValue);
 	}
-	
+
 	public int getSliderValueMappedToPref()
 	{
 		return 0;
 	}
-	
+
 	public Object getValue()
 	{
 		 Preferences p = Preferences.userNodeForPackage(getPrefRoot());
 		 return p.getInt(getPrefName(), ((Integer)getDefaultValue()).intValue());
 	}		
-	
+
 	public void setMessage(String s){
 		this.message = s;
 	}
-	
+
 	public void setValue(Object i)
 	{	
 		previousValue = getValue();
@@ -136,5 +150,4 @@ public abstract class GenericSliderPreference extends BasePref implements Change
 		p.putInt(getPrefName(), getSliderValueMappedToPref());					
 		_fireVuePrefEvent();
 	}
-	
 }
