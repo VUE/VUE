063d2c0c9914926f44c057d149a0e333935a6b22
Scott Fraize <scott.fraize@gmail.com>
2009-06-26 20:02:07 +0000

    debug
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@9342 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/gui/GUI.java
+++ VUE2/src/tufts/vue/gui/GUI.java
@@ -57,7 +57,7 @@ import com.jgoodies.looks.plastic.Plastic3DLookAndFeel;
 /**
  * Various constants for GUI variables and static method helpers.
  *
- * @version $Revision: 1.138 $ / $Date: 2009-06-17 17:01:01 $ / $Author: sfraize $
+ * @version $Revision: 1.139 $ / $Date: 2009-06-26 20:02:07 $ / $Author: sfraize $
  * @author Scott Fraize
  */
 
@@ -3005,12 +3005,15 @@ public class GUI
 
         public void mouseWheelMoved(MouseWheelEvent e) {
 
+            if (DEBUG.FOCUS) Log.debug("MW-HEAD->" + GUI.eventName(e) + "\n\tMouseWheelRelay target 1st: " + GUI.name(head));
             // first, send to the intercept to see if it wants it
             head.mouseWheelMoved(e);
 
             // if unconsumed by the intercept, send on to the original override (usually, a JScrollPane)
-            if (!e.isConsumed())
+            if (!e.isConsumed()) {
+                if (DEBUG.FOCUS) Log.debug("MW-TAIL->" + GUI.eventName(e) + "\n\tMouseWheelRelay target 2nd: " + GUI.name(tail));
                 tail.mouseWheelMoved(e);
+            }
         }
     }
 
