6a6d28305f8af416d0bac91734f067a7b1a4ab00
Anoop Kumar <anoopkum@gmail.com>
2009-07-29 14:40:09 +0000

    added comments
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@9457 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/SEASRComponents/ServiceHead/src/java/edu/tufts/component/servicehead/ServiceHead.java
+++ VUE2/SEASRComponents/ServiceHead/src/java/edu/tufts/component/servicehead/ServiceHead.java
@@ -1,8 +1,24 @@
-
-/**
- *  This is a component to take url requests for seasr components.
- *  This is based on code provided by Xavier
+/*
+ * Copyright 2003-2009 Tufts University  Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ * 
+ * http://www.osedu.org/licenses/ECL-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
  */
+
+/*
+ * ServiceHead.java
+ *
+ *  
+ */
+
 package edu.tufts.component.servicehead;
 
 
@@ -38,7 +54,8 @@ import org.meandre.webui.WebUIException;
 // -------------------------------------------------------------------------
 
 /**
- *  This class implements a component that using the WebUI accepts post requests
+ *  This is a component to take url requests for seasr components.
+ *  This is based on code provided by Xavier
  * 
  * @author Xavier Lor&agrave;
  * @ Modified by Anoop Kumar for VUE
