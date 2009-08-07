817e5849b4932a3689d8718d50875a96ba799c89
Anoop Kumar <anoopkum@gmail.com>
2009-08-07 21:11:52 +0000

    classes to support reading seasr file by castor
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@9507 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/edu/tufts/seasr/Flow.java
@@ -0,0 +1,62 @@
+/*
+ * Flow.java
+ *
+ * Created on 4:56:16 PM
+ *
+ * Copyright 2003-2009 Tufts University  Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package edu.tufts.seasr;
+
+/**
+ * @author akumar03
+ *
+ */
+
+import java.util.List;
+
+public class Flow {
+	List<String> inputList;
+	String label;
+	String url;
+	String uri;
+	
+	public void setInputList(List<String> inputList) {
+		this.inputList = inputList; 
+	}
+	
+	public List<String> getInputList() {
+		return this.inputList;
+	}
+	
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public String getLabel(){
+		return this.label;
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getUrl(){
+		return this.url;
+	}
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+	public String getUri(){
+		return this.uri;
+	}
+
+}
---
+++ VUE2/src/edu/tufts/seasr/FlowGroup.java
@@ -0,0 +1,60 @@
+/*
+ * FlowGroup.java
+ *
+ * Created on 5:05:57 PM
+ *
+ * Copyright 2003-2009 Tufts University  Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package edu.tufts.seasr;
+
+import java.util.List;
+
+/**
+ * @author akumar03
+ *
+ */
+public class FlowGroup {
+	List<Flow> flowList;
+	String label;
+	String input;
+	String output;
+	
+	public void setFlowList(List<Flow> flowList) {
+		this.flowList = flowList; 
+	}
+	
+	public List<Flow> getFlowList() {
+		return this.flowList;
+	}
+	
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public String getLabel(){
+		return this.label;
+	}
+	
+	public void setInput(String input) {
+		this.input =input;
+	}
+	public String getInput(){
+		return this.input;
+	}
+	public void setOutput(String output) {
+		this.output = output;
+	}
+	public String getOutput(){
+		return this.output;
+	}
+}
