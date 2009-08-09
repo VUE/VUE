2d5fd6978dc2a9b3c3629a154333d5a059a0269e
Mike Korcynski <mkorcy@gmail.com>
2009-08-09 15:54:54 +0000

    lots of applet additions.
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@9516 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/VueApplet.java
+++ VUE2/src/tufts/vue/VueApplet.java
@@ -28,9 +28,19 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import java.util.List;
+import java.util.Properties;
 
 import javax.swing.*;
 
+import com.jgoodies.looks.LookUtils;
+import com.jgoodies.looks.plastic.Plastic3DLookAndFeel;
+import com.sun.org.apache.xerces.internal.impl.XMLScanner;
+
+import tufts.vue.ds.DataAction;
+import tufts.vue.ds.DataTree;
+import tufts.vue.ds.Schema;
+import tufts.vue.ds.XmlDataSource;
 import tufts.vue.gui.GUI;
 import tufts.vue.gui.VueMenuBar;
 
@@ -43,21 +53,21 @@ import tufts.vue.gui.VueMenuBar;
 /**
  * Experimental VUE applet.
  * 
- * @version $Revision: 1.13 $ / $Date: 2009-04-06 16:42:25 $ / $Author: mike $
+ * @version $Revision: 1.14 $ / $Date: 2009-08-09 15:54:54 $ / $Author: mike $
  */
-public class VueApplet extends JApplet implements Runnable {
+public class VueApplet extends JApplet {
 
-	private JLabel loadLabel;
 	private static boolean firstInit = true;
 	private static MapViewer viewer = null;
-	private JPanel toolbarPanel = null;
-	private JComponent toolbar = null;
+	private static JPanel toolbarPanel = null;
+	private static JComponent toolbar = null;
+	private static boolean fullyStopped = false;
+	private static boolean isZotero = false;
 	// If want to have viewer left in same state when go forwrd/back in browser,
 	// will need javacript assist to associate the viewer with the instance of a
 	// web browser page:
 	// new applet & context objects are created even when you go forward/back
-	// pages.
-	
+	// pages.	
 	private static MapTabbedPane mMapTabbedPane = null;
 
 	// applet parameters
@@ -65,76 +75,159 @@ public class VueApplet extends JApplet implements Runnable {
 	 * Valid values for zoteroPlugin : true , false default value: false
 	 */
 	private static final String zoteroPlugin = "zoteroPlugin";
-	public static JApplet instance = null;
-	public void init() {
-		instance =this;
+	
+	private static JApplet instance;
+	
+	public VueApplet()
+	{
+		instance = this;
+	//	VUE.setAppletContext(this.getAppletContext());
+	}
+	public void init() 
+	{
+		super.init();
+		this.setBackground(new Color(225,225,225));
+
+		   try {
+    		   UIManager.put("ClassLoader", LookUtils.class.getClassLoader());
+    		      UIManager.setLookAndFeel(new Plastic3DLookAndFeel());
+    		   } catch (Exception e) 
+    		   {
+    			   System.out.println("Couldn't load jlooks look and feel");
+    		   }
+
+		//I think the redone content window makes this code moot.
+		/*
+		GUI.invokeAfterAWT(new Runnable() { public void run() {
+	            
+	            if (VUE.getDRBrowser() != null)
+	            	VUE.getDRBrowser().loadDataSourceViewer();
+	            
+	            // Kick-off tufts.vue.VueDataSource viewer build threads:
+	            // must be done in AWT to be threadsafe, as involves
+	            // non-synhcronized code in tufts.vue.VueDataSource while
+	            // setting up the threads
+
+	            // DataSourceViewer.cacheDataSourceViewers();
+	            
+	    }});		*/
+		
+	}
+	
+	public static String getActiveMapPath()
+	{
+		return VUE.getActiveMap().getFile().getAbsolutePath();
+	}
+	
+	public static String getActiveMapDisplayTitle()
+	{
+		return VUE.getActiveMap().getDisplayLabel();
+	}
+	
+	public static JApplet getInstance()
+	{
+		return instance;
+	}
+	
+	public void start() 
+	{	
+		super.start();
+		instance = this;
 		VUE.setAppletContext(this.getAppletContext());
+		if (!firstInit)
+		while(!fullyStopped) 
+		{
+			try {
+			   Thread.sleep(3000);
+		       System.out.println("WAIT");
+		    } catch (InterruptedException e) {
+		            //keep trying
+		    }
+		}
+		
+		fullyStopped = false;
+		super.init();	
+	
+		
 		processAppletParameters();
 		getContentPane().setLayout(new BorderLayout());
 		
 		msg("init\n\tapplet=" + Integer.toHexString(hashCode())
 				+ "\n\tcontext=" + getAppletContext());
-	//	if (!GUI.isGUIInited())
-		//{
-			VUE.initUI();		
-			VUE.initApplication();
-		//}
-	
-	//	new Thread(this).start();
+		VUE.initUI();		
+		VUE.initApplication();
+		
 		loadViewer();
-		msg("init completed");
-	}
+		
+		if (!VUE.getLeftTabbedPane().isEnabled())
+	       	VUE.getLeftTabbedPane().setEnabled(true);
+		
+		
+		
+	     //-------------------------------------------------------
+        // Trigger the load of the OSID's and set up UrlAuthentication
+        //-------------------------------------------------------      
+        VUE.initDataSources();
+		GUI.invokeAfterAWT(new Runnable() { public void run() {
+			final DRBrowser DR_BROWSER = VUE.getDRBrowser();    
+            if (DR_BROWSER != null)
+                DR_BROWSER.loadDataSourceViewer();
+            
+            // Kick-off tufts.vue.VueDataSource viewer build threads:
+            // must be done in AWT to be threadsafe, as involves
+            // non-synhcronized code in tufts.vue.VueDataSource while
+            // setting up the threads
 
+            // DataSourceViewer.cacheDataSourceViewers();
+          // addZoteroDatasource("collectionName", "/Users/mkorcy01/Desktop/6.xml");
+            VueApplet.addZoteroDatasource("test add", "/Users/mkorcy01/Library/Application Support/Firefox/Profiles/dev/vue-storage/2.xml");
+        }});
+	}
+	
+	public void ToggleAllVisible() {
+		tufts.vue.gui.DockWindow.ToggleAllVisible();
+	}
+	
+	public boolean AllWindowsHidden()
+	{
+		return tufts.vue.gui.DockWindow.AllWindowsHidden();
+	}
+	public void ShowPreviouslyHiddenWindows()
+	{
+		tufts.vue.gui.DockWindow.ShowPreviouslyHiddenWindows();
+	}
+	public void HideAllDockWindows()
+	{
+		tufts.vue.gui.DockWindow.HideAllWindows();
+	}
+	
+	public void stop() 
+	{
+		tufts.vue.gui.DockWindow.HideAllWindows();
+		VUE.finalizeDocks();
+		System.gc();
+		fullyStopped = true;
+		msg("stop");
+	}
+	
+	public void destroy() {
+		super.destroy();
+		msg("destroy");
+	}		
+
+	public static boolean isZoteroApplet()
+	{
+		return isZotero;
+	}
 	private final void processAppletParameters() {
 		String zoteroPlugin = this.getParameter(this.zoteroPlugin);
 		if (zoteroPlugin != null)
 			zoteroPlugin = zoteroPlugin.toLowerCase();
 		if (zoteroPlugin != null && zoteroPlugin.equals("true")) {
-			tufts.vue.action.TextOpenAction.setZoteroPrototypeEnabled(true);
+			isZotero=true;
+		//	tufts.vue.action.TextOpenAction.setZoteroPrototypeEnabled(true);
 		}
-
-	}
-
-	// Load the MapViewer, triggering massive class loading...
-	public void run() {
-		msg("load thread started...");
-		try {
-			loadViewer();
-		} catch (Throwable t) {
-			loadLabel.setText(t.toString());
-		}
-		msg("load thread complete");
-	}
-
-	public void destroy() {
-		// super.destroy();
-		msg("destroy");
-	}
-
-	public void start() {
-	
-
-	//	loadViewer();
-		if (!VUE.getLeftTabbedPane().isEnabled())
-	       	VUE.getLeftTabbedPane().setEnabled(true);
-		msg("start");
-
-	}
-
-	public void stop() {
-		// super.stop();
-		tufts.vue.gui.DockWindow.HideAllWindows();
-		// this.removeAll();
-		VUE.finalizeDocks();
-
-		// map = null;
-		// mMapTabbedPane = null;
-		// viewer= null;
-		// getContentPane().removeAll();
-		System.gc();
-		// System.runFinalization();
-		msg("stop");
-	}
+	}		
 
 	public static String getActiveMapItems()
     {
@@ -182,57 +275,36 @@ public class VueApplet extends JApplet implements Runnable {
     }
 
 	public synchronized void loadViewer() {
-		// viewer = getMapViewer();
 	
-		
-
 		msg("got viewer");
 		msg("is applet ? " + VUE.isApplet());
 
 		msg("setting menu bar...");
 		VueMenuBar vmb = new VueMenuBar();
+		vmb.setBorderPainted(false);
 		setJMenuBar(vmb);
 
 		final VueToolbarController tbc = VueToolbarController.getController();
+		toolbar = tbc.getToolbar().getMainToolbar();
 
-		if (toolbar == null)
-			toolbar = tbc.getToolbar().getMainToolbar();
-
-		if (toolbarPanel == null)
-			toolbarPanel = VUE.constructToolPanel(toolbar);
+		toolbarPanel = VUE.constructToolPanel(toolbar);
 
 		getContentPane().setBackground(toolbarPanel.getBackground());
 		getContentPane().add(toolbarPanel, BorderLayout.NORTH);
-		//getContentPane().add(mMapTabbedPane, BorderLayout.CENTER);
-		if (!firstInit)
-		{
-			VUE.createDockWindows();
-			vmb.rebuildWindowsMenu();
-		}
 
 		getContentPane().add(VUE.getLeftTabbedPane(),BorderLayout.CENTER);	
 		validate();
 		
-	//	if (firstInit)
 		VUE.displayMap(new LWMap("New Map"));
 	
 		msg("validating...");
-
-	
-
 		msg("loading complete");
-		// initialize enabled state of actions via a selection set:
-		if (!firstInit)
-			EditorManager.refresh();
         
-		VUE.getSelection().clearAndNotify();
+	//	VUE.getSelection().clearAndNotify();
 
-		VUE.setActive(MapViewer.class, this, null); // no open viewers
-        firstInit=false;
-        System.out.println("A " +VUE.getLeftTabbedPane().isEnabled());
-        System.out.println("B " +VUE.getLeftTabbedPane().isValid());
-        
-  
+		//VUE.setActive(MapViewer.class, this, null); // no open viewers
+        firstInit=false;      
+       
 	}
 
 	public void setSize(int width, int height)
@@ -243,14 +315,17 @@ public class VueApplet extends JApplet implements Runnable {
 		while (c !=null)
 		{
 			c.setSize(width, height);
+			c.validate();
 			c = c.getParent();
 		}
-		//System.out.println("Applet Set Size : " + width + " , " + height);
+		this.invalidate();
+		this.getContentPane().validate();
+		this.validate();
+		
+		this.repaint();
 	}
 	private void msg(String s) {
 		System.out.println("VueApplet: " + s);
-
-		// showStatus("VueApplet: " + s);
 	}
 
 	@SuppressWarnings("unchecked")
@@ -283,6 +358,95 @@ public class VueApplet extends JApplet implements Runnable {
 			}
 
 		});
+	}
+
+		
+	@SuppressWarnings("unchecked")
+	public static void addZoteroDatasource(final String collectionName, final String fileString) {
+		AccessController.doPrivileged(new PrivilegedAction() {
+
+			public Object run() {
+				try 
+				{
+					DataSourceList l = DataSetViewer.getDataSetList();
+					int length = l.getModel().getSize();
+					for (int i = 0; i<length; i++)
+					{
+						Object o = l.getModel().getElementAt(i);
+						if (o instanceof XmlDataSource) 
+						{
+							XmlDataSource xmlds = (XmlDataSource)o;
+							if (xmlds.getDisplayName().equals(collectionName) && xmlds.getAddress().equals(fileString))
+							{
+								//we already have it in the list refresh it and move on...
+								VUE.getContentPanel().getDSBrowser().getDataSetViewer().setActiveDataSource(xmlds);
+								VUE.getContentPanel().getDSBrowser().getDataSetViewer().refreshBrowser();
+								VUE.getContentDock().setVisible(true);
+								VUE.getContentPanel().showDatasetsTab();
+
+								return null;
+							}
+						}
+					}
+					String xml;
+					tufts.vue.DataSource ds = new XmlDataSource(collectionName, fileString);	
+					Properties props = new Properties();
+					props.put("displayName", collectionName);
+					props.put("name", collectionName);
+					props.put("address", fileString);
+					props.put("item_path", "zoteroCollection.zoteroItem");
+					props.put("key_field", "zoteroCollection.zoteroItem.id");
+					ds.setConfiguration(props);
+					BrowseDataSource		bds = (BrowseDataSource)ds;
+					DataSetViewer.getDataSetList().addOrdered(ds);
+
+					//System.out.println("MIKE");
+					DataSourceViewer.saveDataSourceViewer();
+					Schema schema = Schema.fetch(Resource.instance(fileString),ds.getGUID());
+					schema.dumpSchema(System.out);
+					LWMap map = VUE.getActiveMap();
+				
+					List<LWComponent> nodes =  DataAction.makeRowNodes(schema);
+			        System.out.println("Adding nodes to map");
+			        for(LWComponent component: nodes) {
+			            component.setFillColor(Color.green.darker());
+			            map.add(component);
+			            System.out.println("Adding: "+component.getLabel());
+			        }
+			        LayoutAction.random.act(new LWSelection(nodes));
+			    	VUE.getContentPanel().getDSBrowser().getDataSetViewer().setActiveDataSource(ds);
+					/*
+					boolean added = false;
+					int tries = 0;
+					
+					while (!added && tries < 10)
+					{
+						try
+						{
+					       // DataTree.this.addNewRowsToMap();
+							added = true;
+						
+						}
+						catch(NullPointerException npe)
+						{
+							added = false;
+							tries++;
+							Thread.sleep(1000);
+						}
+					}
+					
+					*/
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				VUE.getContentDock().setVisible(true);
+				VUE.getContentPanel().showDatasetsTab();
+//				VUE.getContentPanel().getDSBrowser().getDataSetViewer().add
+				
+				return null;
+			}
+
+		});
 
 	}
 
@@ -290,6 +454,10 @@ public class VueApplet extends JApplet implements Runnable {
 		innerDisplayMap(urlString);
 	}
 
+	public static void displayLocalMap(String fileString)
+	{
+		innerDisplayLocalMap(fileString);
+	}
 	@SuppressWarnings("unchecked")
 	private static void innerDisplayMap(final String urlString) {
 
@@ -327,6 +495,21 @@ public class VueApplet extends JApplet implements Runnable {
 		});
 
 	}
+	
+	@SuppressWarnings("unchecked")
+	private static void innerDisplayLocalMap(final String fileString) {
+
+		AccessController.doPrivileged(new PrivilegedAction() {
+
+			public Object run() {
+				
+					tufts.vue.action.OpenAction.displayMap(new File(fileString));
+					return null;
+			}
+
+		});
+
+	}
 
 	public static InputStream getInputStream(String fileName)
 			throws IOException {
@@ -344,16 +527,10 @@ public class VueApplet extends JApplet implements Runnable {
 	}
 
 	protected static MapViewer getMapViewer() {
-
-		// VUE.installExampleMap(map);
-
 		return viewer;
 	}
 
 	protected static MapTabbedPane getMapTabbedPane() {
-
-		// VUE.installExampleMap(map);
-
 		return mMapTabbedPane;
 	}
 
