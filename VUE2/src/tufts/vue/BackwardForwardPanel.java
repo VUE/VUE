ae02c243a05158c6245109c692957f7113251020
Brian Goodmon <Brian.Goodmon@tufts.edu>
2009-08-27 17:30:11 +0000

    VUE-1439: add back/forward buttons to toolbar.
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@9624 d66e5487-c4fa-4dda-8c8e-ca23240363c0

---
+++ VUE2/src/tufts/vue/BackwardForwardPanel.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2003-2009 Tufts University  Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ * 
+ * http://www.osedu.org/licenses/ECL-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package tufts.vue;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.Action;
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.JPanel;
+
+import tufts.vue.Actions;
+import tufts.vue.gui.VueButton;
+import tufts.vue.gui.VueButtonIcon;
+
+public class BackwardForwardPanel extends JPanel
+{
+	public static final long	serialVersionUID = 1;
+
+
+	public BackwardForwardPanel() {
+		super(new GridLayout(1, 2, 1, 0));
+
+		setBorder(BorderFactory.createEmptyBorder(0, 8, 0, 0)); 
+
+		VueButton	backwardButton = createButton(Actions.ViewBackward, VueResources.getIcon("Back.raw")),
+					forwardButton = createButton(Actions.ViewForward, VueResources.getIcon("Forward.raw"));
+
+		add(backwardButton);
+		add(forwardButton);
+
+		if (DEBUG.BOXES) {
+			this.setBackground(Color.MAGENTA);
+		}
+	}
+
+
+	protected VueButton createButton(Action action, Icon icon) {
+		VueButton	newButton = new VueButton(action);
+		Dimension	buttonSize = new Dimension(icon.getIconWidth() + 8, icon.getIconHeight() + 8);
+
+		newButton.setPreferredSize(buttonSize);
+		newButton.setAsToolbarButton(true);
+
+		VueButtonIcon.installGenerated(newButton, icon, null);
+
+		return newButton;
+	}
+}
