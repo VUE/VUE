6903af9f1c7fe5e980ebdc4c113112e372986882
Anoop Kumar <anoopkum@gmail.com>
2009-10-13 23:57:39 +0000

    analyze for url
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@9969 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/mbs/SeasrAnalyzer.java
+++ VUE2/src/edu/tufts/vue/mbs/SeasrAnalyzer.java
@@ -39,9 +39,9 @@ public class SeasrAnalyzer implements LWComponentAnalyzer {
 				url = new URL(flow.getUrl()+"?"+flow.getInputList().get(0)+"="+ getSpecFromComponent(c));
 			}
 			XMLDecoder decoder = new XMLDecoder(url.openStream());
-			Map<String,Integer> map = (Map<String,Integer>) decoder.readObject();
-			for(String key: map.keySet()) {
-			results.add(new AnalyzerResult(key, Integer.toString(map.get(key))));
+			Map map =  (Map)decoder.readObject();
+			for(Object key: map.keySet()) {
+			results.add(new AnalyzerResult(key.toString(), map.get(key).toString()));
 			}
  		}catch(Exception ex) {
 			ex.printStackTrace();
@@ -60,15 +60,14 @@ public class SeasrAnalyzer implements LWComponentAnalyzer {
 				url = new URL(DEFAULT_FLOW_URL + "?" + DEFAULT_INPUT + "=" + urlString);
 			} else {
 				url = new URL(flow.getUrl() + "?" + flow.getInputList().get(0) + "=" + urlString);
+				
 			}
-
+			System.out.println("Executing: "+url.toString());
 			XMLDecoder decoder = new XMLDecoder(url.openStream());
-			Map<String, Integer> map = (Map<String, Integer>) decoder.readObject();
-
-			for (String key: map.keySet()) {
-				results.add(new AnalyzerResult("NA", key));
-			}
- 		} catch(Exception ex) {
+			Map map =  (Map)decoder.readObject();
+			for(Object key: map.keySet()) {
+			results.add(new AnalyzerResult(key.toString(), map.get(key).toString()));
+			} 		} catch(Exception ex) {
 			ex.printStackTrace();
 			VueUtil.alert("Can't Execute Flow on the url " + urlString, "Can't Execute Seasr flow");
 		}
