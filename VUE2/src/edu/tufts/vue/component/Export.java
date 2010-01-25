b0e60c5c2fe342b6d1fcb2a47ea84ba4bce58f42
Anoop Kumar <anoopkum@gmail.com>
2010-01-25 22:00:28 +0000

    support for jpeg and svg working now
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@10378 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/component/Export.java
+++ VUE2/src/edu/tufts/vue/component/Export.java
@@ -18,10 +18,111 @@
  */
 package edu.tufts.vue.component;
 
+import tufts.vue.action.OpenAction;
+import tufts.vue.action.ImageConversion;
+
+import java.io.File;
+
+import tufts.vue.*;
 /**
  * @author akumar03
  *
  */
-public class Export {
 
+
+
+public class Export {
+	/** A method that creates a pdf from map file. T 
+     *
+     * @param mapFile is pointer to vue map file 
+     * @param outputFile pdf output file
+     * @throws java.lang.Exception
+     */
+	public void createPdf(String  mapFile, String outputFile) throws Exception { 
+		
+	}
+	
+	/** A method that creates a JPEG from map file. T 
+    *
+    * @param mapFile is pointer to vue map file 
+    * @param outputFile JPEG output file
+    * @throws java.lang.Exception
+    */
+	public void createJPEG(String  mapFile, String outputFile) throws Exception { 
+		LWMap map = OpenAction.loadMap(mapFile);
+		ImageConversion.convert(map.getAsImage(), new File(outputFile), ImageConversion.JPEG);
+		
+	}
+	/** A method that creates a HTML from map file. T 
+    *
+    * @param mapFile is pointer to vue map file 
+    * @param outputFile HTML output file
+    * @throws java.lang.Exception
+    */
+	public void createHTML(String  mapFile, String outputFile) throws Exception { 
+		
+	}
+	/** A method that creates a PNG from map file. T 
+    *
+    * @param mapFile is pointer to vue map file 
+    * @param outputFile PNG output file
+    * @throws java.lang.Exception
+    */
+	public void createPNG(String  mapFile, String outputFile) throws Exception { 
+		LWMap map = OpenAction.loadMap(mapFile);
+		ImageConversion.convert(map.getAsImage(), new File(outputFile), ImageConversion.PNG);
+
+	}
+	
+	/** A method that creates a SVG from map file. T 
+    *
+    * @param mapFile is pointer to vue map file 
+    * @param outputFile SVG output file
+    * @throws java.lang.Exception
+    */
+	public void createSVG(String  mapFile, String outputFile) throws Exception { 
+		
+	}
+	/** A method that creates a RDF from map file. T 
+    *
+    * @param mapFile is pointer to vue map file 
+    * @param outputFile RDF output file
+    * @throws java.lang.Exception
+    */
+	public void createRDF(String  mapFile, String outputFile) throws Exception { 
+		
+	}
+	/** A method that creates a VPK from map file. T 
+    *
+    * @param mapFile is pointer to vue map file 
+    * @param outputFile VPK output file
+    * @throws java.lang.Exception
+    */
+	public void createVPK(String  mapFile, String outputFile) throws Exception { 
+		
+	}
+	
+	public void export(String inputFile,String outputFile,int option) throws Exception {
+		switch (option)  {
+			case 0:
+				createJPEG(inputFile, outputFile);
+				break;
+			case 1:
+				createPNG(inputFile,outputFile);
+				break;
+		}
+	}
+	public static void main(String[] args) throws Exception {
+        String inputFile = args[0];
+        String outputFile = args[1];
+        Export exporter = new Export();
+        int option = 0;
+        if(args.length == 3 && args[2] != null) {
+        	 option = Integer.parseInt(args[2]);
+         
+        }  
+        	exporter.export(inputFile, outputFile,option);
+       
+       }
+	
 }
