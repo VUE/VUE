f8df497546daa888082595c4fc8d1e2b143029bb
Brian Goodmon <Brian.Goodmon@tufts.edu>
2010-02-03 20:24:52 +0000

    Updated copyright date on About VUE window.  Shouldn't this be in VueResources.properties???
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@10445 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/action/AboutAction.java
+++ VUE2/src/tufts/vue/action/AboutAction.java
@@ -78,7 +78,7 @@ public class AboutAction extends tufts.vue.VueAction
         
         JLabel jtf = new JLabel("<html><font color = \"#20316A\"> <br><br>"
                                 + "&nbsp;&nbsp;&nbsp;Developed by Tufts Academic Technology<br>"
-                                + "&nbsp;&nbsp;&nbsp;Copyright &copy; 2003-2008 Tufts University<br>"
+                                + "&nbsp;&nbsp;&nbsp;Copyright &copy; 2003-2010 Tufts University<br>"
   //                              + "&nbsp;&nbsp;&nbsp;Copyright &copy; 2004,2005 MIT University<br>"
                                 + "&nbsp;&nbsp;&nbsp;All Rights Reserved<br><br>"
                                 + "&nbsp;&nbsp;&nbsp;Version "+VueResources.getString("vue.version")+" <br>"
