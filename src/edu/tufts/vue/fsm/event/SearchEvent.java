d0dd93835f5638f27ebb9b2f3a2950d08da3a9ab
Helen Oliver <k.oliver@imperial.ac.uk>
2010-09-23 10:23:24 +0000

    Screwed up the folder structure again, deleting for one more try
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/branches/helenoliver@10578 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- src/edu/tufts/vue/fsm/event/SearchEvent.java
+++
@@ -1,9 +0,0 @@
-package edu.tufts.vue.fsm.event;
-
-public class SearchEvent extends java.util.EventObject
-{
-	public SearchEvent(Object source)
-	{
-		super(source);
-	}
-}
--- src/oki/old/cabutil/CabSyncLocal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ ! -d local ]; then
-    mkdir local
-fi
-if [ ! -d remote ]; then
-    mkdir remote
-fi
-set -x
-java -cp filingAll.jar CabSyncViewer -poll 5 local remote
