48d879c250f3374fba8596feb1223efc24837fbd
Helen Oliver <k.oliver@imperial.ac.uk>
2011-03-12 22:43:43 +0000

    restored an unwanted amendment
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/branches/helenoliver@10751 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/tufts/vue/gui/GUI.java
+++ VUE2/src/tufts/vue/gui/GUI.java
@@ -365,9 +365,8 @@ public class GUI
         ContentFace = new GUI.Face("Arial", Font.PLAIN, 14, null); // shows underscores below underline
         //ContentFace = new GUI.Face("Verdana", Font.PLAIN, 13, null); // underline hides underscores
 
-        // HO 12/03/2011 BEGIN ****************
-        //FocusManager.install();
-        // HO 12/03/2011 END ****************
+        FocusManager.install();
+        
         //tufts.Util.executeIfFound("tufts.vue.gui.WindowManager", "install", null);
 
         org.apache.log4j.Level level = org.apache.log4j.Level.DEBUG;
