c3e6867e9496c7c8a93fe5f8766da7b90d19fee1
Scott Fraize <scott.fraize@gmail.com>
2012-07-09 01:52:05 +0000

    cleanup / debug "exported" SPARQL query variable names changed to "rid" and "val"
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@11198 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/rdf/Query.java
+++ VUE2/src/edu/tufts/vue/rdf/Query.java
@@ -28,59 +28,82 @@ package edu.tufts.vue.rdf;
 
 import java.util.*;
 import tufts.Util;
+import tufts.vue.DEBUG;
 
+/**
+ * Query utility class for VUE to provide a simple API for building up a SPARQL query.
+ * Could probably skip this and just an existing impl of com.hp.hpl.jena.query.Query
+ *
+ * Note that this class is poorly encapsulated: the literal values in the query it constructs:
+ * e.g., "resource" / "keyword", or "rid" / "val", or whatever they are, have to be hardcoded
+ * elsewhere in processing the results of QuerySolution(s).
+ *
+ * This class might better be called something like QueryBuilder or SPARQLBuilder
+ * -- can cause confusion with com.hp.hpl.jena.query.Query.
+ */
 public class Query  {
 
     private static final org.apache.log4j.Logger Log = org.apache.log4j.Logger.getLogger(Query.class);
 
-    private static boolean DEBUG = true;
+    public enum Qualifier { STARTS_WITH, CONTAINS,MATCH, WITHOUT,MATCH_CASE };
+
+    // These appear to have never been used.
+    //public enum Operation { AND , OR }
+    //private boolean regex = true;
+    //private Operation oper  = Operation.AND;
+
+    private final List<Criteria> criteriaList = new ArrayList<Criteria>();
     
-    public enum Operation { AND , OR }
-    public enum Qualifier {STARTS_WITH, CONTAINS,MATCH, WITHOUT,MATCH_CASE};
-    private boolean regex = true;
-    private Operation oper  = Operation.AND;
-    //private Map<String,String> criteria = new HashMap<String,String>();
-    private  List<Criteria> criteriaList = new ArrayList<Criteria>();
     /** Creates a new instance of Query */
-    public Query() {
-    }
+    public Query() {}
     
     public void addCriteria(String key,String value) {
-        if (DEBUG) Log.debug("addCriteria " + key + "=" + Util.tags(value));
+        if (DEBUG.SEARCH) Log.debug("addCriteria " + key + "=" + Util.tags(value));
         Criteria criteria = new Criteria(key,value);
         criteriaList.add(criteria);
     }
     
     public void addCriteria(String key, String value, String condition){
-        if (DEBUG) Log.debug("addCriteria " + key + "=" + Util.tags(value) + " cond=" + condition);
+        if (DEBUG.SEARCH) Log.debug("addCriteria " + key + "=" + Util.tags(value) + " cond=" + Util.tags(condition));
         Criteria criteria = new Criteria(key,value,Qualifier.valueOf(condition));
         criteriaList.add(criteria);
     }
+
+    //private static final String QueryExprFmt = "?resource <%s> ?keyword%d FILTER regex(?keyword%d, \"%s%s\", \"i\") .";
+    //private static final String QueryExprFmt = "?rid <%s> ?val%d FILTER regex(?val%d, \"%s%s\", \"i\") .";
+    
+    private static final String QueryStart = "PREFIX vue: <"+RDFIndex.VUE_ONTOLOGY+">" + "\nSELECT ?rid ?val WHERE { ";
+    private static final String QueryForProperty = "?rid <%s> ?val FILTER regex(?val, \"%s%s\", \"i\") .";
+
+    // "i" means case-independent
+    // Todo: would be nice to be able to handle stuff like: FILTER (?age >= 24)
     
     public String createSPARQLQuery() {
-        String query = new String();
-        query =  "PREFIX vue: <"+RDFIndex.VUE_ONTOLOGY+"> "+
-                "SELECT ?resource " +
-                "WHERE{";
-        int i =0;
-        String keyword = "keyword";
-        for(Criteria criteria: criteriaList) {
-             i++;
-            keyword ="keyword"+i;
+        final StringBuilder query = new StringBuilder(QueryStart.length() + QueryForProperty.length() + 16);
+        final boolean multiLine = true; // criteriaList.size() > 1; // for diagnostic readability
+        query.append(QueryStart);
+        if (multiLine) query.append('\n');
+        int i = 0;
+        for (Criteria criteria : criteriaList) {
+            i++;
+            final String encodedPropertyKey = RDFIndex.getEncodedKey(criteria.key); // better done at critera construct time?
+            final String regexPrefix;
+
+            // if (encodedKey.startsWith(RDFIndex.VUE_ONTOLOGY))
+            //     encodedKey = "vue:" + encodedKey.substring(RDFIndex.VUE_ONTOLOGY.length()); // remove angle brackets as well?
             
-            String encodedKey = RDFIndex.getEncodedKey(criteria.key);
-            
-            switch(criteria.qualifier)  {
-                case CONTAINS:
-                    query +=  "?resource <"+ encodedKey +"> ?"+keyword+" FILTER regex(?"+keyword+",\""+criteria.value+ "\",\"i\") . ";
-                    break;
-                case STARTS_WITH:
-                    query +=  "?resource <"+ encodedKey +"> ?"+keyword+" FILTER regex(?"+keyword+",\"^"+criteria.value+ "\",\"i\") . ";
-                    break;                 
+            switch (criteria.qualifier) {
+                case STARTS_WITH: regexPrefix = "^"; break;
+                default:          regexPrefix = "";
             }
+            if (multiLine) query.append("  ");
+            query.append(String.format(QueryForProperty, encodedPropertyKey, regexPrefix, criteria.value));
+          //query.append(String.format(QueryExprFmt,encodedPropertyKey, i,i, regexPrefix, criteria.value));
+          // Sequentially naming the query vars is just another way to distinguish what was found in the results.
+            if (multiLine) query.append('\n');
         }
-        query  += "}";
-        return query;
+        query.append('}');
+        return query.toString();
     }
     
     class Criteria {
