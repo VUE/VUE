746b466decc546ef2c01f8621d63f96bc3b3a5fa
Scott Fraize <scott.fraize@gmail.com>
2012-07-17 02:53:10 +0000

    fix for only ever using the default 0.5 threshold and ignoring the UI!
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@11263 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/compare/VoteAggregate.java
+++ VUE2/src/edu/tufts/vue/compare/VoteAggregate.java
@@ -28,35 +28,37 @@ public class VoteAggregate extends WeightAggregate
 
     public static final int POSITIVE_VOTE = 1;
     public static final int NEGATIVE_VOTE = 0;
-    public static final double DEFAULT_THRESHOLD = .5;
     
-    private double nodeThreshold = DEFAULT_THRESHOLD;
-    private double linkThreshold = DEFAULT_THRESHOLD;
+    private final double nodeThreshold;
+    private final double linkThreshold;
     
     protected final int[][] linkCounts;
     
-    public static VoteAggregate create(List<ConnectivityMatrix> matrices) {
+    public static VoteAggregate create(List<ConnectivityMatrix> matrices, double nodeThresh, double linkThresh) {
         final IndexedCountingSet preComputed = new IndexedCountingSet();
         for (ConnectivityMatrix matrix : matrices)
             preComputed.addAll(matrix.keys);
         // We pre-compute the index so the matrix aggregate will know how big
         // of a matrix will be required to hold it all.
-        return new VoteAggregate(preComputed, matrices);
+        return new VoteAggregate(preComputed, matrices, nodeThresh, linkThresh);
     }
     
-    private VoteAggregate(IndexedCountingSet preComputed, List<ConnectivityMatrix> matrices) {
+    private VoteAggregate(IndexedCountingSet preComputed, List<ConnectivityMatrix> matrices, double nodeThresh, double linkThresh) {
         super(preComputed, matrices);
         // WeightAggregate superclass will have merged in all the matrices to produce counts: we save them here
         this.linkCounts = super.cx.clone();
+        this.nodeThreshold = nodeThresh;
+        this.linkThreshold = linkThresh;
+        if (DEBUG.Enabled) Log.debug(this + " created with node/link thresholds: " + nodeThresh + "/" + linkThresh);
         computeVotes();
     }
     
-    private void computeVotes() {
-        
+    private void computeVotes()
+    {
         final int count = getCount();
-        final double threshold = (double)count*linkThreshold;
+        final double threshold = (double) count*linkThreshold;
         final int size = super.size();
-        if (DEBUG.Enabled) Log.debug("computeVotes, threshold=" + threshold + " out of " + count);
+        if (DEBUG.Enabled) Log.debug(this + " computeVotes: linkThresh " + linkThreshold + "*count(" + count + ") = vote threshold: " + threshold);
         
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
@@ -81,13 +83,13 @@ public class VoteAggregate extends WeightAggregate
 
         return nodeCount >= threshold;
     }
-   /*
+
+    /*
     * returns the nunber of links between two nodes
     * @param label1 label/merge property of node 1
     * @param label2 label/merge property of node 2
     * @return number of links between node 1 and node 2
     */
-            
     public int getLinkCount(Object label1, Object label2) {
         int index1 = keys.findIndex(label1);
         int index2 = keys.findIndex(label2);
@@ -103,7 +105,6 @@ public class VoteAggregate extends WeightAggregate
      * @param label2 label/merge property of node 2
      * @return true when number of links is above threshold
      */
-    
     public boolean isLinkVoteAboveThreshold(Object key1, Object key2) {
         final int linkCount = getLinkCount(key1, key2);
         final int count = getCount();
@@ -112,15 +113,13 @@ public class VoteAggregate extends WeightAggregate
         return (linkCount >= threshold) && (linkCount > 0);
     }
     
-    public void setLinkThreshold(double percentage) {
-        linkThreshold = percentage;
-        if (DEBUG.Enabled) Log.debug("linkThresh set to " + percentage);
-    }
-    
-    public void setNodeThreshold(double percentage) {
-        nodeThreshold = percentage;
-        if (DEBUG.Enabled) Log.debug("nodeThresh set to " + percentage);
-    }
-
+    // public void setLinkThreshold(double percentage) {
+    //     linkThreshold = percentage;
+    //     if (DEBUG.Enabled) Log.debug("linkThresh set to " + percentage, new Throwable("HERE"));
+    // }
+    // public void setNodeThreshold(double percentage) {
+    //     nodeThreshold = percentage;
+    //     if (DEBUG.Enabled) Log.debug("nodeThresh set to " + percentage, new Throwable("HERE"));
+    // }
     // public VoteAggregate(List<ConnectivityMatrix> matrices,boolean nodeMerge) { super(matrices); }    
 }
