073efee0d3afba1513e272b504b1a5311272f57d
Scott Fraize <scott.fraize@gmail.com>
2012-07-17 00:43:09 +0000

    update for superclass, much faster+simpler, takes any object as key
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@11253 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/compare/VoteAggregate.java
+++ VUE2/src/edu/tufts/vue/compare/VoteAggregate.java
@@ -19,47 +19,51 @@
  */
 package edu.tufts.vue.compare;
 
+import tufts.vue.DEBUG;
 import java.util.*;
 
-public class VoteAggregate extends WeightAggregate {
+public class VoteAggregate extends WeightAggregate
+{
+    private static final org.apache.log4j.Logger Log = org.apache.log4j.Logger.getLogger(VoteAggregate.class);
+
     public static final int POSITIVE_VOTE = 1;
     public static final int NEGATIVE_VOTE = 0;
-    public static final double DEFAULT_THRESHOLD =  .5;
+    public static final double DEFAULT_THRESHOLD = .5;
+    
     private double nodeThreshold = DEFAULT_THRESHOLD;
     private double linkThreshold = DEFAULT_THRESHOLD;
     
-    protected int[][] linkCounts = new int[SIZE][SIZE];
+    protected final int[][] linkCounts;
     
-    /** Creates a new instance of VotingAggregate */
-    public VoteAggregate(List<ConnectivityMatrix> matrices ){
-        super(matrices);
-        for(int i=0;i<SIZE;i++)
-            for(int j=0;j<SIZE;j++)
-                linkCounts[i][j] = c[i][j];
+    public static VoteAggregate create(List<ConnectivityMatrix> matrices) {
+        final IndexedCountingSet preComputed = new IndexedCountingSet();
+        for (ConnectivityMatrix matrix : matrices)
+            preComputed.addAll(matrix.keys);
+        // We pre-compute the index so the matrix aggregate will know how big
+        // of a matrix will be required to hold it all.
+        return new VoteAggregate(preComputed, matrices);
+    }
+    
+    private VoteAggregate(IndexedCountingSet preComputed, List<ConnectivityMatrix> matrices) {
+        super(preComputed, matrices);
+        // WeightAggregate superclass will have merged in all the matrices to produce counts: we save them here
+        this.linkCounts = super.cx.clone();
         computeVotes();
     }
     
-    /**
-     *
-     * @param matrices
-     * @param nodeMerge
-     */
-    public VoteAggregate(List<ConnectivityMatrix> matrices,boolean nodeMerge) {
-        super(matrices);
-    }
-    /*
-     * computes votes
-     */
     private void computeVotes() {
-        int count = getCount();
-        double threshold = (double)count*linkThreshold;
-        for(int i=0; i<size;i++){
-            for(int j=0;j<size;j++) {
-                if(c[i][j] >= threshold) {
-                    c[i][j] = POSITIVE_VOTE;
-                } else {
-                    c[i][j] = NEGATIVE_VOTE;
-                }
+        
+        final int count = getCount();
+        final double threshold = (double)count*linkThreshold;
+        final int size = super.size();
+        if (DEBUG.Enabled) Log.debug("computeVotes, threshold=" + threshold + " out of " + count);
+        
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (super.cx[i][j] >= threshold)
+                    super.cx[i][j] = POSITIVE_VOTE;
+                else
+                    super.cx[i][j] = NEGATIVE_VOTE;
             }
         }
     }
@@ -70,14 +74,12 @@ public class VoteAggregate extends WeightAggregate {
      * @return  true if the node occurs above certain threshold
      */
     
-    public boolean isNodeVoteAboveThreshold(String label) {
-        int nodeCount = getNodeCount(label);
-        int count = getCount();
-        double threshold = (double)count*nodeThreshold;
-        if(nodeCount>=threshold) {
-            return true;
-        }else
-            return false;
+    public boolean isNodeVoteAboveThreshold(Object key) {
+        final int nodeCount = getNodeCount(key);
+        final int count = getCount();
+        final double threshold = (double)count*nodeThreshold;
+
+        return nodeCount >= threshold;
     }
    /*
     * returns the nunber of links between two nodes
@@ -86,14 +88,13 @@ public class VoteAggregate extends WeightAggregate {
     * @return number of links between node 1 and node 2
     */
             
-    public int getLinkCount(String label1,String label2) {
-        int index1 = labels.indexOf(label1);
-        int index2 = labels.indexOf(label2);
-        if(index1 >= 0 && index2 >=0 ){
+    public int getLinkCount(Object label1, Object label2) {
+        int index1 = keys.findIndex(label1);
+        int index2 = keys.findIndex(label2);
+        if (index1 >= 0 && index2 >=0)
             return linkCounts[index1][index2];
-        } else {
+        else
             return 0;
-        }
     }
     
     /*
@@ -103,22 +104,23 @@ public class VoteAggregate extends WeightAggregate {
      * @return true when number of links is above threshold
      */
     
-    public boolean isLinkVoteAboveThreshold(String label1,String label2) {
-        int linkCount = getLinkCount(label1,label2);
-        int count = getCount();
-        double threshold = (double)count*linkThreshold;
-        if( (linkCount>=threshold) && (linkCount > 0) ) {
-            return true;
-        } else {
-            return false;
-        }
+    public boolean isLinkVoteAboveThreshold(Object key1, Object key2) {
+        final int linkCount = getLinkCount(key1, key2);
+        final int count = getCount();
+        final double threshold = (double)count*linkThreshold;
+
+        return (linkCount >= threshold) && (linkCount > 0);
     }
     
     public void setLinkThreshold(double percentage) {
         linkThreshold = percentage;
+        if (DEBUG.Enabled) Log.debug("linkThresh set to " + percentage);
     }
     
     public void setNodeThreshold(double percentage) {
         nodeThreshold = percentage;
+        if (DEBUG.Enabled) Log.debug("nodeThresh set to " + percentage);
     }
+
+    // public VoteAggregate(List<ConnectivityMatrix> matrices,boolean nodeMerge) { super(matrices); }    
 }
