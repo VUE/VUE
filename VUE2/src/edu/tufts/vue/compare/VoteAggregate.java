e962189307ff89ef09d7f5d7dd21bfa0f5e5fb72
Scott Fraize <scott.fraize@gmail.com>
2012-07-23 16:15:40 +0000

    fix for multiple links between nodes in voted merge-maps
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@11312 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/compare/VoteAggregate.java
+++ VUE2/src/edu/tufts/vue/compare/VoteAggregate.java
@@ -29,9 +29,14 @@ public class VoteAggregate extends WeightAggregate
     public static final int POSITIVE_VOTE = 1;
     public static final int NEGATIVE_VOTE = 0;
     
-    private final double nodeThreshold;
-    private final double linkThreshold;
+    /** The percentage of maps that must contain this node (by merge-key) before the vote is yes */
+    private final double nodePercentMaps;
+    /** The percentage of maps that must contain this link (by endpoint merge-keys) before the vote is yes */
+    private final double linkPercentMaps;
     
+    private final int aggregateLinkThreshold;
+    private final int aggregateNodeThreshold;
+
     protected final int[][] linkCounts;
     
     public static VoteAggregate create(List<ConnectivityMatrix> matrices, double nodeThresh, double linkThresh) {
@@ -43,26 +48,33 @@ public class VoteAggregate extends WeightAggregate
         return new VoteAggregate(preComputed, matrices, nodeThresh, linkThresh);
     }
     
-    private VoteAggregate(IndexedCountingSet preComputed, List<ConnectivityMatrix> matrices, double nodeThresh, double linkThresh) {
+    private VoteAggregate(IndexedCountingSet preComputed, List<ConnectivityMatrix> matrices, double nodePercent, double linkPercent) {
         super(preComputed, matrices);
         // WeightAggregate superclass will have merged in all the matrices to produce counts: we save them here
         this.linkCounts = super.cx.clone();
-        this.nodeThreshold = nodeThresh;
-        this.linkThreshold = linkThresh;
-        if (DEBUG.Enabled) Log.debug(this + " created with node/link thresholds: " + nodeThresh + "/" + linkThresh);
-        computeVotes();
+        this.nodePercentMaps = nodePercent;
+        this.linkPercentMaps = linkPercent;
+        final double nodeThresh = nodePercent * (double) matrices.size();
+        final double linkThresh = linkPercent * (double) matrices.size();
+        this.aggregateNodeThreshold = (int) nodeThresh;
+        this.aggregateLinkThreshold = (int) linkThresh;
+        if (DEBUG.Enabled) {
+            Log.debug(this + " nodePercent " + nodePercent + "*count(" + getCount() + ") = vote threshold: " + nodeThresh + "=" + aggregateNodeThreshold);
+            Log.debug(this + " linkPercent " + linkPercent + "*count(" + getCount() + ") = vote threshold: " + linkThresh + "=" + aggregateLinkThreshold);
+        }
+        computeLinkVotes();
     }
     
-    private void computeVotes()
+    private void computeLinkVotes()
     {
-        final int count = getCount();
-        final double threshold = (double) count*linkThreshold;
         final int size = super.size();
-        if (DEBUG.Enabled) Log.debug(this + " computeVotes: linkThresh " + linkThreshold + "*count(" + count + ") = vote threshold: " + threshold);
-        
+
+        // This will cause super.getConnection to return 0 or 1,
+        // depending on if the vote passed.
+
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                if (super.cx[i][j] >= threshold)
+                if (super.cx[i][j] > 0 && super.cx[i][j] >= aggregateLinkThreshold)
                     super.cx[i][j] = POSITIVE_VOTE;
                 else
                     super.cx[i][j] = NEGATIVE_VOTE;
@@ -70,50 +82,59 @@ public class VoteAggregate extends WeightAggregate
         }
     }
     
-    /*
-     * returns true if a node/label ia above the threshold
-     * @param label the label/merge property of the nodes
-     * @return  true if the node occurs above certain threshold
-     */
+    public boolean isNodeVotedIn(Object mergeKey) {
+        return getNodeCount(mergeKey) >= aggregateNodeThreshold;
+    }
     
-    public boolean isNodeVoteAboveThreshold(Object key) {
-        final int nodeCount = getNodeCount(key);
-        final int count = getCount();
-        final double threshold = (double)count*nodeThreshold;
-
-        return nodeCount >= threshold;
+    public boolean isLinkVotedIn(Object headKey, Object tailKey) {
+        return super.getConnection(headKey, tailKey) == POSITIVE_VOTE;
+    }
+    
+    /** if the alpha [head,tail] relationship was voted yes over threshold, zero out the omega
+     * [tail,head] so we can't vote for this head/tail relationship again */
+    public boolean testAndConsumeOppositeLinkVote(Object headKey, Object tailKey) {
+        final int ihead = super.keys.findIndex(headKey);
+        final int itail = super.keys.findIndex(tailKey);
+        if (ihead < 0 || itail < 0)
+            return false;
+        
+        if (super.cx[ihead][itail] > 0) {
+            // reverse the index order and consume the opposite direction link vote
+            super.cx[itail][ihead] = 0;
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    /*
-    * returns the nunber of links between two nodes
-    * @param label1 label/merge property of node 1
-    * @param label2 label/merge property of node 2
-    * @return number of links between node 1 and node 2
-    */
-    public int getLinkCount(Object label1, Object label2) {
-        int index1 = keys.findIndex(label1);
-        int index2 = keys.findIndex(label2);
-        if (index1 >= 0 && index2 >=0)
-            return linkCounts[index1][index2];
+    /**
+     * @param headKey label/merge property of head endpoint node
+     * @param tailKey label/merge property of tail endpoint node
+     * @return number of links between head and tail (based merge-keys)
+     */
+    public int getLinkCount(Object headKey, Object tailKey) {
+        final int headIndex = keys.findIndex(headKey);
+        final int tailIndex = keys.findIndex(tailKey);
+        if (headIndex >= 0 && tailIndex >= 0)
+            return linkCounts[headIndex][tailIndex];
         else
             return 0;
     }
-    
-    /*
-     * This method is similar to isNodeVoteAboveThreshold except it works for links
-     * @param label1 label/merge property of node 1
-     * @param label2 label/merge property of node 2
-     * @return true when number of links is above threshold
-     */
-    public boolean isLinkVoteAboveThreshold(Object key1, Object key2) {
-        final int linkCount = getLinkCount(key1, key2);
-        final int count = getCount();
-        final double threshold = (double)count*linkThreshold;
 
-        return (linkCount >= threshold) && (linkCount > 0);
-    }
     
-    // public void setLinkThreshold(double percentage) {
+    // /*
+    //  * This method is similar to isNodeVoteAboveThreshold except it works for links
+    //  * @param label1 label/merge property of node 1
+    //  * @param label2 label/merge property of node 2
+    //  * @return true when number of links is above threshold
+    //  */
+    // public boolean isLinkVoteAboveThreshold(Object headKey, Object tailKey) {
+    //     return super.getConnection(headKey, tailKey) > 0;
+    //     // This is exactly what was already computed in computeVotes!
+    //     // final int linkCount = getLinkCount(headKey, tailKey);
+    //     // return linkCount > 0 && linkCount >= aggregateLinkThreshold;
+    // }
+    // Public void setLinkThreshold(double percentage) {
     //     linkThreshold = percentage;
     //     if (DEBUG.Enabled) Log.debug("linkThresh set to " + percentage, new Throwable("HERE"));
     // }
