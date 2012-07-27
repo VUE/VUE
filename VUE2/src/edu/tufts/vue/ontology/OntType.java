ccea56f57a30a823933084ce2bb2fb6c3f9bd551
Scott Fraize <scott.fraize@gmail.com>
2012-07-27 21:15:56 +0000

    added getAsKey / matchesKey, fixed performance issue
    git-svn-id: https://tuftsvue.svn.sourceforge.net/svnroot/tuftsvue/trunk@11353 d66e5487-c4fa-4dda-8c8e-ca23240363c0

--- VUE2/src/edu/tufts/vue/ontology/OntType.java
+++ VUE2/src/edu/tufts/vue/ontology/OntType.java
@@ -29,6 +29,8 @@ public class OntType implements java.io.Serializable {
     private String base;
     private String comment;
     private Style style;
+
+    private String baseURL;
     
     // for VUE2.0, unspecified styles always default to Node
     // todo: create vue resource for this choice
@@ -36,9 +38,7 @@ public class OntType implements java.io.Serializable {
     // information here
     private boolean defaultsToNode = true;
     /** Creates a new instance of OntType */
-    public OntType() {
-        
-    }
+    public OntType() { }
    
     public void setDefaultsToNode(boolean set)
     {
@@ -71,23 +71,40 @@ public class OntType implements java.io.Serializable {
     }
     
     public String getBase() {
-        if(this.base.indexOf("/edu/tufts/vue/metadata/dces_1_1.rdf") != -1)
-            return VueResources.getString("metadata.dublincore.url");
-        //if( this.base.indexOf(edu.tufts.vue.metadata.CategoryModel.CUSTOM_METADATA_FILE) != -1)
-        if( this.base.indexOf("edu/tufts/vue/metadata/categories.rdf") != -1)
-        {
-            return VueResources.getString("metadata.vue.url");
-        }
-        if( this.base.indexOf("custom.rdfs") != -1)
-        {    
-        //    return "file:///Users/dhelle01/.vue_2/custom.rdfs";
-              return "http://vue.tufts.edu/custom.rdfs";
-        }
-        return this.base;
+        return this.baseURL;
+    }
+
+    public String getAsKey() {
+        final StringBuilder b = new StringBuilder(this.baseURL);
+        b.append('#');
+        b.append(this.label);
+        return b.toString();
     }
     
-    public void setBase(String base) {
+
+    /** the given url key must end with an exact case-sensitive match to this.label -- this.id is ignored */
+    public boolean matchesKey(String key) {
+        if (!key.startsWith(baseURL))
+            return false;
+        final int localPart = key.indexOf('#');
+        return
+            localPart > 0 &&
+            localPart < key.length() - 1 &&
+            key.substring(localPart + 1).equals(label);
+    }
+    
+    public void setBase(final String base) {
         this.base = base;
+        if (base.indexOf("/edu/tufts/vue/metadata/dces_1_1.rdf") != -1)
+            baseURL = VueResources.local("metadata.dublincore.url");
+     // else if (base.indexOf(edu.tufts.vue.metadata.CategoryModel.CUSTOM_METADATA_FILE) != -1)
+        else if (base.indexOf("edu/tufts/vue/metadata/categories.rdf") != -1)
+            baseURL = VueResources.local("metadata.vue.url");
+        else if (base.indexOf("custom.rdfs") != -1)
+            // return "file:///Users/dhelle01/.vue_2/custom.rdfs";
+            baseURL = "http://vue.tufts.edu/custom.rdfs";
+        else
+            baseURL = base;
     }
     
     public Style getStyle() {
@@ -99,7 +116,8 @@ public class OntType implements java.io.Serializable {
     }
     
     public String toString() {
-        return "base="+base+", name="+ id;
+        final String xid = (id == null ? "" : id);
+        return baseURL+" for "+base+" id="+ id + (!xid.equals(label) ? (" label=" + label) : "");
         //return "Base: "+base+" name: "+ id+" Style: "+style;
     }
             
