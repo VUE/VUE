140c7e16a2ced225787f019a74dee854f34d04a8
Mike Korcynski <mkorcy@gmail.com>
2015-10-08 15:48:35 -0400

    clean up for signed builds

--- signing_code/build.sh
+++ signing_code/build.sh
@@ -3,14 +3,14 @@
 version=$1
 CMD=""
 if [[ -n "$version" ]]; then
-      CMD="scp mkorcy01@estsvuebuild-prod-01.uit.tufts.edu:/usr/local/jenkins/vue/$version/VUE-Java16.app.zip ."
+      CMD="scp mkorcy01@estsvuebuild-prod-01.uit.tufts.edu:/usr/local/jenkins/vue/$version/VUE.app.zip ."
       $CMD
-      unzip VUE-Java16.app.zip
-      rm ./VUE-Java16.app.zip
-      /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier edu.tufts.tts.ests.VUE" ./VUE.app/Contents/Info.plist
+      unzip VUE.app.zip
+      rm ./VUE.app.zip
+#      /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier edu.tufts.tts.ests.VUE" ./VUE.app/Contents/Info.plist
       codesign -f -s "Developer ID Application" ./VUE.app
       spctl --assess --verbose=4 ./VUE.app
-      productbuild --component "VUE.app" /Applications --sign "Developer ID Installer" --product "VUE.app/Contents/Info.plist" VUE.pkg
+      productbuild --component "VUE.app" /Applications --sign "Developer ID Installer" VUE.pkg
       rm -rf ./VUE.app
       CMD="scp VUE.pkg mkorcy01@estsvuebuild-prod-01.uit.tufts.edu:/usr/local/jenkins/vue/$version/"
       $CMD
