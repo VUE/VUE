aea98648c58ca3416c83bb522f040d3e482441a7
Ryan Pavlik <ryan.pavlik@collabora.com>
2019-03-01 11:45:02 -0600

    Remove AIM support, since AIM is no longer in existence

--- VUE2/src/main/java/tufts/vue/MapInspectorPanel.java
+++ VUE2/src/main/java/tufts/vue/MapInspectorPanel.java
@@ -59,7 +59,6 @@ public class MapInspectorPanel extends JPanel
     
     /** info tab panel **/
     private InfoPanel mInfoPanel = null;
-    private VueAimPanel mVueAimPanel = null;
     //private PathwayPane mPathPanel = null;
 
     /** description panel **/
@@ -115,9 +114,6 @@ public class MapInspectorPanel extends JPanel
     	   mTwitterPanel.setName(VueResources.getString("twitter.tabname"));
        }
 
-        mVueAimPanel = new VueAimPanel();
-        mVueAimPanel.setName(VueResources.getString("im.tabname"));
-        
         Widget.setWantsScroller(mapInfoStack, true);
         Widget.setWantsScrollerAlways(mapInfoStack, true);
         
@@ -125,7 +121,6 @@ public class MapInspectorPanel extends JPanel
         mapInfoStack.addPane(mInfoPanel,0f);
         mapInfoStack.addPane(mDescriptionPanel, 0f);
         mapInfoStack.addPane(metadataPanel,0f);
-        mapInfoStack.addPane(mVueAimPanel ,0f);
         if (DEBUG.TWITTER)
         	mapInfoStack.addPane(mTwitterPanel,0f);
         metadataPanel.adjustColumnModel();
