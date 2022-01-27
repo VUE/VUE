77ab1207fa97f0ab78e7c870d5277816660dcbdc
Mike Korcynski <mkorcy@gmail.com>
2022-01-27 12:05:59 -0500

    Update update_installer.rb
    remove absolute path

--- vue_releases/update_installer.rb
+++ vue_releases/update_installer.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env /usr/bin/ruby
-require "/usr/local/atsys/jenkins/vue_releases/installer_modify.rb"
+require "./installer_modify.rb"
 require "date"
 
 props = InstallerModify.new(ARGV[0], ARGV[1])
