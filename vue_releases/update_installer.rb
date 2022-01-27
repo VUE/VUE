c2527549b9dc38a5c02410f1d95a65dc3f5caebe
Mike Korcynski <mkorcy@gmail.com>
2022-01-27 12:48:49 -0500

    Update update_installer.rb

--- vue_releases/update_installer.rb
+++ vue_releases/update_installer.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env /usr/bin/ruby
-require File.expand_path("./installer_modify.rb", __FILE__)
+require File.expand_path("../installer_modify.rb", __FILE__)
 require "date"
 
 props = InstallerModify.new(ARGV[0], ARGV[1])
